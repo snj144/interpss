6a5511649de023372ac9027791d5e898b2cd5a5c
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-08-02 03:01:29 +0000

    first created

---
+++ trunk/ipss.chart/src/com/interpss/chart/dstab/SimpleOneStateChart.java
@@ -0,0 +1,147 @@
+/* ------------------
+ * XYSeriesDemo2.java
+ * ------------------
+ * (C) Copyright 2002-2004, by Object Refinery Limited.
+ *
+ */
+
+package com.interpss.chart.dstab;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.labels.StandardXYToolTipGenerator;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.StandardXYItemRenderer;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+import com.interpss.common.ui.WinUtilities;
+
+/**
+ * Demo for {@link XYSeries}, where all the y values are the same.
+ */
+public class SimpleOneStateChart extends JFrame {
+	public static int Chart_Width = 800;
+	public static int Chart_Height = 500;
+	
+	private String plotTitle = "Machine State";
+
+	private double[] xDataAry;
+	private String xLabel = "Time in Sec";
+
+	private double[] yDataAry;
+	private String   yDataLabel;
+	private String   yLabel = "State";
+	private final Color    yColor = Color.BLACK;
+	
+    /**
+     * A demonstration application showing an {@link XYSeries} where all the y-values are the same.
+     *
+     * @param title  the frame title.
+     */
+    public SimpleOneStateChart(final String title) {
+        super(title);
+        setPlotTitle(title);
+		this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+	}
+    
+    public void showChart() {
+        pack();
+        WinUtilities.center(this);
+        setVisible(true);
+    }
+    
+    public void setPlotData(final String label, final double[] xData, final double[] yData) {
+    	yDataLabel = label;
+    	xDataAry = xData;
+    	yDataAry = yData;
+    }
+    
+	public void setYLabel(final String label) {
+		yLabel = label;
+	}
+
+	public void setXLabel(final String label) {
+		xLabel = label;
+	}
+	
+	public void setPlotTitle(final String title) {
+		plotTitle = title;
+	}
+
+	public void setDimension(final int h, final int w) {
+		Chart_Height = h;
+		Chart_Width = w;
+	}
+	
+    public XYDataset createDataSet(final double[] xData, final double[] yData, final String label) {
+        final XYSeries series = new XYSeries(label);
+        for (int i = 0; i < xDataAry.length; i++) {
+            series.add(xData[i], yData[i]);
+        }
+        return new XYSeriesCollection(series);
+    }
+        
+    public void createChart() {
+        final JFreeChart chart = ChartFactory.createXYLineChart(
+            	plotTitle,
+                xLabel, 
+                yLabel, 
+                createDataSet(xDataAry, yDataAry, yDataLabel),
+                PlotOrientation.VERTICAL,
+                true,
+                false,
+                false
+            );
+
+        
+        final XYPlot plot = (XYPlot) chart.getPlot();
+        final StandardXYItemRenderer renderer = new StandardXYItemRenderer();
+        renderer.setToolTipGenerator(new StandardXYToolTipGenerator());
+        plot.setRenderer(renderer);        
+        
+        //NumberAxis axis_x = (NumberAxis) plot.getDomainAxis();
+        //axis_x.setRangeAboutValue(12.0, 24.0);
+        
+        final NumberAxis axisLeft = (NumberAxis) plot.getRangeAxis();
+        axisLeft.setAutoRangeIncludesZero(false);
+        axisLeft.setAutoRangeMinimumSize(1.0);
+
+        final XYItemRenderer v_renderer = plot.getRenderer(0);
+        v_renderer.setSeriesPaint(0, yColor);
+
+        final ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(Chart_Width, Chart_Height));
+        setContentPane(chartPanel);
+    }
+
+    /**
+     * Starting point for the demonstration application.
+     *
+     * @param args  ignored.
+     */
+    public static void main(final String[] args) {
+    	final SimpleOneStateChart plot = new SimpleOneStateChart("Bus Load Schedule");
+    	
+    	final double[] xData = new double[24];
+    	final double[] yData = new double[24];
+    	for (int i = 0; i < 24; i++) {
+    		xData[i] = i;
+    		yData[i] = i;
+    	}
+    	plot.setPlotData("Machine State", xData, yData);
+    	
+    	plot.createChart();
+    	plot.showChart();
+    }
+}
