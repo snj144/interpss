16162f48bcbc054e694f03d2bda21d5987694886
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-08-02 03:40:44 +0000

    release v0.7.10

---
+++ tags/ipss.test.user_0_7_10/src/com/interpss/test/user/SimpleTestFixture.java
@@ -0,0 +1,12 @@
+package com.interpss.test.user;
+
+import fit.ColumnFixture;
+
+public class SimpleTestFixture extends ColumnFixture {
+	// ColumnFixture
+	public int number;
+
+	public int number() {
+		return number;
+	}
+}
