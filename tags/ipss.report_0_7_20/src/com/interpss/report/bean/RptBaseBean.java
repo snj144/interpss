7f8d3cffab01d6d92c02f8f8b4a37957fd85878a
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-08-04 14:14:17 +0000

    Complete SVN migration

---
+++ tags/ipss.report_0_7_20/src/com/interpss/report/bean/RptBaseBean.java
@@ -0,0 +1,12 @@
+package com.interpss.report.bean;
+
+import com.interpss.common.util.XmlUtil;
+
+public class RptBaseBean {
+	public RptBaseBean() {
+	}
+	
+    public String toString() {
+		return XmlUtil.toXmlString(this);
+	}	
+}
---
+++ tags/ipss.report_0_7_20/src/com/interpss/report/impl/AcscFaultReport.java
@@ -0,0 +1,74 @@
+package com.interpss.report.impl;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
+import com.interpss.common.SpringAppContext;
+import com.interpss.common.util.IpssLogger;
+import com.interpss.editor.data.proj.CaseData;
+import com.interpss.editor.jgraph.GraphSpringAppContext;
+import com.interpss.editor.jgraph.ui.app.IAppSimuContext;
+import com.interpss.report.IIpssReport;
+import com.interpss.report.bean.RptMainTitleBean;
+import com.interpss.report.bean.aclf.AclfRptBeanFactory;
+import com.interpss.report.bean.acsc.AcscRptBeanFactory;
+import com.interpss.report.mapper.SimuCtxReportMapper;
+import com.interpss.simu.SimuContext;
+
+public class AcscFaultReport implements IIpssReport {
+	private String fnameAcsc3PFaultMaster = "reportTemplate/acsc/Acsc3PFaultMaster.jasper";
+	private String fnameAcscNSFaultMaster = "reportTemplate/acsc/AcscNSFaultMaster.jasper";
+	private String fnameSubFaultSummary   = "reportTemplate/acsc/AcscFaultSummarySubReport.jasper";
+	
+	public JasperPrint createReport(String version) {
+		Map<String, Object> parameters = new HashMap<String, Object>(); 
+		parameters.put("FaultSummaryJBeanDatasource", fnameSubFaultSummary);
+		
+		try {
+			IAppSimuContext appSimuCtx = GraphSpringAppContext.getIpssGraphicEditor().getCurrentAppSimuContext();
+			SimuContext simuCtx = (SimuContext)appSimuCtx.getSimuCtx();
+			SimuCtxReportMapper mapper = new SimuCtxReportMapper(SpringAppContext.getIpssMsgHub());
+
+			// map the title bean
+			RptMainTitleBean titleBean = new RptMainTitleBean();
+			mapper.mapping(appSimuCtx, titleBean, RptMainTitleBean.class);
+			titleBean.setReportTitle("Short Circuit Calculation");
+			titleBean.setCaseName(appSimuCtx.getCurrentCaseName(CaseData.CaseType_Acsc));
+			parameters.put("ReportMainTitleBean", titleBean);	
+			
+			parameters.put("FaultSummaryJBeanDatasource", AcscRptBeanFactory.getFaultSummaryDataSource(simuCtx));
+
+			return JasperFillManager.fillReport(fnameAcsc3PFaultMaster, parameters,
+									AcscRptBeanFactory.getAcscVoltAmpsDataSource(simuCtx));
+		} catch (Exception e) {
+			IpssLogger.logErr(e);
+			SpringAppContext.getIpssMsgHub().sendErrorMsg(e.toString());
+		}			
+		return null;
+	}
+
+	/**
+	 * @param fnameAcsc3PFaultMaster the fnameAcsc3PFaultMaster to set
+	 */
+	public void setFnameAcsc3PFaultMaster(String fnameAcsc3PFaultMaster) {
+		this.fnameAcsc3PFaultMaster = fnameAcsc3PFaultMaster;
+	}
+
+	/**
+	 * @param fnameAcscNSFaultMaster the fnameAcscNSFaultMaster to set
+	 */
+	public void setFnameAcscNSFaultMaster(String fnameAcscNSFaultMaster) {
+		this.fnameAcscNSFaultMaster = fnameAcscNSFaultMaster;
+	}
+
+	/**
+	 * @param fnameSubFaultSummary the fnameSubFaultSummary to set
+	 */
+	public void setFnameSubFaultSummary(String fnameSubFaultSummary) {
+		this.fnameSubFaultSummary = fnameSubFaultSummary;
+	}
+}
