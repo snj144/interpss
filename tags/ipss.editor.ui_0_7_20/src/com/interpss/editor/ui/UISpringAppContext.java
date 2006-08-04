3d9ab69dcfe04f06f1c0dd69c9b2ac7fa6acedff
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-08-04 14:08:12 +0000

    Complete SVN migration

---
+++ tags/ipss.editor.ui_0_7_20/src/com/interpss/editor/ui/UISpringAppContext.java
@@ -0,0 +1,42 @@
+package com.interpss.editor.ui;
+
+import com.interpss.common.SpringAppContext;
+import com.interpss.common.datatype.Constants;
+import com.interpss.editor.jgraph.ui.edit.IFormDataDialog;
+
+public class UISpringAppContext extends SpringAppContext {
+	public static int MainTextArea_Row = 30;
+	public static int MainTextArea_Col = 80;
+
+	public static int BrowserDialog_TextRows = 30;
+	public static int BrowserDialog_TextColumns = 130;
+	
+	/**
+	 * Get the OutputTextDialog(singleton) from the SpringAppContext.
+	 *  
+	 * @return the OutputTextDialog object
+	 */	
+	public static IOutputTextDialog getOutputTextDialog(String title) {
+		IOutputTextDialog dialog = (IOutputTextDialog)SpringAppCtx.getBean(Constants.SID_OutputTextDialog);
+		dialog.setTitle(title);
+		return dialog;
+	}
+	
+	/**
+	 * Get the ServiceScheduleDialog(singleton) from the SpringAppContext.
+	 *  
+	 * @return the ServiceScheduleDialog object
+	 */	
+	public static IFormDataDialog getServiceScheduleDialog() {
+		return (IFormDataDialog)SpringAppCtx.getBean(Constants.SID_ServiceScheduleDialog);
+	}	
+	
+	/**
+	 * Get the LoadScheduleDialog(singleton) from the SpringAppContext.
+	 *  
+	 * @return the LoadScheduleDialog object
+	 */	
+	public static IFormDataDialog getLoadScheduleDialog() {
+		return (IFormDataDialog)SpringAppCtx.getBean(Constants.SID_LoadScheduleDialog);
+	}	
+}
