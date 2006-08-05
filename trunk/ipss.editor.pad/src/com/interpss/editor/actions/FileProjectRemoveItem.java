773448f928a2d803f17da0701ade31cb8522abfe
zsz2004 <zsz2004@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-08-05 17:36:06 +0000

    Fix removeitem bug

--- trunk/ipss.editor.pad/src/com/interpss/editor/actions/FileProjectRemoveItem.java
+++ trunk/ipss.editor.pad/src/com/interpss/editor/actions/FileProjectRemoveItem.java
@@ -7,6 +7,7 @@ import javax.swing.JOptionPane;
 
 import com.interpss.editor.coreframework.IpssAbstractGraphAction;
 import com.interpss.editor.doc.IpssProject;
+import com.interpss.editor.doc.IpssProjectItem;
 import com.interpss.editor.resources.Translator;
 import com.interpss.editor.util.Utilities;
 
@@ -17,11 +18,12 @@ public class FileProjectRemoveItem extends IpssAbstractGraphAction {
 	 */
 	public void actionPerformed(ActionEvent e) {
 
-		IpssProject project = graphpad.getCurrentProjectItem().getProject();
+		IpssProjectItem currentProjectItem = graphpad.getCurrentProjectItem();
+		IpssProject project = currentProjectItem.getProject();
 
 		int r = JOptionPane.showConfirmDialog(graphpad.getFrame(), Translator
 				.getString("ItemDeleteDialog")
-				+ "'" + graphpad.getCurrentProjectItem().getFileName() + "' ?",
+				+ "'" + currentProjectItem.getFileName() + "' ?",
 				Translator.getString("Title"), JOptionPane.YES_NO_OPTION);
 
 		if (r == JOptionPane.NO_OPTION) {
@@ -29,10 +31,10 @@ public class FileProjectRemoveItem extends IpssAbstractGraphAction {
 		} else if (r == JOptionPane.YES_OPTION) {
 			graphpad.getCommand("FileProjectCloseItem").actionPerformed(e);
 
-			String filename = graphpad.getCurrentProjectItem().getName();
+			String filename = currentProjectItem.getName();
 			Utilities.delFile(filename);
 			graphpad.getProjectPanel().removeProjectItem(
-					graphpad.getCurrentProjectItem());
+					currentProjectItem);
 			graphpad.saveProject(project);
 			graphpad.update();
 		}
