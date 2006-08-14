9832d10adf4206965dd7ca64830bb931a722ab5b
zsz2004 <zsz2004@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-08-14 06:11:54 +0000

    delete temp dir

--- ipss.install/exec/run.bat
+++ ipss.install/exec/run.bat
@@ -8,4 +8,4 @@ set RPT_PATH=lib/report/jasperreports.jar;lib/apache/commons-logging.jar;lib/apa
 set IPSS_PATH=lib/ipss/ipss_pad.jar;lib/ipss/pad_export.jar;lib/ipss/ipss_core.jar;lib/ipss/ipss_dstab.jar;lib/ipss/ipss_dist.jar;lib/ipss/ipss_simu.jar;lib/ipss/ipss_control.jar;lib/ipss/ipss_custom.jar;lib/ipss/ipss_geditor_images.jar;lib/ipss/ipss_common.jar;lib/ipss/ipss_report.jar;lib/ipss/ipss_chart.jar;lib/ipss/ipss_editor_ui.jar;lib/ipss/ipss_editor_simu.jar;lib/ipss/ipss_jgraph.jar;lib/ipss/ipss_psat.jar
 set EMF_PATH=lib/eclipse/org.eclipse.emf_2.2.0.jar;lib/eclipse/org.eclipse.emf.ecore_2.2.0.jar;lib/eclipse/org.eclipse.emf.common_2.2.0.jar;
 set CUS_PATH=lib/custom/tsinghua.jar;
-java -classpath %LIB_PATH%;%BATIK_PATH%;%RPT_PATH%;%DB_PATH%;%SPRING_PATH%;%XML_PATH%;%CHART_PATH%;%IPSS_PATH%;%EMF_PATH%;%CUS_PATH% -Xms512m -Xmx1024m com.interpss.editor.GEditor
+start java -classpath %LIB_PATH%;%BATIK_PATH%;%RPT_PATH%;%DB_PATH%;%SPRING_PATH%;%XML_PATH%;%CHART_PATH%;%IPSS_PATH%;%EMF_PATH%;%CUS_PATH% -Xms512m -Xmx1024m com.interpss.editor.GEditor
