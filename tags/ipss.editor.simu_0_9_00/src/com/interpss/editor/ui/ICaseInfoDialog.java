40a7dbf787eaf83951e65e4ac1cbed7e3367de03
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-08-16 16:02:21 +0000

    release 0.9.00

---
+++ tags/ipss.editor.simu_0_9_00/src/com/interpss/editor/ui/ICaseInfoDialog.java
@@ -0,0 +1,7 @@
+package com.interpss.editor.ui;
+
+import com.interpss.editor.jgraph.ui.edit.IFormDataDialog;
+
+public interface ICaseInfoDialog extends IFormDataDialog {
+    void setCaseType(String caseType);
+}
