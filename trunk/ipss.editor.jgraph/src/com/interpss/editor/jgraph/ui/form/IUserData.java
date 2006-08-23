dc932f81c8065d168355bf31f091f34aaaf42d81
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-08-23 00:50:01 +0000

    fixed copy/paste/delete issues

--- trunk/ipss.editor.jgraph/src/com/interpss/editor/jgraph/ui/form/IUserData.java
+++ trunk/ipss.editor.jgraph/src/com/interpss/editor/jgraph/ui/form/IUserData.java
@@ -1,10 +1,12 @@
 package com.interpss.editor.jgraph.ui.form;
 
+import java.io.Serializable;
+
 /**
 *	UserData interface for the getUserObject() method of a graph cell
 */
 
-public interface IUserData {
+public interface IUserData extends Serializable {
 	public static final String NET_LABEL           = "NetLabel";
 	public static final String NET_ANNOTATE_LABEL  = "NetAnnotateLabel";
 	public static final String BUS_LABEL           = "BusLabel";
