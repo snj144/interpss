0d42a9b91f9ecec3d91b426bfae94bfb6efaa110
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-08-23 14:00:04 +0000

    PTI adapter implementation, milestone-1

--- ipss.custom/src/dev/TestAdapter.java
+++ ipss.custom/src/dev/TestAdapter.java
@@ -3,6 +3,10 @@ package dev;
 import com.interpss.common.msg.IPSSMsgHub;
 import com.interpss.common.msg.StdoutMsgListener;
 import com.interpss.common.msg.TextMessage;
+import com.interpss.simu.SimuContext;
+import com.interpss.simu.io.IpssFileAdapter;
+
+import custom.input.FileAdapter_PTIFormat;
 
 public class TestAdapter {
 
@@ -14,9 +18,21 @@ public class TestAdapter {
 		try {
 			msg = new IPSSMsgHub();
 			msg.addMsgListener(new StdoutMsgListener(TextMessage.TYPE_STATUS));
+			
+			testPTIAdapter(msg);
  		} catch (Exception e) {
  			e.printStackTrace();
 		}
 	}
 
+	public static void testPTIAdapter(IPSSMsgHub msg) throws Exception {
+		IpssFileAdapter adapter = new FileAdapter_PTIFormat();
+		SimuContext simuCtx = adapter.load("testData/ieee300.psse", msg);
+
+		//AclfNetwork net = simuCtx.getAclfNet();
+
+	  	//LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
+	  	//algo.loadflow(SpringAppContext.getIpssMsgHub());
+  		//System.out.println(net.net2String());
+	}	
 }
