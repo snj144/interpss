5c9a194488dd51deb4009db564c3e2b507421baf
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-08-26 23:03:28 +0000

    Change package name from com.interpss.x to org.interpss.x

--- ipss.test/src/com/interpss/test/TestConstants.java
+++
@@ -1,6 +0,0 @@
-package com.interpss.test;
-
-public class TestConstants {
-	public static String SpringConfigXmlFile = "c:/eclipse/interpss3.2/ipss.editor/properties/springConfig/editorAppContext.xml";
-	
-}
