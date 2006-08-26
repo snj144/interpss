31aa3df739a16b76486e77fdcbdb8cbcdc359cb4
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-08-26 22:44:29 +0000

    Change package name from com.interpss.x to org.interpss.x

--- trunk/ipss.editor.simu/src/com/interpss/editor/ui/ICaseInfoDialog.java
+++
@@ -1,7 +0,0 @@
-package com.interpss.editor.ui;
-
-import com.interpss.editor.jgraph.ui.edit.IFormDataDialog;
-
-public interface ICaseInfoDialog extends IFormDataDialog {
-    void setCaseType(String caseType);
-}
