d112d837e91933536a50b601d8b9e1b791bce566
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-08-27 15:21:02 +0000

    package name changed

---
+++ tags/Release_0_9_10/ipss.sample/org/interpss/sample/dstab/DStab_5Bus.java
@@ -0,0 +1,47 @@
+package org.interpss.sample.dstab;
+
+import java.util.logging.Level;
+
+import com.interpss.common.util.IpssLogger;
+import com.interpss.dstab.DStabObjectFactory;
+import com.interpss.dstab.DStabilityNetwork;
+import com.interpss.dstab.DynamicSimuAlgorithm;
+import com.interpss.dstab.mach.Machine;
+import com.interpss.dstab.util.IDStabSimuOutputHandler;
+import com.interpss.dstab.util.TextOutDStabSimuActionHandler;
+import com.interpss.simu.SimuContext;
+import com.interpss.simu.SimuCtxType;
+import com.interpss.simu.SimuObjectFactory;
+
+public class DStab_5Bus extends TestSetupBase {
+	
+	public void testCase() {
+		System.out.println("Begin DStab_5Bus.testCase()");
+
+		SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.DSTABILITY_NET_LITERAL, msg);
+		TestCommonUtil.loadCaseData("testData/DStab-5bus.ipss", simuCtx, msg);
+		
+		DStabilityNetwork net = simuCtx.getDStabilityNet();
+		
+		DynamicSimuAlgorithm algo = DStabObjectFactory.createDynamicSimuAlgorithm(net, msg);
+		algo.setSimuStepSec(0.01);
+		algo.setTotalSimuTimeSec(1.0);
+		
+		Machine mach = net.getMachine("Mach@0005");
+//		mach = null;
+		algo.setRefMachine(mach);
+
+		net.removeAllDEvent();
+				
+		// set up output and run the simulation
+		IDStabSimuOutputHandler handler = new TextOutDStabSimuActionHandler();
+		algo.setSimuOutputHandler(handler);
+		IpssLogger.getLogger().setLevel(Level.WARNING);
+		if (algo.initialization(msg)) {
+			handler.init(0, "");
+			algo.performSimulation(msg);
+		}
+		
+		System.out.println("End DStab_5Bus.testCase()");
+	}
+}
