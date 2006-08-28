b0b99e9529ae00690151539b21138a8bb4f2d996
zsz2004 <zsz2004@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-08-28 20:40:49 +0000

    reset packege name from com. to org.

---
+++ trunk/ipss.editor.jgraph/src/org/interpss/editor/jgraph/ui/IIpssGraphModel.java
@@ -0,0 +1,11 @@
+package org.interpss.editor.jgraph.ui;
+
+import org.interpss.editor.jgraph.ui.form.IGFormContainer;
+
+
+public interface IIpssGraphModel {
+
+	void setGFormContainer(IGFormContainer netContainer);
+
+	IGFormContainer getGFormContainer();
+}
