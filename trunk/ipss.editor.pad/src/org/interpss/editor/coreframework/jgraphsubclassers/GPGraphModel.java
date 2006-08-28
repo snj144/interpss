fac84d46bee803b36fe86457faba737ffd6997ac
zsz2004 <zsz2004@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-08-28 20:37:19 +0000

    reset packege name from com. to org.

---
+++ trunk/ipss.editor.pad/src/org/interpss/editor/coreframework/jgraphsubclassers/GPGraphModel.java
@@ -0,0 +1,101 @@
+/*
+ * Created on 05.02.2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package org.interpss.editor.coreframework.jgraphsubclassers;
+
+import java.util.List;
+
+import org.interpss.editor.coreframework.GPUserObject;
+import org.interpss.editor.jgraph.cells.AnnotateLabelCell;
+import org.interpss.editor.jgraph.cells.BusCell;
+import org.interpss.editor.jgraph.cells.LabelCell;
+import org.interpss.editor.jgraph.ui.IIpssGraphModel;
+import org.interpss.editor.jgraph.ui.form.IGBranchForm;
+import org.interpss.editor.jgraph.ui.form.IGBusForm;
+import org.interpss.editor.jgraph.ui.form.IGFormContainer;
+import org.interpss.editor.util.ICellBuisnessObject;
+import org.jgraph.graph.AttributeMap;
+import org.jgraph.graph.DefaultGraphModel;
+
+import com.interpss.common.util.IpssLogger;
+
+
+/**
+ * The base class JGraph graph model subclassers for GPGraphpad. To use a
+ * custom GraphModel subcalsser register your subclass in the graph model
+ * factory. As part of the non copyrihted online documentation of JGraph (FAQ
+ * section), this file is released here under the LGPL license as stated by the
+ * Free Software Fundation.
+ * 
+ * @see org.interpss.editor.coreframework.GPGraphpadModel
+ */
+public class GPGraphModel extends DefaultGraphModel implements IIpssGraphModel {
+
+	private IGFormContainer _netContainer = null;
+
+	/* not used any more
+	private ProjData projData = null;
+	*/
+	public GPGraphModel(IGFormContainer container) {
+		super();
+        this.setGFormContainer(container);
+		IpssLogger.getLogger().info("IpssModel object constructed");
+	}
+
+	// required for serialization
+	public GPGraphModel(List roots, AttributeMap attributes) {
+		super(roots, attributes);
+	}
+
+	protected Object cloneUserObject(Object userObject) {
+		if (userObject instanceof ICellBuisnessObject)
+			return ((ICellBuisnessObject) userObject).clone();
+		else if (userObject instanceof IGBusForm) {
+			return _netContainer.createGBusForm((IGBusForm)userObject);
+		}
+		else if (userObject instanceof IGBranchForm) {
+			return _netContainer.createGBranchForm((IGBranchForm)userObject);
+		}
+		return super.cloneUserObject(userObject);
+	}
+
+//	protected Object cloneCell(Object cellObj) {
+//		if (cellObj instanceof BusCell){
+//		//	return ((BusCell) cellObj).clone();
+//			return new BusCell(cloneUserObject(((BusCell)cellObj).getUserObject()));
+//		}
+//
+//		return super.cloneCell(cellObj);
+//	}
+
+	public Object valueForCellChanged(Object cell, Object newValue) {
+		Object userObject = getValue(cell);
+		if (userObject instanceof GPUserObject && newValue instanceof String) {
+			ICellBuisnessObject user = (ICellBuisnessObject) userObject;
+			Object oldLabel = String.valueOf(user);
+			user.setValue(newValue);
+			return oldLabel;
+		}
+
+		return super.valueForCellChanged(cell, newValue);
+	}
+
+	public void setGFormContainer(IGFormContainer _netContainer) {
+		this._netContainer = _netContainer;
+	}
+
+	public IGFormContainer getGFormContainer() {
+		return _netContainer;
+	}
+
+	/* not used anymore
+	public ProjData getProjData() { return this.projData; }
+	public void setProjData(ProjData info) {
+		this.projData = info;
+	}
+	*/
+}
+
