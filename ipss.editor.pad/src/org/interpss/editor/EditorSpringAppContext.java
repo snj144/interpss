e65591c4648955a29ec98a274426cc63072671f7
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-09-12 20:24:21 +0000

    added copyright text

--- ipss.editor.pad/src/org/interpss/editor/EditorSpringAppContext.java
+++ ipss.editor.pad/src/org/interpss/editor/EditorSpringAppContext.java
@@ -1,10 +1,33 @@
+ /*
+  * @(#)EditorSpringAppContext.java   
+  *
+  * Copyright (C) 2006 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 09/15/2006
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
 package org.interpss.editor;
 
 import org.interpss.editor.app.AppContext;
 import org.interpss.editor.coreframework.GPGraphpad;
 import org.interpss.editor.jgraph.ui.app.IAppStatus;
 import org.interpss.editor.resources.Translator;
-import org.interpss.editor.util.Utilities;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import com.interpss.common.SpringAppContext;
