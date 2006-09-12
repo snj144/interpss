e9bd4c42244881e2c5ac67ed617df450849219a3
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-09-12 20:04:35 +0000

    added copyright text

--- ipss.editor/src/org/interpss/editor/AppConfig.java
+++ ipss.editor/src/org/interpss/editor/AppConfig.java
@@ -1,3 +1,27 @@
+ /*
+  * @(#)AppConfig.java   
+  *
+  * Copyright (C) 2006 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 09/15/2006
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
 package org.interpss.editor;
 
 /**
@@ -10,7 +34,6 @@ import java.sql.SQLException;
 import org.interpss.editor.data.proj.ProjData;
 import org.interpss.editor.report.IpssRptViewer;
 import org.interpss.editor.resources.Translator;
-import org.interpss.editor.util.Utilities;
 
 import com.interpss.common.SpringAppContext;
 import com.interpss.common.io.DBManager;
