29e2aff75e2e76b89e893340e6d0ff634c112b85
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-09-12 20:01:42 +0000

    added copyright text

--- trunk/ipss.dstab.control/src/org/interpss/dstab/control/common/AbstractController.java
+++ trunk/ipss.dstab.control/src/org/interpss/dstab/control/common/AbstractController.java
@@ -1,13 +1,27 @@
-/**
- * <copyright>
- * 	Copyright www.interpss.com 2005
- * </copyright>
- * 
- * An abstrat class for implementation all controllers. All the methods has to be implemented in order to plug-in the
- * controller into the transient stability simulation program.
- *
- * $Id$
- */
+ /*
+  * @(#)AbstractController.java   
+  *
+  * Copyright (C) 2006 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 09/15/2006
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
 package org.interpss.dstab.control.common; 
 
 import java.util.Hashtable;
--- trunk/ipss.dstab.control/src/org/interpss/dstab/control/exc/ieee/ieeeAC4A/IEEE_AC4AExciterData.java
+++ trunk/ipss.dstab.control/src/org/interpss/dstab/control/exc/ieee/ieeeAC4A/IEEE_AC4AExciterData.java
@@ -1,17 +1,29 @@
-/*
- * Created on Aug 23, 2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
+ /*
+  * @(#)IEEE_AC4AExciterData.java   
+  *
+  * Copyright (C) 2006 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 09/15/2006
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
 package org.interpss.dstab.control.exc.ieee.ieeeAC4A;
 
-/**
- * @author di xiao
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
 public class IEEE_AC4AExciterData {
 	public IEEE_AC4AExciterData() {}
 	// We need to put the default values here, so that the controller could be 
