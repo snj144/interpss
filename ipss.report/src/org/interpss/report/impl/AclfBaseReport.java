9d9bedd7b048c638162b3f11b4e912f86c26dcd7
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-09-12 20:31:37 +0000

    added copyright text

--- ipss.report/src/org/interpss/report/impl/AclfBaseReport.java
+++ ipss.report/src/org/interpss/report/impl/AclfBaseReport.java
@@ -1,3 +1,27 @@
+ /*
+  * @(#)AclfBaseReport.java   
+  *
+  * Copyright (C) 2006 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 09/15/2006
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
 package org.interpss.report.impl;
 
 import java.util.Map;
@@ -11,8 +35,6 @@ import net.sf.jasperreports.engine.data.JRBeanArrayDataSource;
 
 import com.interpss.common.util.IpssLogger;
 import com.interpss.simu.SimuContext;
-
-
 
 public class AclfBaseReport {
 	private String fnameSubAclfMismatch = "reportTemplate/aclf/AclfMismatchSubReport.jasper";
