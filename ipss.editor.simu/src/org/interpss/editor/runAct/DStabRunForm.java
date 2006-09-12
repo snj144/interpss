03e870bc7b1e52693786b2ef900f1e50513a97ac
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-09-12 21:02:02 +0000

    added copyright text

--- ipss.editor.simu/src/org/interpss/editor/runAct/DStabRunForm.java
+++ ipss.editor.simu/src/org/interpss/editor/runAct/DStabRunForm.java
@@ -1,6 +1,28 @@
-package org.interpss.editor.runAct;
+ /*
+  * @(#)DStabRunForm.java   
+  *
+  * Copyright (C) 2006 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 09/15/2006
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
 
-import javax.swing.JDialog;
+package org.interpss.editor.runAct;
 
 import org.interpss.editor.SimuAppSpringAppContext;
 import org.interpss.editor.data.proj.AclfCaseData;
--- ipss.editor.simu/src/org/interpss/editor/ui/run/common/NBFaultLocDataPanel.java
+++ ipss.editor.simu/src/org/interpss/editor/ui/run/common/NBFaultLocDataPanel.java
@@ -1,3 +1,27 @@
+ /*
+  * @(#)NBFaultLocDataPanel.java   
+  *
+  * Copyright (C) 2006 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 09/15/2006
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
 package org.interpss.editor.ui.run.common;
 
 import java.util.Vector;
@@ -9,11 +33,6 @@ import org.interpss.editor.jgraph.ui.edit.IFormDataPanel;
 import com.interpss.common.ui.VerifyUtil;
 import com.interpss.common.util.IpssLogger;
 import com.interpss.common.util.Num2Str;
-
-/**
- *
- * @author  mzhou
- */
 
 public class NBFaultLocDataPanel extends javax.swing.JPanel implements IFormDataPanel{
 	private static final long serialVersionUID = 1;
