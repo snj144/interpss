a45dda00837c8b5f7f26f2b2716c9e1d85f10415
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-09-12 02:25:52 +0000

    Created for release 1.1 development

---
+++ branches/Release1.1/ipss.editor.jgraph/src/org/interpss/editor/jgraph/cells/IIpssEdge.java
@@ -0,0 +1,8 @@
+package org.interpss.editor.jgraph.cells;
+
+import org.jgraph.JGraph;
+import org.jgraph.graph.Port;
+
+public interface IIpssEdge {
+	public void connect(JGraph graph, Port aSource, Port aTarget) ;
+}
---
+++ branches/Release1.1/ipss.editor.jgraph/src/org/interpss/editor/jgraph/ui/IGraphicEditor.java
@@ -0,0 +1,41 @@
+package org.interpss.editor.jgraph.ui;
+
+/**
+ *  The Editor's MainWindow Editor has to implement the following methods to make this UI package plugable. 
+ */
+
+import java.awt.Frame;
+
+import org.interpss.editor.jgraph.ui.app.IAppSimuContext;
+import org.interpss.editor.jgraph.ui.app.IAppStatus;
+
+
+public interface IGraphicEditor {
+	/**
+	 * Get the current appSimuContext object object
+	 * 
+	 * @return the ProjData object
+	 */
+	IAppSimuContext getCurrentAppSimuContext();
+
+	/**
+	 * 
+	 * Get the current IAppStatus object
+	 */
+	IAppStatus getAppStatus();
+
+	/**
+	 * Find the hosting frame.
+	 */
+	Frame getFrame();	
+
+	/**
+	 * Get the editor version.
+	 */
+	String getVersion();
+
+	/**
+	 * refresh the current document editor panel to reflect data change
+	 */
+	void refreshCurrentDocumentEditorPanel();
+}
