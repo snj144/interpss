6cf3f8f4fe400b7331a6bda1798c2ea02088ccfc
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-09-12 21:57:54 +0000

    added copyright text

--- trunk/ipss.editor.ui/src/org/interpss/editor/ui/edit/trans/branch/NBBranchScDataPanel.java
+++ trunk/ipss.editor.ui/src/org/interpss/editor/ui/edit/trans/branch/NBBranchScDataPanel.java
@@ -1,8 +1,29 @@
- package org.interpss.editor.ui.edit.trans.branch;
+ /*
+  * @(#)NBBranchScDataPanel.java   
+  *
+  * Copyright (C) 2006 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 09/15/2006
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
+package org.interpss.editor.ui.edit.trans.branch;
   
-/**
- *
- */
 import java.util.Vector;
 
 import javax.swing.JDialog;
@@ -12,7 +33,6 @@ import org.interpss.editor.form.GBranchForm;
 import org.interpss.editor.form.GFormContainer;
 import org.interpss.editor.jgraph.ui.edit.IFormDataPanel;
 import org.interpss.editor.jgraph.ui.form.IGBranchForm;
-import org.interpss.editor.jgraph.ui.form.IGFormContainer;
 import org.interpss.editor.ui.edit.common.NBGConnectionPanel;
 
 import com.interpss.common.ui.VerifyUtil;
