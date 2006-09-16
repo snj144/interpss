a02218e4e2d3c808c42bac64a70d4b204056c132
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-09-16 19:33:42 +0000

    delete because of quota issue

--- tags/Release_0_9_00/ipss.editor.ui/src/com/interpss/editor/data/dstab/DStabPssData.java
+++
@@ -1,9 +0,0 @@
-package com.interpss.editor.data.dstab;
-
-public class DStabPssData extends DStabControllerData {
-	public static final String PssType_Simple = "SimpleStabilizer";
-	
-	public DStabPssData() {
-		this.setTypeName(PssType_Simple);
-	}		
-}
