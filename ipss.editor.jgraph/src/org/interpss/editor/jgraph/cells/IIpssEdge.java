2ef3fa0fd7f5fe61aaf94903e289e22d2ebaf530
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-09-16 20:21:41 +0000

    Release1.0 Code base

--- ipss.editor.jgraph/src/org/interpss/editor/jgraph/cells/IIpssEdge.java
+++
@@ -1,8 +0,0 @@
-package org.interpss.editor.jgraph.cells;
-
-import org.jgraph.JGraph;
-import org.jgraph.graph.Port;
-
-public interface IIpssEdge {
-	public void connect(JGraph graph, Port aSource, Port aTarget) ;
-}
