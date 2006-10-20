53cbf1ce6ce61917ec4e3121d11f787284c16edf
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-10-20 22:11:41 +0000

    Release1.0 Code base

---
+++ tags/release1.0/ipss.editor/src/org/interpss/editor/actions/AnnotateAcscPositive.java
@@ -0,0 +1,25 @@
+package org.interpss.editor.actions;
+
+import java.awt.event.ActionEvent;
+
+import org.interpss.editor.coreframework.IpssAbstractGraphAction;
+import org.interpss.editor.coreframework.IpssEditorDocument;
+import org.interpss.editor.ui.SimuActionAdapter;
+import org.interpss.editor.util.DocumentUtilFunc;
+
+
+public class AnnotateAcscPositive extends IpssAbstractGraphAction {
+    
+	/**
+	 * @see java.awt.event.ActionListener#actionPerformed(ActionEvent)
+	 */
+	public void actionPerformed(ActionEvent e) {
+		SimuActionAdapter.menu_annotate_acscPositive(getCurrentGraph());
+	}
+
+	public void update() {
+		IpssEditorDocument doc = getCurrentDocument();
+		setEnabled(DocumentUtilFunc.isAcscDocument(doc) || DocumentUtilFunc.isDStabDocument(doc));
+	}
+	
+}
