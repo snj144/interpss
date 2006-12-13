955c5e03f434d1c97bcf5af863bc2e7b6c8fe8bc
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-12-13 14:01:19 +0000

    add getObjectName in the scripting

--- ipss.plugin/src/org/interpss/dstab/control/script/ScriptingGovernor.java
+++ ipss.plugin/src/org/interpss/dstab/control/script/ScriptingGovernor.java
@@ -11,7 +11,6 @@ package org.interpss.dstab.control.script;
 
 import javax.script.ScriptEngine;
 
-import com.interpss.common.msg.IPSSMsgHub;
 import com.interpss.dstab.mach.ControllerType;
 
 public class ScriptingGovernor extends BaseScriptingController {
@@ -24,7 +23,7 @@ public class ScriptingGovernor extends BaseScriptingController {
 	 * @param name excitor name
 	 */
 	public ScriptingGovernor() {
-		super("id", "name", "caty", ControllerType.GOVERNOR_LITERAL);
+		super("govId", "govName", "govCaty", ControllerType.GOVERNOR_LITERAL);
 	}
 	
 	/**
@@ -33,19 +32,8 @@ public class ScriptingGovernor extends BaseScriptingController {
 	 * @param id governor id
 	 * @param name excitor name
 	 */
-	public ScriptingGovernor(final String id, final String name, final String caty, ScriptEngine engine) {
+	public ScriptingGovernor(final String id, final String name, final String caty) {
 		super(id, name, caty, ControllerType.GOVERNOR_LITERAL);
-		this.engine = engine;
-	}
-	
-	/**
-	 *  Init the controller states
-	 *  
-	 *  @param msg the SessionMsg object
-	 */
-	@Override
-	public boolean initStates(final IPSSMsgHub msg) {
-		return initController(GovernorScriptingObject, msg);
 	}
 } 
 
