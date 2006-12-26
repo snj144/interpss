92451d464fec745df3015b6ffcc7c0cecfbd7bc3
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-12-26 14:57:42 +0000

    add more comments to the PQLoadAngleLimiter class.

--- trunk/proj.crm/src/custom/dstab/controller/device/PQLoadAngleLimiter.java
+++ trunk/proj.crm/src/custom/dstab/controller/device/PQLoadAngleLimiter.java
@@ -21,7 +21,14 @@ import com.interpss.dstab.controller.device.IControlDevice;
 public class PQLoadAngleLimiter implements IControlDevice {
 	private double[] u = new double[3];
 	private double[] y = new double[2];
- 
+
+	/**
+    * This method receives vector y[0 1] from calling class
+    * and shall initialise the required states by knowledge 
+    * of its own outputs.
+    * 
+    * @param y0 controller output initial values
+	*/
 	public boolean initState(double[] y0) {
 		//TODO: RO makes some assumptions
             /* This area receives vector y[0 1] from calling class
@@ -34,21 +41,19 @@ public class PQLoadAngleLimiter implements IControlDevice {
 	}
 	
 	/**
-	 * This method called after the initState called.
-	 */
-        //TODO: RO makes some assumptions
-        /*
-         *this method called by originating controller?
-         *receives nothing - at the moment
-         *can be used for setting initial states for cases where inputs are
-         *required for setting
-         */
+    * This method called by the upstream controller block/device for initialization
+    * purpose. It has to called called after the initState method() called.
+    */
         
 	public double[] getU0() {
             /* should this method receive parameters, to tell the controller
              * which machine and bus it should get data from?
              *
              * then e.g. can getMachine().getQbus() to use for setting states
+             * 
+             * Mike: I think controller block and device should be atomic. Their parameters, inputs 
+             * are depending the Machine, Bus or even Network object. However, they should know 
+             * how to "behavior" themsleves.
              */
                        
 		return u;
@@ -72,8 +77,14 @@ public class PQLoadAngleLimiter implements IControlDevice {
             /* TODO: RO: For flexibility, sometimes it would make sense
              * to send the u from calling class, other times to get the 
              * u from this class.
+             * 
+             * Mike: u should always coming from outside
+             * 
              * Is it expected to send an empty set of u, or data of net/mach/bus
              * to allow this method to calculate its own inputs?
+             * 
+             * For delay block, y totally depend on the state variable, so u is empty
+             * For washout block, y = f(x, u), so we need pass u in
              */
 		return y;
 	}
