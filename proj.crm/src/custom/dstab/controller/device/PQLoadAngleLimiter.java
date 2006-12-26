e2222f98ea4fb008b03ee07f5b845f17d8b82c41
oosterwijk.ron <oosterwijk.ron@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2006-12-26 05:31:14 +0000

    RO comments and queries in device/PQLoadAngleLimiter.java

--- proj.crm/src/custom/dstab/controller/device/PQLoadAngleLimiter.java
+++ proj.crm/src/custom/dstab/controller/device/PQLoadAngleLimiter.java
@@ -2,6 +2,7 @@ package custom.dstab.controller.device;
 
 import com.interpss.dstab.controller.device.IControlDevice;
 
+
 /**
  * Implementation Notes:
  * 
@@ -22,29 +23,58 @@ public class PQLoadAngleLimiter implements IControlDevice {
 	private double[] y = new double[2];
  
 	public boolean initState(double[] y0) {
-		// TODO Auto-generated method stub
-		return false;
+		//TODO: RO makes some assumptions
+            /* This area receives vector y[0 1] from calling class
+             * and shall initialise the required states by knowledge 
+             * of its own outputs
+             */
+            boolean success = true;
+            if (success) return true;
+            else return false;
 	}
 	
 	/**
 	 * This method called after the initState called.
 	 */
+        //TODO: RO makes some assumptions
+        /*
+         *this method called by originating controller?
+         *receives nothing - at the moment
+         *can be used for setting initial states for cases where inputs are
+         *required for setting
+         */
+        
 	public double[] getU0() {
+            /* should this method receive parameters, to tell the controller
+             * which machine and bus it should get data from?
+             *
+             * then e.g. can getMachine().getQbus() to use for setting states
+             */
+                       
 		return u;
 	}
 
 	public void eulerStep1(double[] u, double dt) {
 		// TODO diff eqn implementation needed
-		
+	/* RO assumes we create the math step 1. 
+         * presumably the ControlBlock extensions could be used
+         */	
 	}
 
 	public void eulerStep2(double[] u, double dt) {
 		// TODO diff eqn implementation needed
-		
+	/* RO assumes we create the math step 2
+         */	
 	}
 
 	public double[] getY(double[] u) {
 		// need more impl
+            /* TODO: RO: For flexibility, sometimes it would make sense
+             * to send the u from calling class, other times to get the 
+             * u from this class.
+             * Is it expected to send an empty set of u, or data of net/mach/bus
+             * to allow this method to calculate its own inputs?
+             */
 		return y;
 	}
 
