3960289286877543044e3f5300b90e9cc41c5824
oosterwijk.ron <oosterwijk.ron@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2007-01-01 12:21:48 +0000

    PQLim, VcModule, RotatingExciter2LL drafts

--- proj.crm/src/custom/dstab/controller/device/PQLoadAngleLimiter.java
+++ proj.crm/src/custom/dstab/controller/device/PQLoadAngleLimiter.java
@@ -1,6 +1,9 @@
 package custom.dstab.controller.device;
 
 import com.interpss.dstab.controller.device.IControlDevice;
+import com.interpss.dstab.controller.block.DelayControlBlock;
+import com.interpss.dstab.controller.block.IControlBlock;
+import com.interpss.common.func.LookupTable;
 
 
 /**
@@ -16,29 +19,69 @@ import com.interpss.dstab.controller.device.IControlDevice;
  *      y[1] - PQLim_out
 
  * @author mzhou
+ * modified Ron O
  *
  */
 public class PQLoadAngleLimiter implements IControlDevice {
 	private double[] u = new double[3];
 	private double[] y = new double[2];
-
+        private double[] x = new double[6];
+        private DelayControlBlock QgMeas = null;
+        private DelayControlBlock VgMeas = null;
+        private DelayControlBlock PgMeas = null;
+        private LookupTable QPLookup = null;
+        private DelayControlBlock filt = null;
+        private LookupTable GRLookup = null;
+        private double v1 = 0.0;
+        
+        /*
+         * How to use a form to get parameters into this block?
+         */
+        private double TR = 0.020, KPQ = 0.5;
+        private double P0 = 0, P25 = 0.25, P50 = 0.50, P75 = 0.75, P100 = 1.0;
+        private double Q0 = -0.40, Q25 = -0.35, Q50 = -0.30, Q75 = -0.25, Q100 = -0.20;
+        private double G0 = 1.0, G25 = 1.0, G50 = 1.0, G75 = 1.0, G100 = 1.0;
+        
+        
 	/**
-    * This method receives vector y[0 1] from calling class
-    * and shall initialise the required states by knowledge 
-    * of its own outputs.
+    * This method receives vector u[0 1 2] from calling class
+    * and shall initialise the required states by this knowledge 
     * 
-    * @param y0 controller output initial values
+    * @param u0 controller initial values
 	*/
-	public boolean initState(double[] y0) {
-		//TODO: RO makes some assumptions
-            /* This area receives vector y[0 1] from calling class
-             * and shall initialise the required states by knowledge 
-             * of its own outputs
-             */
-            boolean success = true;
-            if (success) return true;
-            else return false;
-	}
+	public boolean initState(double[] u0) {
+            
+            // must initialise from inputs in this device
+            // u[0] = Qt  u[1] = Vt  u[2] = Pt
+            QgMeas = new DelayControlBlock(1, TR);
+            if (!(QgMeas.initState(u[0]))) return false;
+            VgMeas = new DelayControlBlock(1, TR);
+            if (!(VgMeas.initState(u[1]))) return false;
+            PgMeas = new DelayControlBlock(1, TR);
+            if (!(PgMeas.initState(u[2]))) return false;
+            
+            QPLookup = new LookupTable(LookupTable.TypeLinearLine);
+            QPLookup.addPoint(new LookupTable.Point(P0, Q0));
+            QPLookup.addPoint(new LookupTable.Point(P25, Q25));
+            QPLookup.addPoint(new LookupTable.Point(P50, Q50));
+            QPLookup.addPoint(new LookupTable.Point(P75, Q75));
+            QPLookup.addPoint(new LookupTable.Point(P100, Q100));
+            QPLookup.addPoint(new LookupTable.Point(5.0, Q100));
+            
+            filt = new DelayControlBlock(1.0, 1.0);
+            v1 = VgMeas.getU0()*VgMeas.getU0()*QPLookup.getY(PgMeas.getU0());
+            if (!(filt.initState(v1))) return false;
+            
+            GRLookup = new LookupTable(LookupTable.TypeLinearLine);
+            GRLookup.addPoint(new LookupTable.Point(P0, G0));
+            GRLookup.addPoint(new LookupTable.Point(P25, G25));
+            GRLookup.addPoint(new LookupTable.Point(P50, G50));
+            GRLookup.addPoint(new LookupTable.Point(P75, G75));
+            GRLookup.addPoint(new LookupTable.Point(P100, G100));
+            GRLookup.addPoint(new LookupTable.Point(5.0, G100));
+            
+            return true;
+        }
 	
 	/**
     * This method called by the upstream controller block/device for initialization
@@ -46,54 +89,54 @@ public class PQLoadAngleLimiter implements IControlDevice {
     */
         
 	public double[] getU0() {
-            /* should this method receive parameters, to tell the controller
-             * which machine and bus it should get data from?
-             *
-             * then e.g. can getMachine().getQbus() to use for setting states
-             * 
-             * Mike: I think controller block and device should be atomic. Their parameters, inputs 
-             * are depending the Machine, Bus or even Network object. However, they should know 
-             * how to "behavior" themsleves.
-             */
-                       
-		return u;
+            u[0]=QgMeas.getU0();
+            u[1]=VgMeas.getU0();
+            u[2]=PgMeas.getU0();
+	return u;
 	}
 
 	public void eulerStep1(double[] u, double dt) {
-		// TODO diff eqn implementation needed
-	/* RO assumes we create the math step 1. 
-         * presumably the ControlBlock extensions could be used
-         */	
+            QgMeas.eulerStep1(u[0], dt);
+            VgMeas.eulerStep1(u[1], dt);
+            PgMeas.eulerStep1(u[2], dt);
+            v1 = calculateV1(u);
+            filt.eulerStep1(v1, dt);
 	}
 
 	public void eulerStep2(double[] u, double dt) {
-		// TODO diff eqn implementation needed
-	/* RO assumes we create the math step 2
-         */	
+            QgMeas.eulerStep2(u[0], dt);
+            VgMeas.eulerStep2(u[1], dt);
+            PgMeas.eulerStep2(u[2], dt);
+            v1 = calculateV1(u);
+            filt.eulerStep2(v1, dt);
 	}
+        
+        private double calculateV1(double[] u) {
+            return VgMeas.getY(u[1])*VgMeas.getY(u[1])*QPLookup.getY(PgMeas.getY(u[2]));
+        }
+        
 
 	public double[] getY(double[] u) {
-		// need more impl
-            /* TODO: RO: For flexibility, sometimes it would make sense
-             * to send the u from calling class, other times to get the 
-             * u from this class.
-             * 
-             * Mike: u should always coming from outside
-             * 
-             * Is it expected to send an empty set of u, or data of net/mach/bus
-             * to allow this method to calculate its own inputs?
-             * 
-             * For delay block, y totally depend on the state variable, so u is empty
-             * For washout block, y = f(x, u), so we need pass u in
-             */
-		return y;
+            // u[0] = Qt  u[1] = Vt  u[2] = Pt
+            // y[0] = PQLim_sup  y[1] = PQLim_out
+            y[0] = filt.getStateX()-QgMeas.getStateX();
+            y[1] = y[0]*GRLookup.getY(PgMeas.getStateX())*KPQ;
+            
+	return y;
 	}
 
 	/**
 	 * return state variable for testing purpose.
 	 */
 	public double[] getStateX() {
-
-		return null;
+            // x[0] = QgMeasState, x[1] = VgMeasState, x[2]=PgMeasState,
+            // x[3] = filtState, x[4] = QPLookupY, x[5] = GRLookupY
+            x[0] = QgMeas.getStateX();
+            x[1] = VgMeas.getStateX();
+            x[2] = PgMeas.getStateX();
+            x[3] = filt.getStateX();
+            x[4] = QPLookup.getY(PgMeas.getStateX());
+            x[5] = GRLookup.getY(PgMeas.getStateX());
+        return x;
 	}
 }
