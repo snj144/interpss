8d51796b124495ac4450b726def181c2306a41b6
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2007-01-08 22:02:55 +0000

    added scriptOutputHandler - m1

---
+++ ipss.plugin/src/org/interpss/dstab/script/ScriptSimuOutputHandler.java
@@ -0,0 +1,13 @@
+package org.interpss.dstab.script;
+
+/**
+ * Text out dstab simulation action handler for processing dstab output events. This is mainly for 
+ * debug purpose 
+ */
+
+import com.interpss.dstab.util.SimuOutputHandlerAdapter;
+
+public class ScriptSimuOutputHandler extends SimuOutputHandlerAdapter {
+	public ScriptSimuOutputHandler() {
+	}
+}
