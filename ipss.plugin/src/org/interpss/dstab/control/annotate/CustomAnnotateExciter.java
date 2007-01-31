e91cba61c67e8532d1612dbdd3feca713296c3f1
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2007-01-31 19:16:25 +0000

    2nd pass of annotate controller impl

--- ipss.plugin/src/org/interpss/dstab/control/annotate/CustomAnnotateExciter.java
+++ ipss.plugin/src/org/interpss/dstab/control/annotate/CustomAnnotateExciter.java
@@ -13,7 +13,7 @@ import com.interpss.dstab.mach.Controller;
         input="pss.vs - mach.vt",
         output="this.delayBlock.y",
         refPoint="this.delayBlock.u0 - pss.vs + mach.vt",
-        display= {"Efd, this.delayBlock.y"})
+        display= {"str.Efd, this.delayBlock.y"})
 // User custom code end
         
 // Your custom class has to extend the AbstractAnnotateController. Do not the following line         
@@ -23,19 +23,19 @@ public class CustomAnnotateExciter extends AbstractAnnotateController {
 	// Define controller parameters, fields and field annotation here 
 	public double k = 50.0, t = 0.05, vmax = 10.0, vmin = 0.0;
     @AnControllerField(
-            type= AnControllerField.Type.ControlBlock,
+            type= "type.ControlBlock",
             input="this.refPoint + pss.vs - mach.vt",
+            parameter={"type.Limit", "this.k", "this.t", "this.vmax", "this.vmin"},
             y0="mach.efd"	)
-    DelayControlBlock delayBlock = new DelayControlBlock(IControlBlock.Type_Limit, k, t, vmax, vmin);
+    DelayControlBlock delayBlock;
     // User custom code end
     
 // do not modify any code below this point     
     public AnController getAnController() {
     	return (AnController)getClass().getAnnotation(AnController.class);  }
-    public IControlBlock getControlBlockField(Field field) throws Exception {
-    	return (IControlBlock)field.get(this);   }
-    public IStaticBlock getStaticBlockField(Field field) throws Exception {
-    	return (IStaticBlock)field.get(this);    }
+    public double getDoubleField(String fieldName) throws Exception {
+    	Field field = getClass().getField(fieldName);
+    	return ((Double)field.get(this)).doubleValue();   }
     public Controller getControllerField(Field field) throws Exception {
     	return (Controller)field.get(this);    }
 }
