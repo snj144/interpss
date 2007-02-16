66a48612899092942fedc37d73ca508ea9266342
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2007-02-16 13:08:52 +0000

    add JavaCompile class

---
+++ trunk/ipss.editor/src/org/interpss/editor/actions/ToolsDebugJavaCompile.java
@@ -0,0 +1,19 @@
+package org.interpss.editor.actions;
+
+import java.awt.event.ActionEvent;
+
+import org.interpss.editor.coreframework.IpssAbstractActionDefault;
+import org.interpss.editor.util.JavaCompiler;
+
+import com.interpss.common.util.IpssLogger;
+
+public class ToolsDebugJavaCompile extends IpssAbstractActionDefault {
+    
+	/**
+	 * @see java.awt.event.ActionListener#actionPerformed(ActionEvent)
+	 */
+	public void actionPerformed(ActionEvent e) {
+		IpssLogger.getLogger().info("Tools | Degug | Java Compile");
+		JavaCompiler.test();
+	}
+}
