3d2b2379bbe339df507d306fa2b7114f2912f728
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2007-02-28 03:27:57 +0000

    refatored annotate controller implementation

--- trunk/ipss.plugin/src/org/interpss/dstab/control/exc/simple/SimpleExciter.java
+++ trunk/ipss.plugin/src/org/interpss/dstab/control/exc/simple/SimpleExciter.java
@@ -9,154 +9,92 @@
  */
 package org.interpss.dstab.control.exc.simple;
 
-import com.interpss.common.exp.InvalidInputException;
+import java.lang.reflect.Field;
+
 import com.interpss.common.msg.IPSSMsgHub;
-import com.interpss.common.util.XmlUtil;
-import com.interpss.core.net.Network;
 import com.interpss.dstab.DStabBus;
-import com.interpss.dstab.DynamicSimuMethods;
-import com.interpss.dstab.controller.AbstractExciter;
+import com.interpss.dstab.controller.annotate.AnController;
+import com.interpss.dstab.controller.annotate.AnControllerField;
+import com.interpss.dstab.controller.annotate.AnnotateExciter;
 import com.interpss.dstab.controller.block.DelayControlBlock;
-import com.interpss.dstab.controller.block.IBlock;
+import com.interpss.dstab.mach.Controller;
 import com.interpss.dstab.mach.Machine;
 
-public class SimpleExciter extends AbstractExciter {
-	// define UI Editor panel for editing the controller data
-	private static final NBSimpleExciterEditPanel _editPanel = new NBSimpleExciterEditPanel();
-	
-	// define state vriables
-	private double stateVref = 0.0;
-	private DelayControlBlock controlBlock = null;
-	
-	/**
-	 * Default Constructor
-	 *
-	 */
-	public SimpleExciter() {
-		this("excId", "SimpleExciter", "InterPSS"); 
-	}
-	
-	/**
-	 * Constructor
-	 * 
-	 * @param id excitor id
-	 * @param name excitor name
-	 */
-	public SimpleExciter(final String id, final String name, final String caty) {
-		super(id, name, caty);
-		// _data is defined in the parent class. However we must be reference to an actual object
-		_data = new SimpleExciterData();
-	}
-	
-	/**
-	 * Get the excitor data object
-	 * 
-	 * @return the data object
-	 */
-	public SimpleExciterData getData() {
-		return (SimpleExciterData)_data;
-	}
-	
-	/**
-	 * Set controller data from an xml string
-	 * 
-	 * @param xmlString controller parameter xml string
-	 */
-	@Override
-	public void setDataXmlString(final String xmlString) {
-		super.setDataXmlString(xmlString);
-		// transfer the xml string to a data object
-		_data = XmlUtil.toObject(xmlString, SimpleExciterData.class);
-	}
-	
-	/**
-	 *  Init the controller states
-	 *  
-	 *  @param msg the SessionMsg object
-	 */
-	@Override
-	public boolean initStates(DStabBus abus, Machine mach, final IPSSMsgHub msg) {
-		controlBlock = new DelayControlBlock(IBlock.Type.Limit,
-				getData().getKa(), getData().getTa(), getData().getVrmax(), getData().getVrmin()); 
-		controlBlock.initStateY0(mach.getEfd());
-		
-		final double vt = abus.getVoltage().abs() / mach.getVMultiFactor();
-		stateVref = vt + controlBlock.getU0();
-		return true;
-	}
-	
-	/**
-	 * Perform one step d-eqn calculation
-	 *  
-	 * @param dt simulation time interval
-	 * @param method d-eqn solution method
-	 * @param baseFreq base frequency
-	 * @param msg the SessionMsg object
-	 */
-	@Override
-	public boolean nextStep(final double dt, final DynamicSimuMethods method, DStabBus abus, Machine mach, final Network net, final IPSSMsgHub msg) {
-		if (method == DynamicSimuMethods.MODIFIED_EULER_LITERAL) {
-			final double u = calculateU(abus, mach);
+@AnController(
+        input="pss.vs - mach.vt",
+        output="this.delayBlock.y",
+        refPoint="this.delayBlock.u0 - pss.vs + mach.vt",
+        display= {"str.Efd, this.output", "str.ExciterState, this.delayBlock.state"})
+public class SimpleExciter extends AnnotateExciter {
+	public double k = 50.0, t = 0.05, vmax = 10.0, vmin = 0.0;
+    @AnControllerField(
+            type= "type.ControlBlock",
+            input="this.refPoint + pss.vs - mach.vt",
+            parameter={"type.Limit", "this.k", "this.t", "this.vmax", "this.vmin"},
+            y0="mach.efd"	)
+    DelayControlBlock delayBlock;
+ 	
+    // UI Editor panel
+    private static NBSimpleExciterEditPanel _editPanel = new NBSimpleExciterEditPanel();
+    
+    /**
+     * Default Constructor
+     *
+     */
+    public SimpleExciter() {
+        this("excId", "excName", "InterPSS");
+    }
+    
+    /**
+     * Constructor
+     *
+     * @param id excitor id
+     * @param name excitor name
+     */
+    public SimpleExciter(String id, String name, String caty) {
+        super(id, name, caty);
+        // _data is defined in the parent class. However init it here is a MUST
+        _data = new SimpleExciterData();
+    }
+    
+    /**
+     * Get the excitor data
+     *
+     * @return the data object
+     */
+    public SimpleExciterData getData() {
+        return (SimpleExciterData)_data;
+    }
+    
+    /**
+     *  Init the controller states
+     *
+     *  @param msg the SessionMsg object
+     */
+    public boolean initStates(DStabBus bus, Machine mach, IPSSMsgHub msg) {
+        this.k = getData().getKa();
+        this.t = getData().getTa();
+        this.vmax = getData().getVrmax();
+        this.vmin = getData().getVrmin();
+        return super.initStates(bus, mach, msg);
+    }
 
-			controlBlock.eulerStep1(u, dt);
-			controlBlock.eulerStep2(u, dt);
-			return true;
-		}
-		else if (method == DynamicSimuMethods.RUNGE_KUTTA_LITERAL) {
-			// TODO: TBImpl
-			return false;
-		} else {
-			throw new InvalidInputException("SimpleExciter.nextStep(), invalid method");
-		}
-	}
-	
-	private double calculateU(DStabBus abus, Machine mach) {
-		final double vt = abus.getVoltage().abs() / mach.getVMultiFactor();
-		final double vpss = mach.hasStabilizer()? mach.getStabilizer().getOutput(abus, mach) : 0.0;
-		return stateVref + vpss - vt;		
-	}
-	
-	/**
-	 * Get the controller output
-	 * 
-	 * @return the output
-	 */
-	@Override
-	public double getOutput(DStabBus abus, Machine mach) {
-		return controlBlock.getY();
-	}
-
-	/**
-	 * Get the ref voltage Vref, for testing purpose.
-	 * 
-	 * @return Returns the vref.
-	 */
-	public double getStateVref() {
-		return stateVref;
-	}
-	
-	/**
-	 * Get the editor panel for controller data editing
-	 * 
-	 * @return the editor panel object
-	 */
-	@Override
-	public Object getEditPanel() {
-		_editPanel.init(this);
-		return _editPanel;
-	}
-	
-	/**
-	 * Get state variable X1, for testing purpose.
-	 * 
-	 * @return Returns the x1.
-	 */
-	public double getStateX1() {
-		return controlBlock.getStateX();
-	}
-	
-	public void setRefPoint(double x) {
-		stateVref = x;
-	}
-} // SimpleExcAdapter
+    /**
+     * Get the editor panel for controller data editing
+     *
+     * @return the editor panel object
+     */
+    public Object getEditPanel() {
+        _editPanel.init(this);
+        return _editPanel;
+    }
+ 
+    public AnController getAnController() {
+    	return (AnController)getClass().getAnnotation(AnController.class);  }
+    public double getDoubleField(String fieldName) throws Exception {
+    	Field field = getClass().getField(fieldName);
+    	return ((Double)field.get(this)).doubleValue();   }
+    public Controller getControllerField(Field field) throws Exception {
+    	return (Controller)field.get(this);    }
+} // SimpleExciter
 
