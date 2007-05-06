3fa4020302f6686c0d501fa314ea558d2efd754d
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2007-05-06 16:16:45 +0000

    impl output scripting

--- trunk/ipss.editor.ui/src/org/interpss/editor/ui/IScriptTool.java
+++ trunk/ipss.editor.ui/src/org/interpss/editor/ui/IScriptTool.java
@@ -3,8 +3,6 @@ package org.interpss.editor.ui;
 import java.util.Hashtable;
 import java.util.List;
 
-import com.interpss.dstab.DStabilityNetwork;
-
 public interface IScriptTool {
 	/**
 	 * 
@@ -17,46 +15,4 @@ public interface IScriptTool {
 			IOutputTextDialog textDialog,   // out text dialog
 			List<String> nameList,          // variable name list
 			List<Hashtable<String, String>> valueList);  // var value list);
-	/**
-	 * 
-	 * @param net
-	 * @throws Exception
-	 */
-	public void initDStabOutputScripting(DStabilityNetwork net) throws Exception;
-
-	/**
-	 * 
-	 * @param net
-	 * @param stateTable
-	 * @return
-	 */
-	public boolean machStatesDStabOutputScripting(DStabilityNetwork net, Hashtable stateTable);
-
-	/**
-	 * 
-	 * @param net
-	 * @param varTable
-	 * @return
-	 */
-	public boolean busVariablesDStabOutputScripting(DStabilityNetwork net, Hashtable varTable);
-
-	/**
-	 * 
-	 * @param net
-	 * @param stateTable
-	 * @return
-	 */
-	public boolean busDeviceStatesDStabOutputScripting(DStabilityNetwork net, Hashtable stateTable);
-
-	/**
-	 * 
-	 * @throws Exception
-	 */
-	public void endOfSimuStepDStabOutputScripting() throws Exception;
-
-	/**
-	 * 
-	 * @throws Exception
-	 */
-	public void closeDStabOutputScripting() throws Exception;
 }
