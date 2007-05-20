990e2b1ddc540b5e2d2154e08aea60c8b51d3355
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2007-05-20 20:17:21 +0000

    for release 1.2.01

---
+++ tags/release1.2/ipss.editor.jgraph/src/org/interpss/editor/resources/BasicProperLoader.java
@@ -0,0 +1,28 @@
+package org.interpss.editor.resources;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class BasicProperLoader {
+
+	private static Properties appProps = new Properties();
+
+	static {
+		try {
+			java.io.FileInputStream in = new java.io.FileInputStream(
+					"properties/editor.properties");
+			appProps.load(in);
+			in.close();
+		} catch (FileNotFoundException e) {
+			System.err.println(e);
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+	
+	public static String getString (final String sKey){
+		return appProps.getProperty(sKey, null);
+	}
+	
+}
