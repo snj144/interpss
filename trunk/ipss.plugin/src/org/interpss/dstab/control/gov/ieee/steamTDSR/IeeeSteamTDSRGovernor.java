1abb40a5a103872ef30c90c7bc57754f0dd025a0
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2007-05-27 01:39:07 +0000

    move cml block impl to ipss.plugin

--- trunk/ipss.plugin/src/org/interpss/dstab/control/gov/ieee/steamTDSR/IeeeSteamTDSRGovernor.java
+++ trunk/ipss.plugin/src/org/interpss/dstab/control/gov/ieee/steamTDSR/IeeeSteamTDSRGovernor.java
@@ -26,16 +26,17 @@ package org.interpss.dstab.control.gov.ieee.steamTDSR;
 
 import java.lang.reflect.Field;
 
+import org.interpss.dstab.control.cml.block.DelayControlBlock;
+import org.interpss.dstab.control.cml.block.FilterControlBlock;
+import org.interpss.dstab.control.cml.block.GainBlock;
+import org.interpss.dstab.control.cml.block.IntegrationControlBlock;
+
 import com.interpss.common.func.CMLFieldType;
 import com.interpss.common.msg.IPSSMsgHub;
 import com.interpss.dstab.DStabBus;
 import com.interpss.dstab.controller.annotate.AnController;
 import com.interpss.dstab.controller.annotate.AnControllerField;
 import com.interpss.dstab.controller.annotate.AnnotateGovernor;
-import com.interpss.dstab.controller.block.DelayControlBlock;
-import com.interpss.dstab.controller.block.FilterControlBlock;
-import com.interpss.dstab.controller.block.GainBlock;
-import com.interpss.dstab.controller.block.IntegrationControlBlock;
 import com.interpss.dstab.mach.Machine;
 
 @AnController(
