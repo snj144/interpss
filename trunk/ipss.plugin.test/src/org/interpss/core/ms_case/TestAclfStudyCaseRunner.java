08323aa29d6ab22b48f1d2cd3f959070a6c894af
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2007-09-11 01:34:45 +0000

    complete first phase GridGain implementation

---
+++ trunk/ipss.plugin.test/src/org/interpss/core/ms_case/TestAclfStudyCaseRunner.java
@@ -0,0 +1,46 @@
+package org.interpss.core.ms_case;
+
+import org.interpss.core.ms_case.aclf.AbstractAclfStudyCaseRunner;
+import org.interpss.core.ms_case.aclf.AclfBusResult;
+import org.interpss.core.ms_case.aclf.AclfStudyCaseUtilFunc;
+
+import com.interpss.common.SpringAppContext;
+import com.interpss.common.datatype.Constants;
+import com.interpss.common.exp.InterpssException;
+import com.interpss.common.util.IpssLogger;
+import com.interpss.core.ms_case.StudyCase;
+
+public class TestAclfStudyCaseRunner extends AbstractAclfStudyCaseRunner {
+	private double[] pFactorList = {
+			0.3, 0.3, 0.3, 0.3, 1.0, 1.0,
+			1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+			1.0, 1.0, 1.4, 1.3, 1.3, 1.0,
+			1.0, 1.0, 0.2, 0.2, 0.2, 0.1
+			};
+	private double[] qFactorList = {
+			0.2, 0.2, 0.2, 0.2, 0.3, 1.0,
+			1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+			1.0, 1.0, 1.0, 1.2, 1.2, 1.2,
+			1.0, 1.0, 1.0, 0.1, 0.1, 0.1
+			};
+	private StudyCase baseCase = null;
+	
+	public boolean generateCaseData(StudyCase studyCase) {
+		if (baseCase == null) {
+			baseCase = studyCase.getParent().getStudyCase(Constants.BaseStudyCaseName);
+			IpssLogger.getLogger().info("Base Study Case created - " + baseCase.getId());
+		}
+		try {
+			int index = studyCase.getCaseNumber()-1;
+			
+			String busId = "1";
+			AclfBusResult r = (AclfBusResult)baseCase.getBusResult(busId);
+			AclfStudyCaseUtilFunc.increaseBusLoad(r, pFactorList[index], qFactorList[index]);
+			IpssLogger.getLogger().info("Study Case generted - " + baseCase.getId() + ", # " + studyCase.getCaseNumber());
+		} catch (InterpssException e) {
+			SpringAppContext.getIpssMsgHub().sendErrorMsg(e.toString());
+			return false;
+		}
+		return true;
+	}			
+}
---
+++ trunk/ipss.plugin.test/src/org/interpss/core/ms_case/TestGridGainJob.java
@@ -0,0 +1,41 @@
+package org.interpss.core.ms_case;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import org.gridgain.grid.resources.GridLocalNodeIdResource;
+import org.interpss.core.grid.gridgain.AbstractIpssGridGainJob;
+import org.interpss.core.grid.gridgain.IpssGridGainUtil;
+
+import com.interpss.common.SpringAppContext;
+import com.interpss.common.ui.SerializeEMFObjectUtil;
+import com.interpss.core.CoreObjectFactory;
+import com.interpss.core.aclf.AclfNetwork;
+import com.interpss.core.algorithm.LoadflowAlgorithm;
+
+public class TestGridGainJob extends AbstractIpssGridGainJob {
+	private static final long serialVersionUID = 1;
+	
+	@GridLocalNodeIdResource
+	private UUID nodeId;
+	
+	//private GridMultiStudyCase gridMultiStudyCase = null;	
+
+	public TestGridGainJob(String model) {
+		super(model);
+	}
+	
+    public Serializable execute() {
+    	initEMFPackage();
+    	// de-serialized the model to a AclfNetwork object 
+    	String modelStr = getArgument();
+		AclfNetwork net = (AclfNetwork)SerializeEMFObjectUtil.loadModel(modelStr);
+		 
+		// perform loadflow calculation
+		LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
+		algo.loadflow(SpringAppContext.getIpssMsgHub());
+		
+		return IpssGridGainUtil.serializeAclfResult(nodeId.toString(), net);
+    }
+
+}
