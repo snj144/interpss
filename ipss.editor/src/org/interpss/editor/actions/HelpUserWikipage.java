adb0733897d27b6df516f768bb1357d4f20f7ba3
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2007-09-15 14:37:28 +0000

    change help menu location

--- ipss.editor/src/org/interpss/editor/actions/HelpUserWikipage.java
+++ ipss.editor/src/org/interpss/editor/actions/HelpUserWikipage.java
@@ -29,7 +29,7 @@ import org.interpss.editor.coreframework.IpssAbstractGraphAction;
 import org.interpss.editor.resources.Translator;
 
 
-public class HelpWikipage extends IpssAbstractGraphAction {
+public class HelpUserWikipage extends IpssAbstractGraphAction {
 	private static final long serialVersionUID = 1;
 
 	/**
