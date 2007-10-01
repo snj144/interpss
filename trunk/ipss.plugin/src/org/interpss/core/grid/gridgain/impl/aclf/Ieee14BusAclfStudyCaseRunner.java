9e312b0e4185efda3dd6ae2eed8eb941f4d4e97b
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2007-10-01 00:20:07 +0000

    moved Test<*>.java to the ipss.plugin package

---
+++ trunk/ipss.plugin/src/org/interpss/core/grid/gridgain/impl/aclf/Ieee14BusAclfStudyCaseRunner.java
@@ -0,0 +1,24 @@
+package org.interpss.core.grid.gridgain.impl.aclf;
+
+import org.interpss.core.ms_case.aclf.AbstractAclfStudyCaseRunner;
+
+import com.interpss.common.datatype.Constants;
+import com.interpss.common.util.IpssLogger;
+import com.interpss.core.ms_case.StudyCase;
+
+public class Ieee14BusAclfStudyCaseRunner extends AbstractAclfStudyCaseRunner {
+	private StudyCase baseCase = null;
+	
+	public boolean generateCaseData(StudyCase studyCase) {
+		if (baseCase == null) {
+			baseCase = studyCase.getParent().getStudyCase(Constants.BaseStudyCaseName);
+			IpssLogger.getLogger().info("Base Study Case created - " + baseCase.getId());
+		}
+
+		int index = studyCase.getCaseNumber()-1;
+		// doing nothing
+		IpssLogger.getLogger().info("Study Case generted - " + baseCase.getId() + ", # " + index);
+
+		return true;
+	}			
+}
---
+++ trunk/ipss.plugin/src/org/interpss/core/grid/gridgain/impl/aclf/SampleGridStudyCaseRunner.java
@@ -0,0 +1,51 @@
+package org.interpss.core.grid.gridgain.impl.aclf;
+
+import org.interpss.core.grid.gridgain.IpssGridGainUtil;
+import org.interpss.core.ms_case.aclf.AclfStudyCaseUtilFunc;
+
+import com.interpss.common.util.IpssLogger;
+import com.interpss.common.util.SerializeEMFObjectUtil;
+import com.interpss.core.CoreObjectFactory;
+import com.interpss.core.ms_case.GridMultiStudyCase;
+import com.interpss.core.ms_case.StudyCase;
+import com.interpss.core.ms_case.StudyCaseCreationType;
+import com.interpss.core.ms_case.impl.AbstractGridStudyCaseRunner;
+import com.interpss.core.ms_case.result.AclfNetworkResult;
+
+public class SampleGridStudyCaseRunner extends AbstractGridStudyCaseRunner {
+	@Override
+	public boolean gridRunCase() {
+		try {
+			// get the containing GridMultiStudyCase object
+			GridMultiStudyCase gridMCase = (GridMultiStudyCase)this.eContainer;
+			Object[] results = (Object[])
+				IpssGridGainUtil.performGridTask("Test Custom IpssGrid Task impl ", gridMCase);
+
+			boolean converged = true;
+			for (Object obj : results) {
+				String modelStr = (String)obj;
+				//System.out.println((String)obj);
+				AclfNetworkResult rnet = (AclfNetworkResult)SerializeEMFObjectUtil.loadModel(modelStr);
+
+				// transfer result to StudyCase, use case number for object correlation
+				int caseNumber = rnet.getCaseNumber();
+				StudyCase studyCase;
+				if (gridMCase.getCaseCreationType() == StudyCaseCreationType.MASTER_NODE_CREATION)
+					studyCase = gridMCase.getStudyCase(caseNumber);
+				else
+					// in the case of distributed
+					studyCase = CoreObjectFactory.createStudyCase("StudyCase"+caseNumber, "Case"+caseNumber, caseNumber, gridMCase);
+					
+				// reference relationship between AclfNetwork and AclfNetworkResult will also build
+				AclfStudyCaseUtilFunc.setAclfNetResult2StudyCase(studyCase, rnet);
+
+				if (!rnet.isLfConverged())
+					converged = false;
+			}
+			return converged;
+		} catch (Exception e) {
+			IpssLogger.getLogger().severe(e.toString());
+			return false;
+		}
+	}
+}
