ff99df563e41870873dfee23915492b3be293c49
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2007-10-23 13:35:51 +0000

    Release 1.3 code

---
+++ ipss.plugin.test/src/org/interpss/BaseTestSetup.java
@@ -0,0 +1,43 @@
+ /*
+  * @(#)BaseTestSetup.java   
+  *
+  * Copyright (C) 2006 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 07/15/2007
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
+package org.interpss;
+
+import org.junit.BeforeClass;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import com.interpss.common.SpringAppContext;
+import com.interpss.common.util.TestUtilFunc;
+
+public class BaseTestSetup {
+	@BeforeClass
+	public static void setSpringAppCtx() {
+		if (SpringAppContext.SpringAppCtx == null) {
+			String xmlFile = TestUtilFunc.Plugin_SpringConfigXmlFile;
+			// Set the SpringAppContext to all ApplicationContextAware objects.
+			SpringAppContext.SpringAppCtx = new FileSystemXmlApplicationContext(xmlFile);
+		}
+	}
+}
+
---
+++ ipss.plugin.test/src/org/interpss/core/ms_case/GridGain_DC_MultiCaseStudyTest.java
@@ -0,0 +1,93 @@
+ /*
+  * @(#)AclfSampleTest.java   
+  *
+  * Copyright (C) 2006 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 07/15/2007
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
+package org.interpss.core.ms_case;
+
+import static org.junit.Assert.assertTrue;
+
+import org.interpss.BaseTestSetup;
+import org.interpss.core.gridgain.Aclf5BusSampleAclfStudyCaseRunner;
+import org.interpss.core.gridgain.SampleGridGainJob;
+import org.interpss.core.gridgain.SampleGridStudyCaseRunner;
+import org.junit.Test;
+
+import com.interpss.common.SpringAppContext;
+import com.interpss.common.exp.InterpssException;
+import com.interpss.core.CoreObjectFactory;
+import com.interpss.core.aclf.AclfNetwork;
+import com.interpss.core.ms_case.GridMultiStudyCase;
+import com.interpss.core.ms_case.StudyCase;
+import com.interpss.core.ms_case.StudyCaseCreationType;
+import com.interpss.core.ms_case.result.AclfBusResult;
+import com.interpss.core.ms_case.result.AclfNetworkResult;
+import com.interpss.core.util.sample.SampleCases;
+
+public class GridGain_DC_MultiCaseStudyTest extends BaseTestSetup {
+	@Test
+	public void loadProfileCaseTest() throws InterpssException {
+		// step-1: define and load a EMF network object
+  		AclfNetwork net = CoreObjectFactory.createAclfNetwork();
+		SampleCases.load_LF_5BusSystem(net, SpringAppContext.getIpssMsgHub());
+		//System.out.println(net.net2String());
+		
+		// step-2: create a GridMultiStudyCase object, holding the net object
+		GridMultiStudyCase gridMCase = CoreObjectFactory.createGridMultiStudyCase(net, StudyCaseCreationType.DISTRIBUTED_CREATION);
+		
+		// step-3: define a GridStudyCaseRunner
+		gridMCase.setCaseRunner(new SampleGridStudyCaseRunner());
+		
+		// step-4: define an actual case runner to run the case. The actual work is 
+		//         delegated to the case runner
+		gridMCase.getGridStudyCaseRunner().setCaseRunner(new Aclf5BusSampleAclfStudyCaseRunner());
+		
+		// step-5 : define grid task jobs
+		gridMCase.createBaseCase();
+		for (int i = 1; i <= 24; i++ ) {
+			// create study case i
+			int caseNumber = i;
+			gridMCase.getGridJobs().add(new SampleGridGainJob(Integer.toString(caseNumber)));
+		}
+
+		// ste-6 : run all grid task jobs 
+		assertTrue(gridMCase.runAllCase());
+		
+		StudyCase case1 = gridMCase.getStudyCase("StudyCase1");
+		assertTrue(((AclfNetworkResult)case1.getNetResult()).isLfConverged());
+		AclfBusResult busResult = (AclfBusResult)case1.getBusResult("1");
+		assertTrue(Math.abs(busResult.getLoad().getReal()-0.48)<0.0001);
+		assertTrue(Math.abs(busResult.getLoad().getImaginary()-0.16) < 0.0001);
+
+		StudyCase case12 = gridMCase.getStudyCase("StudyCase12");
+		assertTrue(((AclfNetworkResult)case12.getNetResult()).isLfConverged());
+		busResult = (AclfBusResult)case12.getBusResult("1");
+		assertTrue(Math.abs(busResult.getLoad().getReal()-1.6)<0.0001);
+		assertTrue(Math.abs(busResult.getLoad().getImaginary()-0.8)<0.0001);
+		
+		StudyCase case24 = gridMCase.getStudyCase("StudyCase24");
+		assertTrue(((AclfNetworkResult)case24.getNetResult()).isLfConverged());
+		busResult = (AclfBusResult)case24.getBusResult("1");
+		assertTrue(Math.abs(busResult.getLoad().getReal()-0.16)<0.0001);
+		assertTrue(Math.abs(busResult.getLoad().getImaginary()-0.08)<0.0001);			
+	}	
+}
