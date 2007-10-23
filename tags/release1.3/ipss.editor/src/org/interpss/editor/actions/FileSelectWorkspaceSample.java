789823bff97cf7fe4050b2be637b4768835cc830
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2007-10-23 13:29:28 +0000

    Release 1.3 code

---
+++ tags/release1.3/ipss.editor/src/org/interpss/editor/actions/FileSelectWorkspaceSample.java
@@ -0,0 +1,29 @@
+package org.interpss.editor.actions;
+
+import java.awt.event.ActionEvent;
+
+import org.interpss.editor.GEditor;
+import org.interpss.editor.coreframework.IpssAbstractActionDefault;
+import org.interpss.editor.resources.BasicProperLoader;
+
+import com.interpss.common.ui.Workspace;
+
+public class FileSelectWorkspaceSample extends IpssAbstractActionDefault {
+	private static final long serialVersionUID = 1;
+
+	/**
+	 * @see java.awt.event.ActionListener#actionPerformed(ActionEvent)
+	 */
+	public void actionPerformed(ActionEvent e) {
+		GEditor.getGraphPad().closeWorkspace(e);
+		GEditor.getGraphPad().getSmartFrame().setVisible(false);
+		GEditor.getGraphPad().getSmartFrame().dispose();
+
+		BasicProperLoader.setUserPty(GEditor.Pty_CurrentWorkspace, GEditor.Pty_SampleWorkspace);
+		GEditor.main(new String[] {});
+	}
+	
+	public void update() {
+		setEnabled(Workspace.getCurrentType() == Workspace.Type.User);
+	}	
+}
---
+++ tags/release1.3/ipss.editor/src/org/interpss/editor/actions/RunAcsc.java
@@ -0,0 +1,60 @@
+package org.interpss.editor.actions;
+
+import java.awt.event.ActionEvent;
+
+import org.interpss.editor.coreframework.GPDocument;
+import org.interpss.editor.coreframework.IpssAbstractActionDefault;
+import org.interpss.editor.coreframework.IpssCustomDocument;
+import org.interpss.editor.coreframework.IpssEditorDocument;
+import org.interpss.editor.io.CustomFileUtility;
+import org.interpss.editor.ui.EditorActionAdapter;
+import org.interpss.editor.util.DocumentUtilFunc;
+
+import com.interpss.common.SpringAppContext;
+import com.interpss.simu.SimuContext;
+
+public class RunAcsc extends IpssAbstractActionDefault {
+	private static final long serialVersionUID = 1;
+
+	/**
+	 * @see java.awt.event.ActionListener#actionPerformed(ActionEvent)
+	 */
+	public void actionPerformed(ActionEvent e) {
+
+		if (graphpad.isBGProcessingBusy()) {
+    		SpringAppContext.getEditorDialogUtil().showWarnMsgDialog("Simulation Thread Busy", 
+			"The run-simulation thread is busy. Please wait for its finishing before starting another one.");
+			return;
+		}	
+
+		IpssEditorDocument doc = getCurrentDocument();
+		if (doc instanceof GPDocument) {
+			EditorActionAdapter.menu_run_acsc(true, graphpad.getCurrentGraph(), doc);
+		}
+		else if (doc instanceof IpssCustomDocument) {
+			if (((IpssCustomDocument)doc).getDocFile().isModified()) {
+				String filepath = graphpad.getCurrentProject().getProjectPath() + "/" + 
+				((IpssCustomDocument)doc).getFileName();
+				SimuContext simuCtx = (SimuContext)doc.getSimuAppContext().getSimuCtx();
+				if (CustomFileUtility.loadCustomFile(filepath, simuCtx)) {
+					EditorActionAdapter.menu_run_acsc(false, null, doc);
+					doc.getSimuAppContext().setSimuNetDataDirty(false);
+				}	
+				else {
+					SpringAppContext.getIpssMsgHub().sendWarnMsg("Custom data file loading error, filename: " + filepath);
+				}
+			} 
+			else {
+				EditorActionAdapter.menu_run_acsc(false, null, doc);
+			}
+		}
+		// After a run, some menuitems may need to be enabled
+		graphpad.update();
+	}
+
+	@Override
+	public void update() {
+		IpssEditorDocument doc = getCurrentDocument();
+		setEnabled(DocumentUtilFunc.isAcscDocument(doc) || DocumentUtilFunc.isDStabDocument(doc));
+	}
+}
