41b9356757a7193932ab9cb0ba61f5fd939a3b8f
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2007-12-08 14:48:42 +0000

    impl DCLF

---
+++ trunk/ipss.editor/src/org/interpss/editor/actions/RunTranCapacityAnalysisRunDclf.java
@@ -0,0 +1,19 @@
+package org.interpss.editor.actions;
+
+import java.awt.event.ActionEvent;
+
+import org.interpss.editor.coreframework.IpssAbstractActionDefault;
+
+public class RunTranCapacityAnalysisRunDclf extends IpssAbstractActionDefault {
+	private static final long serialVersionUID = 1;
+
+	/**
+	 * @see java.awt.event.ActionListener#actionPerformed(ActionEvent)
+	 */
+	public void actionPerformed(ActionEvent e) {
+	}
+
+	@Override
+	public void update() {
+	}
+}
