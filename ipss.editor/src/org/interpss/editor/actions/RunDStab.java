9ff0c78748144ae74fc7fa7ed85792275f960723
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2007-12-09 17:10:39 +0000

    refactor run action

--- ipss.editor/src/org/interpss/editor/actions/RunDStab.java
+++ ipss.editor/src/org/interpss/editor/actions/RunDStab.java
@@ -2,16 +2,11 @@ package org.interpss.editor.actions;
 
 import java.awt.event.ActionEvent;
 
-import org.interpss.editor.coreframework.GPDocument;
 import org.interpss.editor.coreframework.IpssAbstractActionDefault;
-import org.interpss.editor.coreframework.IpssCustomDocument;
 import org.interpss.editor.coreframework.IpssEditorDocument;
-import org.interpss.editor.io.CustomFileUtility;
 import org.interpss.editor.ui.EditorActionAdapter;
 import org.interpss.editor.util.DocumentUtilFunc;
-
-import com.interpss.common.SpringAppContext;
-import com.interpss.simu.SimuContext;
+import org.interpss.editor.util.RunUtilFunc;
 
 public class RunDStab extends IpssAbstractActionDefault {
 	private static final long serialVersionUID = 1;
@@ -20,35 +15,7 @@ public class RunDStab extends IpssAbstractActionDefault {
 	 * @see java.awt.event.ActionListener#actionPerformed(ActionEvent)
 	 */
 	public void actionPerformed(ActionEvent e) {
-    	if (graphpad.isBGProcessingBusy()) {
-    		SpringAppContext.getEditorDialogUtil().showWarnMsgDialog("Simulation Thread Busy", 
-    				"The run-simulation thread is busy. Please wait for its finishing before starting another one.");
-    		return;
-    	}
-
-		IpssEditorDocument doc = getCurrentDocument();
-		if (doc instanceof GPDocument) {
-			EditorActionAdapter.menu_run_dstab(true, graphpad.getCurrentGraph(), doc);
-		}
-		else if (doc instanceof IpssCustomDocument) {
-			if (((IpssCustomDocument)doc).getDocFile().isModified()) {
-				String filepath = graphpad.getCurrentProject().getProjectPath() + "/" + 
-				((IpssCustomDocument)doc).getFileName();
-				SimuContext simuCtx = (SimuContext)doc.getSimuAppContext().getSimuCtx();
-				if (CustomFileUtility.loadCustomFile(filepath, simuCtx)) {
-					EditorActionAdapter.menu_run_dstab(false, null, doc);
-					doc.getSimuAppContext().setSimuNetDataDirty(false);
-				}	
-				else {
-					SpringAppContext.getIpssMsgHub().sendWarnMsg("Custom data file loading error, filename: " + filepath);
-				}
-			} 
-			else {
-				EditorActionAdapter.menu_run_dstab(false, null, doc);
-			}
-		}
-		// After a run, some menuitems may need to be enabled
-		graphpad.update();
+		RunUtilFunc.performRunAction(getCurrentDocument(), EditorActionAdapter.RunType.DStab, graphpad);
 	}
 
 	public void update() {
--- ipss.editor/src/org/interpss/editor/actions/RunScripting.java
+++ ipss.editor/src/org/interpss/editor/actions/RunScripting.java
@@ -2,16 +2,11 @@ package org.interpss.editor.actions;
 
 import java.awt.event.ActionEvent;
 
-import org.interpss.editor.coreframework.GPDocument;
 import org.interpss.editor.coreframework.IpssAbstractActionDefault;
-import org.interpss.editor.coreframework.IpssCustomDocument;
 import org.interpss.editor.coreframework.IpssEditorDocument;
-import org.interpss.editor.io.CustomFileUtility;
 import org.interpss.editor.ui.EditorActionAdapter;
 import org.interpss.editor.util.DocumentUtilFunc;
-
-import com.interpss.common.SpringAppContext;
-import com.interpss.simu.SimuContext;
+import org.interpss.editor.util.RunUtilFunc;
 
 public class RunScripting extends IpssAbstractActionDefault {
 	private static final long serialVersionUID = 1;
@@ -20,29 +15,7 @@ public class RunScripting extends IpssAbstractActionDefault {
 	 * @see java.awt.event.ActionListener#actionPerformed(ActionEvent)
 	 */
 	public void actionPerformed(ActionEvent e) {
- 		IpssEditorDocument doc = getCurrentDocument();
-		if (doc instanceof GPDocument) {
-			EditorActionAdapter.menu_run_scripting(true, graphpad.getCurrentGraph(), doc);
-		}
-		else if (doc instanceof IpssCustomDocument) {
-			if (((IpssCustomDocument)doc).getDocFile().isModified()) {
-				String filepath = graphpad.getCurrentProject().getProjectPath() + "/" + 
-				((IpssCustomDocument)doc).getFileName();
-				SimuContext simuCtx = (SimuContext)doc.getSimuAppContext().getSimuCtx();
-				if (CustomFileUtility.loadCustomFile(filepath, simuCtx)) {
-					EditorActionAdapter.menu_run_scripting(false, null, doc);
-					doc.getSimuAppContext().setSimuNetDataDirty(false);
-				}	
-				else {
-					SpringAppContext.getIpssMsgHub().sendWarnMsg("Custom data file loading error, filename: " + filepath);
-				}
-			} 
-			else {
-				EditorActionAdapter.menu_run_scripting(false, null, doc);
-			}
-		}
-		// After a run, some menu items may need to be enabled
-		graphpad.update();
+		RunUtilFunc.performRunAction(getCurrentDocument(), EditorActionAdapter.RunType.Script, graphpad);
 	}
 
 	public void update() {
