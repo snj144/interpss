03dd9269b84e9c1f9a78a7de632924d5bab74db6
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-01-02 01:58:10 +0000

    impl Xml DStab run, complete first testcase

--- ipss.plugin.test/src/org/interpss/schema/IpssSchemaIEEE11ModelTest.java
+++ ipss.plugin.test/src/org/interpss/schema/IpssSchemaIEEE11ModelTest.java
@@ -28,35 +28,32 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 
-import org.apache.commons.math.complex.Complex;
 import org.interpss.dstab.ieeeModel.DStabTestSetupBase;
 import org.interpss.editor.mapper.RunForm2AlgorithmMapper;
 import org.interpss.xml.IpssXmlParser;
 import org.junit.Test;
 
-import com.interpss.common.SpringAppContext;
 import com.interpss.common.mapper.IpssMapper;
-import com.interpss.common.util.TestUtilFunc;
-import com.interpss.core.acsc.AcscBusFault;
 import com.interpss.core.algorithm.LoadflowAlgorithm;
 import com.interpss.dstab.DStabObjectFactory;
 import com.interpss.dstab.DStabilityNetwork;
 import com.interpss.dstab.DynamicSimuAlgorithm;
+import com.interpss.dstab.DynamicSimuMethods;
 import com.interpss.dstab.test.StateVariableTestRecorder;
-import com.interpss.dstab.test.YMatrixChangeTestRecorder;
 import com.interpss.dstab.util.DStabOutSymbol;
 import com.interpss.simu.SimuContext;
 import com.interpss.simu.SimuCtxType;
 import com.interpss.simu.SimuObjectFactory;
 
 public class IpssSchemaIEEE11ModelTest extends DStabTestSetupBase {
+	/*
 	private double[] 
 	      timePoints    = {0.0,    1.0,    2.0,    3.0,    4.0,    5.0,    6.0,    7.0,    8.0,    9.0,    10.0},
 	      machAngPoints = {49.460, 49.460, 46.705, 47.171, 47.563, 47.883, 48.145, 48.361, 48.540, 48.689, 48.814},
 	      machPePoints  = {0.426,  0.426,  0.333,  0.352,  0.367,  0.379,  0.389,  0.396,  0.402,  0.407,  0.411};
 	private Complex yFault = new Complex(1.2595,-100000012.97521),
 	                yClear = new Complex(1.2595,-12.97521);
-	
+	*/
 	@Test
 	public void test_Case1() throws Exception {
 		File xmlFile = new File("testData/xml/RunDStabCase.xml");
@@ -71,51 +68,36 @@ public class IpssSchemaIEEE11ModelTest extends DStabTestSetupBase {
 		
 		DynamicSimuAlgorithm algo = DStabObjectFactory.createDynamicSimuAlgorithm(net, msg);
 	  	IpssMapper mapper = new RunForm2AlgorithmMapper();
+	  	int caseCnt = 0;
 	  	for ( RunDStabStudyCaseXmlType dstabCase : parser.getRunDStabStudyCaseList()) {
 	  		mapper.mapping(dstabCase, algo, RunDStabStudyCaseXmlType.class);
+	  		
+	  		if (caseCnt++ == 0) {
+			  	assertTrue(algo.getTotalSimuTimeSec() == 1.0);
+			  	assertTrue(algo.getSimuStepSec() == 0.002);
+			  	assertTrue(algo.getSimuMethod() == DynamicSimuMethods.MODIFIED_EULER);
+			  	assertTrue(algo.getRefMachine() != null);
+			  	assertTrue(algo.getRefMachine().getId().equals("Mach@0003"));
+			  	assertTrue(algo.getDStabNet().getDEventList().size() == 0);
+
+			  	LoadflowAlgorithm aclfAlgo = algo.getAclfAlgorithm();
+				aclfAlgo.loadflow(msg);
+			  	assertTrue(simuCtx.getDStabilityNet().isLfConverged());
+				
+				double[] timePoints    = {0.0,  0.5,  1.0},
+		     	 machAngPoints = {49.460, 49.460, 49.460};
+				StateVariableTestRecorder stateTestRecorder = new StateVariableTestRecorder(0.0001);
+				stateTestRecorder.addTestRecords("Mach@0001", StateVariableTestRecorder.RecType_Machine, 
+						DStabOutSymbol.OUT_SYMBOL_MACH_ANG, timePoints, machAngPoints);
+				algo.setSimuOutputHandler(stateTestRecorder);
+			  	
+				if (algo.initialization(msg)) {
+					System.out.println("Running DStab simulation ...");
+					algo.performSimulation(msg);
+				}
+				
+				assertTrue(stateTestRecorder.diffTotal("Mach@0001", StateVariableTestRecorder.RecType_Machine, 
+						DStabOutSymbol.OUT_SYMBOL_MACH_ANG) < 0.01);	  		}
 	  	}
-		
-		addDynamicEventData(net);
-		
-		LoadflowAlgorithm aclfAlgo = algo.getAclfAlgorithm();
-		aclfAlgo.loadflow(msg);
-	  	assertTrue(simuCtx.getDStabilityNet().isLfConverged());
-		
-		StateVariableTestRecorder stateTestRecorder = new StateVariableTestRecorder(0.0001);
-		stateTestRecorder.addTestRecords("Mach@0001", StateVariableTestRecorder.RecType_Machine, 
-				DStabOutSymbol.OUT_SYMBOL_MACH_ANG, timePoints, machAngPoints);
-		stateTestRecorder.addTestRecords("Mach@0001", StateVariableTestRecorder.RecType_Machine, 
-				DStabOutSymbol.OUT_SYMBOL_MACH_PE, timePoints, machPePoints);
-		algo.setSimuOutputHandler(stateTestRecorder);
-
-	  	
-		YMatrixChangeTestRecorder yTestRecorder = new YMatrixChangeTestRecorder(0.0001);
-		// a 3P fault at t = 1.0, duration = 0.1, Y matrix should change
-		yTestRecorder.addTestRecord(new YMatrixChangeTestRecorder.TestRecord("0003", 1.0));
-		// The fault cleared at t = 1.1, Y matrix should change again.
-		yTestRecorder.addTestRecord(new YMatrixChangeTestRecorder.TestRecord("0003", 1.1));
-		yTestRecorder.initBusNumber(net);
-		net.setNetChangeListener(yTestRecorder);	
-
-		if (algo.initialization(msg)) {
-			System.out.println("Running DStab simulation ...");
-			algo.performSimulation(msg);
-		}
-
-		assertTrue(stateTestRecorder.diffTotal("Mach@0001", StateVariableTestRecorder.RecType_Machine, 
-				DStabOutSymbol.OUT_SYMBOL_MACH_ANG) < 0.01);
-		assertTrue(stateTestRecorder.diffTotal("Mach@0001", StateVariableTestRecorder.RecType_Machine, 
-				DStabOutSymbol.OUT_SYMBOL_MACH_PE) < 0.01);
-
-		// check 3P fault at t = 1.0
-		assertTrue(yTestRecorder.getTestRecord(1.0, "0003").isChanged());
-		assertTrue(yTestRecorder.getTestRecord(1.0, "0003").difference(yFault).abs() < 0.0001);
-		
-		// check 3P fault clearing at t = 1.1
-		assertTrue(yTestRecorder.getTestRecord(1.1, "0003").isChanged());
-		assertTrue(yTestRecorder.getTestRecord(1.1, "0003").difference(yClear).abs() < 0.0001);
-		
-		//System.out.println(stateTestRecorder);
-		//System.out.println(yTestRecorder);
 	}
 }
