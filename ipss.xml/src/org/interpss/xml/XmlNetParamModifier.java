a01b0352d4c9fd40931557d67a75a9054a4ccdc7
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-01-12 18:29:26 +0000

    no code chnage, c-s-F to re-format code

--- ipss.xml/src/org/interpss/xml/XmlNetParamModifier.java
+++ ipss.xml/src/org/interpss/xml/XmlNetParamModifier.java
@@ -1,26 +1,26 @@
- /*
-  * @(#)XmlNetParamModifier.java   
-  *
-  * Copyright (C) 2006-2007 www.interpss.org
-  *
-  * This program is free software; you can redistribute it and/or
-  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
-  * as published by the Free Software Foundation; either version 2.1
-  * of the License, or (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * @Author Mike Zhou
-  * @Version 1.0
-  * @Date 09/15/2007
-  * 
-  *   Revision History
-  *   ================
-  *
-  */
+/*
+ * @(#)XmlNetParamModifier.java   
+ *
+ * Copyright (C) 2006-2007 www.interpss.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @Author Mike Zhou
+ * @Version 1.0
+ * @Date 09/15/2007
+ * 
+ *   Revision History
+ *   ================
+ *
+ */
 
 package org.interpss.xml;
 
@@ -51,99 +51,120 @@ import com.interpss.core.net.Bus;
 import com.interpss.core.net.Network;
 
 public class XmlNetParamModifier {
-	private static enum ParamType {Power, Z};
+	private static enum ParamType {
+		Power, Z
+	};
 
 	/**
 	 * Apply the modification record to the network object
 	 * 
-	 * @param net a Network/AclfNetwork/AclfAdjNetwork/... object to be modified
-	 * @param mod the modification record
+	 * @param net
+	 *            a Network/AclfNetwork/AclfAdjNetwork/... object to be modified
+	 * @param mod
+	 *            the modification record
 	 */
-	public static boolean applyModification2Net(Network net, ModificationXmlType mod) {
+	public static boolean applyModification2Net(Network net,
+			ModificationXmlType mod) {
 		// apply the Network-level changes
 		if (mod.getNetChangeRec() != null) {
 			if (mod.getNetChangeRec().getBusChangeRecArray().length > 0) {
-				for (BusChangeRecXmlType busRec : mod.getNetChangeRec().getBusChangeRecArray()) {
+				for (BusChangeRecXmlType busRec : mod.getNetChangeRec()
+						.getBusChangeRecArray()) {
 					Bus bus = getBus(busRec, net);
 					bus.setStatus(busRec.getSetSatus());
-					IpssLogger.getLogger().info("Bus " + bus.getId() + " status has been set to " + bus.isActive());
-  				}
-  			}
-  				
-  			if (mod.getNetChangeRec().getBranchChangeRecArray().length > 0) {
-  				for (BranchChangeRecXmlType braRec : mod.getNetChangeRec().getBranchChangeRecArray()) {
-  					Branch branch = getBranch(braRec, net);
-  					if (branch != null) {
-  						branch.setStatus(braRec.getSetInService());
-  						IpssLogger.getLogger().info("Branch " + branch.getId() + " service status has been set to " + branch.isActive());
-  					} else {
-  			  			return false;
-  					}
-  				}
-  			}
+					IpssLogger.getLogger().info(
+							"Bus " + bus.getId() + " status has been set to "
+									+ bus.isActive());
+				}
+			}
+
+			if (mod.getNetChangeRec().getBranchChangeRecArray().length > 0) {
+				for (BranchChangeRecXmlType braRec : mod.getNetChangeRec()
+						.getBranchChangeRecArray()) {
+					Branch branch = getBranch(braRec, net);
+					if (branch != null) {
+						branch.setStatus(braRec.getSetInService());
+						IpssLogger.getLogger().info(
+								"Branch " + branch.getId()
+										+ " service status has been set to "
+										+ branch.isActive());
+					} else {
+						return false;
+					}
+				}
+			}
 		}
-  		
+
 		// apply the AclfNetwork-level changes
-  		if (mod.getAclfNetChangeRec() != null && net instanceof AclfNetwork) {
-  			AclfNetwork aclfNet = (AclfNetwork)net;
+		if (mod.getAclfNetChangeRec() != null && net instanceof AclfNetwork) {
+			AclfNetwork aclfNet = (AclfNetwork) net;
 			if (mod.getAclfNetChangeRec().getAclfBusChangeRecArray().length > 0) {
-				for (AclfBusChangeRecXmlType busRec : mod.getAclfNetChangeRec().getAclfBusChangeRecArray()) {
-					AclfBus bus = (AclfBus)getBus(busRec, aclfNet);
+				for (AclfBusChangeRecXmlType busRec : mod.getAclfNetChangeRec()
+						.getAclfBusChangeRecArray()) {
+					AclfBus bus = (AclfBus) getBus(busRec, aclfNet);
 					if (bus != null) {
 						if (busRec.getLoadChange() != null) {
 							// modify load value
 							if (busRec.getLoadChange().getValueChange() != null) {
-								Complex x = applyPowerChangeRec(bus.getLoad(), busRec.getLoadChange().getValueChange(), aclfNet.getBaseKva());
+								Complex x = applyPowerChangeRec(
+										bus.getLoad(),
+										busRec.getLoadChange().getValueChange(),
+										aclfNet.getBaseKva());
 								bus.setLoadP(x.getReal());
 								bus.setLoadQ(x.getImaginary());
 							}
-							
+
 							// modify load code
 							if (busRec.getLoadChange().getCodeChange() != null) {
-								AclfLoadCodeChangeXmlType codeChange = busRec.getLoadChange().getCodeChange(); 
-								bus.setLoadCode(
-									codeChange.getCode() == AclfLoadCodeXmlData.CONST_P? AclfLoadCode.CONST_P :
-										(codeChange.getCode() == AclfLoadCodeXmlData.CONST_I? AclfLoadCode.CONST_I :
-											(codeChange.getCode() == AclfLoadCodeXmlData.CONST_Z? AclfLoadCode.CONST_Z :
-												(codeChange.getCode() == AclfLoadCodeXmlData.EXPONENTIAL? AclfLoadCode.EXPONENTIAL : AclfLoadCode.NON_LOAD))));
+								AclfLoadCodeChangeXmlType codeChange = busRec
+										.getLoadChange().getCodeChange();
+								bus
+										.setLoadCode(codeChange.getCode() == AclfLoadCodeXmlData.CONST_P ? AclfLoadCode.CONST_P
+												: (codeChange.getCode() == AclfLoadCodeXmlData.CONST_I ? AclfLoadCode.CONST_I
+														: (codeChange.getCode() == AclfLoadCodeXmlData.CONST_Z ? AclfLoadCode.CONST_Z
+																: (codeChange
+																		.getCode() == AclfLoadCodeXmlData.EXPONENTIAL ? AclfLoadCode.EXPONENTIAL
+																		: AclfLoadCode.NON_LOAD))));
 								if (bus.getLoadCode() == AclfLoadCode.EXPONENTIAL) {
 									bus.setExpLoadP(codeChange.getExpLoadP());
 									bus.setExpLoadQ(codeChange.getExpLoadQ());
 								}
 							}
 						}
-					}
-					else {
-			  			return false;
+					} else {
+						return false;
 					}
 				}
 			}
-  				
+
 			if (mod.getAclfNetChangeRec().getAclfBranchChangeRecArray().length > 0) {
-				for (AclfBranchChangeRecXmlType braRec : mod.getAclfNetChangeRec().getAclfBranchChangeRecArray()) {
-					AclfBranch branch = (AclfBranch)getBranch(braRec, aclfNet);
+				for (AclfBranchChangeRecXmlType braRec : mod
+						.getAclfNetChangeRec().getAclfBranchChangeRecArray()) {
+					AclfBranch branch = (AclfBranch) getBranch(braRec, aclfNet);
 					if (branch != null) {
 						if (braRec.getZChange() != null) {
-							Complex z = applyComplexParamChangeRec(branch.getZ(), braRec.getZChange(), 
-											            ParamType.Z, aclfNet.getBaseKva(), branch.getHigherBaseVoltage());
+							Complex z = applyComplexParamChangeRec(branch
+									.getZ(), braRec.getZChange(), ParamType.Z,
+									aclfNet.getBaseKva(), branch
+											.getHigherBaseVoltage());
 							branch.setZ(z);
 						}
 					} else {
-			  			return false;
+						return false;
 					}
 				}
 			}
 		}
-	  	return true;
+		return true;
 	}
 
 	private static Bus getBus(BusRecXmlType busRec, Network net) {
-		String busId = busRec.getBusId(); 
+		String busId = busRec.getBusId();
 		Bus bus = net.getBus(busId);
 		if (bus == null) {
 			IpssLogger.getLogger().warning("Bus not found, busId: " + busId);
-  			SpringAppContext.getEditorDialogUtil().showErrMsgDialog("Error in Xml", 
-		  				"Bus not found, busId: " + busId);
+			SpringAppContext.getEditorDialogUtil().showErrMsgDialog(
+					"Error in Xml", "Bus not found, busId: " + busId);
 		}
 		return bus;
 	}
@@ -158,32 +179,41 @@ public class XmlNetParamModifier {
 		else
 			branch = net.getBranch(fromId, toId);
 		if (branch == null) {
-			IpssLogger.getLogger().warning("Branch not found, fromId, toId, cirNo: " + fromId + ", " + toId + ", " + cirNo);
-		  	SpringAppContext.getEditorDialogUtil().showErrMsgDialog("Error in Xml", 
-		  				"Branch not found, fromId, toId, cirNo: " + fromId + ", " + toId + ", " + cirNo);
+			IpssLogger.getLogger().warning(
+					"Branch not found, fromId, toId, cirNo: " + fromId + ", "
+							+ toId + ", " + cirNo);
+			SpringAppContext.getEditorDialogUtil().showErrMsgDialog(
+					"Error in Xml",
+					"Branch not found, fromId, toId, cirNo: " + fromId + ", "
+							+ toId + ", " + cirNo);
 		}
 		return branch;
 	}
-	
-	private static Complex applyPowerChangeRec(Complex original, ComplexParamChangeXmlType changeRec, double baseKva) {
-		return applyComplexParamChangeRec(original, changeRec, ParamType.Power, baseKva, 1.0);
+
+	private static Complex applyPowerChangeRec(Complex original,
+			ComplexParamChangeXmlType changeRec, double baseKva) {
+		return applyComplexParamChangeRec(original, changeRec, ParamType.Power,
+				baseKva, 1.0);
 	}
-	
-	private static Complex applyComplexParamChangeRec(Complex original, ComplexParamChangeXmlType changeRec, ParamType ptype, double baseKva, double busBaseVolt) {
-		if (changeRec.getAction() == ChangeActionXmlData.ADD ||
-			changeRec.getAction() == ChangeActionXmlData.SET) {
+
+	private static Complex applyComplexParamChangeRec(Complex original,
+			ComplexParamChangeXmlType changeRec, ParamType ptype,
+			double baseKva, double busBaseVolt) {
+		if (changeRec.getAction() == ChangeActionXmlData.ADD
+				|| changeRec.getAction() == ChangeActionXmlData.SET) {
 			// for add/set, use value and unit (PU or power unit)
 			ComplexXmlType c = changeRec.getValue();
 			double re = c.getRe(), im = c.getIm();
 			if (changeRec.getUnit() != UnitXmlData.PU) {
 				// convert set/add value to PU
-				byte unit = IpssXmlParser.mapXmlUnitType2IpssUnitType(changeRec.getUnit());
+				byte unit = IpssXmlParser.mapXmlUnitType2IpssUnitType(changeRec
+						.getUnit());
 				if (ptype == ParamType.Power) {
 					re = UnitType.pConversion(re, baseKva, unit, UnitType.PU);
 					im = UnitType.pConversion(im, baseKva, unit, UnitType.PU);
-				}
-				else if (ptype == ParamType.Z) {
-					Complex z = UnitType.zConversion(new Complex(re,im), busBaseVolt, baseKva, unit, UnitType.PU);
+				} else if (ptype == ParamType.Z) {
+					Complex z = UnitType.zConversion(new Complex(re, im),
+							busBaseVolt, baseKva, unit, UnitType.PU);
 					re = z.getReal();
 					im = z.getImaginary();
 				}
@@ -193,19 +223,21 @@ public class XmlNetParamModifier {
 				im += original.getImaginary();
 			}
 			return new Complex(re, im);
-		}
-		else if (changeRec.getAction() == ChangeActionXmlData.INCREASE ||
-				 changeRec.getAction() == ChangeActionXmlData.DECREASE) {
+		} else if (changeRec.getAction() == ChangeActionXmlData.INCREASE
+				|| changeRec.getAction() == ChangeActionXmlData.DECREASE) {
 			// for increase/decrease, use percent and unit (PU or percent)
 			double factor = changeRec.getPercent();
 			if (changeRec.getUnit() == UnitXmlData.PERCENT)
 				factor *= 0.01;
 			if (changeRec.getAction() == ChangeActionXmlData.DECREASE)
 				factor = -factor;
-			return new Complex(original.getReal()*(1.0+factor), original.getImaginary()*(1.0+factor));
+			return new Complex(original.getReal() * (1.0 + factor), original
+					.getImaginary()
+					* (1.0 + factor));
 		}
 
-		IpssLogger.getLogger().warning("Wrong ChangeAction, changeRec: " + changeRec.toString());
+		IpssLogger.getLogger().warning(
+				"Wrong ChangeAction, changeRec: " + changeRec.toString());
 		return original;
 	}
 }
