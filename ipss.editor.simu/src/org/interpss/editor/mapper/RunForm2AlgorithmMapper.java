ad70fcc372d62f53af6c735475783a730fed154d
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-01-12 18:26:06 +0000

    no code chnage, c-s-F to re-format code

--- ipss.editor.simu/src/org/interpss/editor/mapper/RunForm2AlgorithmMapper.java
+++ ipss.editor.simu/src/org/interpss/editor/mapper/RunForm2AlgorithmMapper.java
@@ -1,26 +1,26 @@
- /*
-  * @(#)RunForm2AlgorithmMapper.java   
-  *
-  * Copyright (C) 2006 www.interpss.org
-  *
-  * This program is free software; you can redistribute it and/or
-  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
-  * as published by the Free Software Foundation; either version 2.1
-  * of the License, or (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * @Author Mike Zhou
-  * @Version 1.0
-  * @Date 09/15/2006
-  * 
-  *   Revision History
-  *   ================
-  *
-  */
+/*
+ * @(#)RunForm2AlgorithmMapper.java   
+ *
+ * Copyright (C) 2006 www.interpss.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @Author Mike Zhou
+ * @Version 1.0
+ * @Date 09/15/2006
+ * 
+ *   Revision History
+ *   ================
+ *
+ */
 
 package org.interpss.editor.mapper;
 
@@ -50,38 +50,39 @@ public class RunForm2AlgorithmMapper extends AbstractMapper {
 	 * @param fromObj a <*>RunForm object
 	 * @param toObj an Algorithm object
 	 * @param kclass class type of the toObj 
-	 */	
+	 */
 	public boolean mapping(Object fromObj, Object toObj, Class<?> klass) {
 		if (klass == LoadflowAlgorithm.class) {
-			AclfRunForm runForm = (AclfRunForm)fromObj;
-	  		LoadflowAlgorithm algo = (LoadflowAlgorithm)toObj;
-			CaseData2AlgorithmMapperImpl.aclfCaseData2AlgoMapping(runForm.getAclfCaseData(), algo);
-		}
-		else if (klass == SimpleFaultAlgorithm.class) {
-			AcscRunForm runForm = (AcscRunForm)fromObj;
-			SimpleFaultAlgorithm algo = (SimpleFaultAlgorithm)toObj;
-			return CaseData2AlgorithmMapperImpl.acscCaseData2AlgoMapping(runForm.getAcscCaseData(), algo);
-		}
-		else if (klass == DynamicSimuAlgorithm.class) {
-			DStabRunForm runForm = (DStabRunForm)fromObj;
-			DynamicSimuAlgorithm algo = (DynamicSimuAlgorithm)toObj;
+			AclfRunForm runForm = (AclfRunForm) fromObj;
+			LoadflowAlgorithm algo = (LoadflowAlgorithm) toObj;
+			CaseData2AlgorithmMapperImpl.aclfCaseData2AlgoMapping(runForm
+					.getAclfCaseData(), algo);
+		} else if (klass == SimpleFaultAlgorithm.class) {
+			AcscRunForm runForm = (AcscRunForm) fromObj;
+			SimpleFaultAlgorithm algo = (SimpleFaultAlgorithm) toObj;
+			return CaseData2AlgorithmMapperImpl.acscCaseData2AlgoMapping(
+					runForm.getAcscCaseData(), algo);
+		} else if (klass == DynamicSimuAlgorithm.class) {
+			DStabRunForm runForm = (DStabRunForm) fromObj;
+			DynamicSimuAlgorithm algo = (DynamicSimuAlgorithm) toObj;
 			return CaseData2AlgorithmMapperImpl.dstabCaseData2AlgoMapping(
-					runForm.getDStabCaseData(), runForm.getAclfCaseData(), algo, msg);
-		}
-		else if (klass == RunAclfStudyCaseXmlType.class) {
+					runForm.getDStabCaseData(), runForm.getAclfCaseData(),
+					algo, msg);
+		} else if (klass == RunAclfStudyCaseXmlType.class) {
 			// map an AclfStudyCase xml record to an LoadflowAlgorithm object
-		  	Xml2AlgorithmMapperImpl.aclfCaseData2AlgoMapping(
-		  			(RunAclfStudyCaseXmlType)fromObj, (LoadflowAlgorithm)toObj);
-		}
-		else if (klass == RunAcscStudyCaseXmlType.class) {
+			Xml2AlgorithmMapperImpl.aclfCaseData2AlgoMapping(
+					(RunAclfStudyCaseXmlType) fromObj,
+					(LoadflowAlgorithm) toObj);
+		} else if (klass == RunAcscStudyCaseXmlType.class) {
 			// map an AcscStudyCase xml record to an LoadflowAlgorithm object
-		  	return Xml2AlgorithmMapperImpl.acscCaseData2AlgoMapping(
-		  			(RunAcscStudyCaseXmlType)fromObj, (SimpleFaultAlgorithm)toObj);
-		}
-		else if (klass == RunDStabStudyCaseXmlType.class) {
+			return Xml2AlgorithmMapperImpl.acscCaseData2AlgoMapping(
+					(RunAcscStudyCaseXmlType) fromObj,
+					(SimpleFaultAlgorithm) toObj);
+		} else if (klass == RunDStabStudyCaseXmlType.class) {
 			// map a DStabStudyCase xml record to an LoadflowAlgorithm object
-		  	return Xml2DStabAlgorithmMapperImpl.dstabCaseData2AlgoMapping(
-		  			(RunDStabStudyCaseXmlType)fromObj, (DynamicSimuAlgorithm)toObj, msg);
+			return Xml2DStabAlgorithmMapperImpl.dstabCaseData2AlgoMapping(
+					(RunDStabStudyCaseXmlType) fromObj,
+					(DynamicSimuAlgorithm) toObj, msg);
 		}
 		return true;
 	}
