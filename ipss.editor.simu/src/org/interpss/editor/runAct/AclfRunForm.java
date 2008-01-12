7c5c455cb49ea61e32317ded719588138d534d4d
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-01-12 18:24:23 +0000

    no code chnage, c-s-F to re-format code

--- ipss.editor.simu/src/org/interpss/editor/runAct/AclfRunForm.java
+++ ipss.editor.simu/src/org/interpss/editor/runAct/AclfRunForm.java
@@ -1,26 +1,26 @@
- /*
-  * @(#)AclfRunForm.java   
-  *
-  * Copyright (C) 2006 www.interpss.org
-  *
-  * This program is free software; you can redistribute it and/or
-  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
-  * as published by the Free Software Foundation; either version 2.1
-  * of the License, or (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * @Author Mike Zhou
-  * @Version 1.0
-  * @Date 09/15/2006
-  * 
-  *   Revision History
-  *   ================
-  *
-  */
+/*
+ * @(#)AclfRunForm.java   
+ *
+ * Copyright (C) 2006 www.interpss.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @Author Mike Zhou
+ * @Version 1.0
+ * @Date 09/15/2006
+ * 
+ *   Revision History
+ *   ================
+ *
+ */
 
 package org.interpss.editor.runAct;
 
@@ -49,101 +49,121 @@ import com.interpss.simu.SimuContext;
 import com.interpss.simu.SimuCtxType;
 
 public class AclfRunForm extends BaseRunForm implements ISimuCaseRunner {
-	public AclfRunForm() {}
+	public AclfRunForm() {
+	}
 
 	private AclfCaseData aclfCaseData;
-	public AclfCaseData getAclfCaseData() {	return this.aclfCaseData; }
-	public void setAclfCaseData(AclfCaseData acase) {this.aclfCaseData = acase;}
-	
-  	public boolean runCase(SimuContext simuCtx, IPSSMsgHub msg) {
-  		boolean converge = false;
-  		if (simuCtx.getNetType() == SimuCtxType.DISTRIBUTE_NET) {
-  			converge = runLoadflow(simuCtx.getDistNet(), simuCtx);
-  		}
-  		else {
-  			if (aclfCaseData.isGridComputing()) {
-  				Grid grid = IpssGridGainUtil.getDefaultGrid();
-  				String nodeId = IpssGridGainUtil.nodeIdLookup(aclfCaseData.getGridNodeName());
-  				AssignJob2NodeDStabTask.RemoteNodeId = nodeId;
-  				AbstractAssignJob2NodeTask.MasterNodeId = grid.getLocalNode().getId().toString();
-  				try {
-  					String str = (String)IpssGridGainUtil.performGridTask(grid,
-  									"InterPSS Grid Aclf Calculation", 
-  									simuCtx.getLoadflowAlgorithm(), 
-  									aclfCaseData.getGridTimeout());
-  	  				AclfAdjNetwork adjNet = (AclfAdjNetwork)SerializeEMFObjectUtil.loadModel(str);
-  	  				simuCtx.setAclfAdjNet(adjNet);
-  	  				converge = adjNet.isLfConverged();
-  	  				if (getAclfCaseData().getShowSummary()) {
-  	  					IOutputTextDialog dialog = UISpringAppContext.getOutputTextDialog("Loadflow Analysis Run by Remote " + aclfCaseData.getGridNodeName());
-  	  					dialog.display(adjNet);
-  	  				}
-  				} catch (GridException e) {
-  					SpringAppContext.getEditorDialogUtil().showErrMsgDialog("Grid Aclf Error", e.toString());
-  					return false;
-  				}
-  			}
-  			else
-  				converge = runLoadflow(simuCtx.getAclfAdjNet(), simuCtx);
-  		}
-  		return converge;
-  	}
-  	
-	public void displaySummaryResult(SimuContext simuCtx) {
-	  	if (getAclfCaseData().getShowSummary()) {
-	  		IOutputTextDialog dialog = UISpringAppContext.getOutputTextDialog("Loadflow Analysis Info");
-	  		dialog.display(simuCtx.getAclfAdjNet());
-	  	}
-	}
-  	
-  	private boolean runLoadflow(DistNetwork distNet, SimuContext simuCtx) {
-  		boolean converge = true;
-  		if (distNet.getLoadNetData().getSchedulePoints() == 0) {
-  			distNet.setNameplateAclfNetData(simuCtx.getMsgHub());
-			converge = runLoadflow_internal(distNet.getAcscNet(), simuCtx.getLoadflowAlgorithm(), simuCtx.getMsgHub());
-		  	
-		  	if (getAclfCaseData().getShowSummary()) {
-		  		IOutputTextDialog dialog = UISpringAppContext.getOutputTextDialog("Loadflow Analysis Info");
-		  		dialog.display(distNet.getAcscNet());
-		  	}
-		}
-  		else {
-  			double loss = 0.0;
-  			for (int i = 0; i < distNet.getLoadNetData().getSchedulePoints(); i++) {
-  				distNet.setPointAclfNetData(i, simuCtx.getMsgHub());
-  				if (!runLoadflow_internal(distNet.getAcscNet(), simuCtx.getLoadflowAlgorithm(), simuCtx.getMsgHub()))
-  					converge = false;
-  				
-  				for( Bus b : distNet.getBusList()) {
-  					DistBus distBus = (DistBus)b;
-  			  		DistBusAdapter aBusApt = (DistBusAdapter)distBus.adapt(DistBusAdapter.class);
-  			  		aBusApt.setPointVoltage(distBus.getAcscBus().getVoltage(), i);
-  				}
-  			}
 
-		  	distNet.getLoadNetData().setTotalLossKwHr(loss);  		
-		  	if (getAclfCaseData().getShowSummary()) {
-		  		IOutputTextDialog dialog = UISpringAppContext.getOutputTextDialog("Distribution Loadflow Analysis Info");
-		  		dialog.display(distNet);
-		  	}
-  		}
-  		return converge; 
-  	}
-  	
-  	private boolean runLoadflow(AclfAdjNetwork aclfAdjNet, SimuContext simuCtx) {
-  	  	boolean converge = runLoadflow_internal(aclfAdjNet, simuCtx.getLoadflowAlgorithm(), simuCtx.getMsgHub());
-	  	if (!converge)
-	  		simuCtx.getMsgHub().sendWarnMsg("Loadflow does not converge!");
-	  	displaySummaryResult(simuCtx);
-  	  	return converge;
-  	}
-  	
-  	private boolean runLoadflow_internal(AclfAdjNetwork aclfAdjNet, LoadflowAlgorithm algo, IPSSMsgHub msg) {
-  		IpssMapper mapper = SimuAppSpringAppContext.getRunForm2AlgorithmMapper();
-  		mapper.mapping(this, algo, LoadflowAlgorithm.class);
+	public AclfCaseData getAclfCaseData() {
+		return this.aclfCaseData;
+	}
+
+	public void setAclfCaseData(AclfCaseData acase) {
+		this.aclfCaseData = acase;
+	}
+
+	public boolean runCase(SimuContext simuCtx, IPSSMsgHub msg) {
+		boolean converge = false;
+		if (simuCtx.getNetType() == SimuCtxType.DISTRIBUTE_NET) {
+			converge = runLoadflow(simuCtx.getDistNet(), simuCtx);
+		} else {
+			if (aclfCaseData.isGridComputing()) {
+				Grid grid = IpssGridGainUtil.getDefaultGrid();
+				String nodeId = IpssGridGainUtil.nodeIdLookup(aclfCaseData
+						.getGridNodeName());
+				AssignJob2NodeDStabTask.RemoteNodeId = nodeId;
+				AbstractAssignJob2NodeTask.MasterNodeId = grid.getLocalNode()
+						.getId().toString();
+				try {
+					String str = (String) IpssGridGainUtil.performGridTask(
+							grid, "InterPSS Grid Aclf Calculation", simuCtx
+									.getLoadflowAlgorithm(), aclfCaseData
+									.getGridTimeout());
+					AclfAdjNetwork adjNet = (AclfAdjNetwork) SerializeEMFObjectUtil
+							.loadModel(str);
+					simuCtx.setAclfAdjNet(adjNet);
+					converge = adjNet.isLfConverged();
+					if (getAclfCaseData().getShowSummary()) {
+						IOutputTextDialog dialog = UISpringAppContext
+								.getOutputTextDialog("Loadflow Analysis Run by Remote "
+										+ aclfCaseData.getGridNodeName());
+						dialog.display(adjNet);
+					}
+				} catch (GridException e) {
+					SpringAppContext.getEditorDialogUtil().showErrMsgDialog(
+							"Grid Aclf Error", e.toString());
+					return false;
+				}
+			} else
+				converge = runLoadflow(simuCtx.getAclfAdjNet(), simuCtx);
+		}
+		return converge;
+	}
+
+	public void displaySummaryResult(SimuContext simuCtx) {
+		if (getAclfCaseData().getShowSummary()) {
+			IOutputTextDialog dialog = UISpringAppContext
+					.getOutputTextDialog("Loadflow Analysis Info");
+			dialog.display(simuCtx.getAclfAdjNet());
+		}
+	}
+
+	private boolean runLoadflow(DistNetwork distNet, SimuContext simuCtx) {
+		boolean converge = true;
+		if (distNet.getLoadNetData().getSchedulePoints() == 0) {
+			distNet.setNameplateAclfNetData(simuCtx.getMsgHub());
+			converge = runLoadflow_internal(distNet.getAcscNet(), simuCtx
+					.getLoadflowAlgorithm(), simuCtx.getMsgHub());
+
+			if (getAclfCaseData().getShowSummary()) {
+				IOutputTextDialog dialog = UISpringAppContext
+						.getOutputTextDialog("Loadflow Analysis Info");
+				dialog.display(distNet.getAcscNet());
+			}
+		} else {
+			double loss = 0.0;
+			for (int i = 0; i < distNet.getLoadNetData().getSchedulePoints(); i++) {
+				distNet.setPointAclfNetData(i, simuCtx.getMsgHub());
+				if (!runLoadflow_internal(distNet.getAcscNet(), simuCtx
+						.getLoadflowAlgorithm(), simuCtx.getMsgHub()))
+					converge = false;
+
+				for (Bus b : distNet.getBusList()) {
+					DistBus distBus = (DistBus) b;
+					DistBusAdapter aBusApt = (DistBusAdapter) distBus
+							.adapt(DistBusAdapter.class);
+					aBusApt.setPointVoltage(distBus.getAcscBus().getVoltage(),
+							i);
+				}
+			}
+
+			distNet.getLoadNetData().setTotalLossKwHr(loss);
+			if (getAclfCaseData().getShowSummary()) {
+				IOutputTextDialog dialog = UISpringAppContext
+						.getOutputTextDialog("Distribution Loadflow Analysis Info");
+				dialog.display(distNet);
+			}
+		}
+		return converge;
+	}
+
+	private boolean runLoadflow(AclfAdjNetwork aclfAdjNet, SimuContext simuCtx) {
+		boolean converge = runLoadflow_internal(aclfAdjNet, simuCtx
+				.getLoadflowAlgorithm(), simuCtx.getMsgHub());
+		if (!converge)
+			simuCtx.getMsgHub().sendWarnMsg("Loadflow does not converge!");
+		displaySummaryResult(simuCtx);
+		return converge;
+	}
+
+	private boolean runLoadflow_internal(AclfAdjNetwork aclfAdjNet,
+			LoadflowAlgorithm algo, IPSSMsgHub msg) {
+		IpssMapper mapper = SimuAppSpringAppContext
+				.getRunForm2AlgorithmMapper();
+		mapper.mapping(this, algo, LoadflowAlgorithm.class);
 
 		algo.loadflow(msg);
 
-	  	return aclfAdjNet.isLfConverged();
-  	}
+		return aclfAdjNet.isLfConverged();
+	}
 }
--- ipss.editor.simu/src/org/interpss/editor/runAct/RunActUtilFunc.java
+++ ipss.editor.simu/src/org/interpss/editor/runAct/RunActUtilFunc.java
@@ -1,26 +1,26 @@
- /*
-  * @(#)RunActUtilFunc.java   
-  *
-  * Copyright (C) 2006 www.interpss.org
-  *
-  * This program is free software; you can redistribute it and/or
-  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
-  * as published by the Free Software Foundation; either version 2.1
-  * of the License, or (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * @Author Mike Zhou
-  * @Version 1.0
-  * @Date 09/15/2006
-  * 
-  *   Revision History
-  *   ================
-  *
-  */
+/*
+ * @(#)RunActUtilFunc.java   
+ *
+ * Copyright (C) 2006 www.interpss.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @Author Mike Zhou
+ * @Version 1.0
+ * @Date 09/15/2006
+ * 
+ *   Revision History
+ *   ================
+ *
+ */
 
 package org.interpss.editor.runAct;
 
@@ -54,140 +54,159 @@ import com.interpss.dstab.util.IDStabSimuDatabaseOutputHandler;
 
 public class RunActUtilFunc {
 	public static String AllControlDevices = "All Control Devices";
-	
-	public static Object[] getFunctionLoadList(AclfAdjNetwork adjNet, double tolerance, IPSSMsgHub msg) {
+
+	public static Object[] getFunctionLoadList(AclfAdjNetwork adjNet,
+			double tolerance, IPSSMsgHub msg) {
 		List<String> list = new ArrayList<String>();
 		list.add(AllControlDevices);
 		for (int i = 0; i < adjNet.getFunctionLoadList().size(); i++) {
-			FunctionLoad load = (FunctionLoad)adjNet.getFunctionLoadList().get(i);
+			FunctionLoad load = (FunctionLoad) adjNet.getFunctionLoadList()
+					.get(i);
 			if (load.needAdjust(tolerance, adjNet.getBaseKva(), msg))
-				list.add( load.getId() + " at " + load.getBus().getName());
+				list.add(load.getId() + " at " + load.getBus().getName());
 		}
 		return list.toArray();
 	}
 
-	public static Object[] getPVBusLimitList(AclfAdjNetwork adjNet, IPSSMsgHub msg) {
+	public static Object[] getPVBusLimitList(AclfAdjNetwork adjNet,
+			IPSSMsgHub msg) {
 		List<String> list = new ArrayList<String>();
 		list.add(AllControlDevices);
 		for (int i = 0; i < adjNet.getPvBusLimitList().size(); i++) {
-			PVBusLimit pvLimit = (PVBusLimit)adjNet.getPvBusLimitList().get(i);
+			PVBusLimit pvLimit = (PVBusLimit) adjNet.getPvBusLimitList().get(i);
 			if (pvLimit.needAdjust(0.0, adjNet.getBaseKva(), msg))
-				list.add( pvLimit.getId() + " at " + pvLimit.getBus().getName() +
-					"(" + (pvLimit.isActive()?"on":"off") + ")");
-		}
-		return list.toArray();	
-	}
-
-	public static Object[] getPQBusLimitList(AclfAdjNetwork adjNet, IPSSMsgHub msg) {
-		List<String> list = new ArrayList<String>();
-		list.add(AllControlDevices);
-		for (int i = 0; i < adjNet.getPqBusLimitList().size(); i++) {
-			PQBusLimit pqLimit = (PQBusLimit)adjNet.getPqBusLimitList().get(i);
-			if (pqLimit.needAdjust(0.0, adjNet.getBaseKva(), msg))
-				list.add( pqLimit.getId() + " at " + pqLimit.getBus().getName() +
-					"(" + (pqLimit.isActive()?"on":"off") + ")");
-		}
-		return list.toArray();	
-	}
-
-	public static Object[] getRemoteQBusList(AclfAdjNetwork adjNet, double tolerance, IPSSMsgHub msg) {
-		List<String> list = new ArrayList<String>();
-		list.add(AllControlDevices);
-		for (int i = 0; i < adjNet.getRemoteQBusList().size(); i++) {
-			RemoteQBus reQ = (RemoteQBus)adjNet.getRemoteQBusList().get(i);
-			if (reQ.needAdjust(tolerance, adjNet.getBaseKva(), msg)) {
-				if (reQ.getControlType() == RemoteQControlType.BUS_VOLTAGE)
-					list.add( reQ.getId() + " at " + reQ.getBus().getName() +
-							"-> Bus:" + reQ.getRemoteBus().getId());
-				else
-					list.add( reQ.getId() + " at " + reQ.getBus().getName() +
-							"-> Branch:" + reQ.getRemoteBranch().getId());
-			}	
+				list.add(pvLimit.getId() + " at " + pvLimit.getBus().getName()
+						+ "(" + (pvLimit.isActive() ? "on" : "off") + ")");
 		}
 		return list.toArray();
 	}
 
-	public static Object[] getXfrTapControlList(AclfAdjNetwork adjNet, double tolerance, IPSSMsgHub msg) {
+	public static Object[] getPQBusLimitList(AclfAdjNetwork adjNet,
+			IPSSMsgHub msg) {
+		List<String> list = new ArrayList<String>();
+		list.add(AllControlDevices);
+		for (int i = 0; i < adjNet.getPqBusLimitList().size(); i++) {
+			PQBusLimit pqLimit = (PQBusLimit) adjNet.getPqBusLimitList().get(i);
+			if (pqLimit.needAdjust(0.0, adjNet.getBaseKva(), msg))
+				list.add(pqLimit.getId() + " at " + pqLimit.getBus().getName()
+						+ "(" + (pqLimit.isActive() ? "on" : "off") + ")");
+		}
+		return list.toArray();
+	}
+
+	public static Object[] getRemoteQBusList(AclfAdjNetwork adjNet,
+			double tolerance, IPSSMsgHub msg) {
+		List<String> list = new ArrayList<String>();
+		list.add(AllControlDevices);
+		for (int i = 0; i < adjNet.getRemoteQBusList().size(); i++) {
+			RemoteQBus reQ = (RemoteQBus) adjNet.getRemoteQBusList().get(i);
+			if (reQ.needAdjust(tolerance, adjNet.getBaseKva(), msg)) {
+				if (reQ.getControlType() == RemoteQControlType.BUS_VOLTAGE)
+					list.add(reQ.getId() + " at " + reQ.getBus().getName()
+							+ "-> Bus:" + reQ.getRemoteBus().getId());
+				else
+					list.add(reQ.getId() + " at " + reQ.getBus().getName()
+							+ "-> Branch:" + reQ.getRemoteBranch().getId());
+			}
+		}
+		return list.toArray();
+	}
+
+	public static Object[] getXfrTapControlList(AclfAdjNetwork adjNet,
+			double tolerance, IPSSMsgHub msg) {
 		List<String> list = new ArrayList<String>();
 		list.add(AllControlDevices);
 		for (int i = 0; i < adjNet.getTapControlList().size(); i++) {
-			TapControl xfr = (TapControl)adjNet.getTapControlList().get(i);
+			TapControl xfr = (TapControl) adjNet.getTapControlList().get(i);
 			if (xfr.needAdjust(tolerance, adjNet.getBaseKva(), msg)) {
-				list.add( xfr.getId() + " at " + xfr.getAclfBranch().getName());
-			}	
+				list.add(xfr.getId() + " at " + xfr.getAclfBranch().getName());
+			}
 		}
-		return list.toArray();	
+		return list.toArray();
 	}
 
-	public static Object[] getPSXfrPControlList(AclfAdjNetwork adjNet, double tolerance, IPSSMsgHub msg) {
+	public static Object[] getPSXfrPControlList(AclfAdjNetwork adjNet,
+			double tolerance, IPSSMsgHub msg) {
 		List<String> list = new ArrayList<String>();
 		list.add(AllControlDevices);
 		for (int i = 0; i < adjNet.getPsXfrPControlList().size(); i++) {
-			PSXfrPControl psXfr = (PSXfrPControl)adjNet.getPsXfrPControlList().get(i);
+			PSXfrPControl psXfr = (PSXfrPControl) adjNet.getPsXfrPControlList()
+					.get(i);
 			if (psXfr.needAdjust(tolerance, adjNet.getBaseKva(), msg)) {
-				list.add( psXfr.getId() + " at " + psXfr.getAclfBranch().getName());
-			}	
+				list.add(psXfr.getId() + " at "
+						+ psXfr.getAclfBranch().getName());
+			}
 		}
-		return list.toArray();	
+		return list.toArray();
 	}
 
-	public static Object[] getInterareaPControlList(AclfAdjNetwork adjNet, IPSSMsgHub msg) {
+	public static Object[] getInterareaPControlList(AclfAdjNetwork adjNet,
+			IPSSMsgHub msg) {
 		List<String> list = new ArrayList<String>();
 		list.add(AllControlDevices);
 		for (Object obj : adjNet.getAreaList()) {
-			Area a = (Area)obj;
+			Area a = (Area) obj;
 			if (a.getRegDeviceList().size() > 0) {
-				IRegulationDevice regDevice = (IRegulationDevice)a.getRegDeviceList().get(0);
+				IRegulationDevice regDevice = (IRegulationDevice) a
+						.getRegDeviceList().get(0);
 				if (regDevice.needAdjustment(a, adjNet, msg))
-					list.add( a.getNumber() + " name: " + a.getName());
+					list.add(a.getNumber() + " name: " + a.getName());
 			}
-		}		
-		return list.toArray();	
+		}
+		return list.toArray();
 	}
-	
+
 	/**
 	 * display fault analysis summary
 	 * 
 	 * @parem faultNet
 	 */
 	public static void displayAcscSummaryResult(SimpleFaultNetwork faultNet) {
-  		IOutputTextDialog dialog = UISpringAppContext.getOutputTextDialog("Short Circuit Analysis Result Summary");
-  		dialog.display(AcscOutFunc.faultResult2String(faultNet));
+		IOutputTextDialog dialog = UISpringAppContext
+				.getOutputTextDialog("Short Circuit Analysis Result Summary");
+		dialog.display(AcscOutFunc.faultResult2String(faultNet));
 	}
-	
+
 	/**
 	 * Display Aclf summary if selected by the user
 	 * 
 	 * @param algo
 	 */
 	public static void displayAclfSummaryResult(DynamicSimuAlgorithm algo) {
-  		IOutputTextDialog dialog = UISpringAppContext.getOutputTextDialog("Loadflow Analysis Info");
-  		dialog.display(algo);
-	}	
-	
+		IOutputTextDialog dialog = UISpringAppContext
+				.getOutputTextDialog("Loadflow Analysis Info");
+		dialog.display(algo);
+	}
+
 	/**
-	 * Create a DB output handler object for UI based DStab run. 
+	 * Create a DB output handler object for UI based DStab run.
 	 * 
 	 * @param algo
 	 * @return
 	 */
-	public static IDStabSimuDatabaseOutputHandler createDBOutputHandler(DynamicSimuAlgorithm algo) {
+	public static IDStabSimuDatabaseOutputHandler createDBOutputHandler(
+			DynamicSimuAlgorithm algo) {
 		return createDBOutputHandler(algo, null);
 	}
-	
+
 	/**
-	 * Create a DB output handler object. If dstabXmlCase != null, it RecId will be appended to the db case name. 
+	 * Create a DB output handler object. If dstabXmlCase != null, it RecId will
+	 * be appended to the db case name.
 	 * 
 	 * @param algo
 	 * @param dstabXmlCase
 	 * @return
 	 */
-	public static IDStabSimuDatabaseOutputHandler createDBOutputHandler(DynamicSimuAlgorithm algo, RunDStabStudyCaseXmlType dstabXmlCase) {
-		IDStabSimuDatabaseOutputHandler handler = (IDStabSimuDatabaseOutputHandler)algo.getSimuOutputHandler();
-		IAppSimuContext appSimuCtx = GraphSpringAppContext.getIpssGraphicEditor().getCurrentAppSimuContext();
-		ProjData projData = (ProjData)appSimuCtx.getProjData();
+	public static IDStabSimuDatabaseOutputHandler createDBOutputHandler(
+			DynamicSimuAlgorithm algo, RunDStabStudyCaseXmlType dstabXmlCase) {
+		IDStabSimuDatabaseOutputHandler handler = (IDStabSimuDatabaseOutputHandler) algo
+				.getSimuOutputHandler();
+		IAppSimuContext appSimuCtx = GraphSpringAppContext
+				.getIpssGraphicEditor().getCurrentAppSimuContext();
+		ProjData projData = (ProjData) appSimuCtx.getProjData();
 		try {
-			// to avoid conflict with StudyCase name, we add " SimuRecord" to the SimuRecord case.
+			// to avoid conflict with StudyCase name, we add " SimuRecord" to
+			// the SimuRecord case.
 			String casename = "SimuRecord_";
 			if (dstabXmlCase != null)
 				casename += dstabXmlCase.getRecId();
@@ -197,7 +216,8 @@ public class RunActUtilFunc {
 				return null;
 		} catch (Exception e) {
 			IpssLogger.logErr(e);
-			SpringAppContext.getEditorDialogUtil().showErrMsgDialog("Error to Create DB SimuRecord", 
+			SpringAppContext.getEditorDialogUtil().showErrMsgDialog(
+					"Error to Create DB SimuRecord",
 					e.toString() + "\nPlease contact InterPSS support");
 		}
 		appSimuCtx.setDbSimuCaseId(handler.getDBCaseId());
@@ -211,17 +231,20 @@ public class RunActUtilFunc {
 	 * @param msg
 	 * @return
 	 */
-	public static boolean checkDStabSimuData(DynamicSimuAlgorithm algo, IPSSMsgHub msg) {
+	public static boolean checkDStabSimuData(DynamicSimuAlgorithm algo,
+			IPSSMsgHub msg) {
 		if (!algo.checkData(msg)) {
-			IpssLogger.getLogger().warning("DStab simulation data checking failed");
+			IpssLogger.getLogger().warning(
+					"DStab simulation data checking failed");
 			return false;
 		}
 
 		// dstab net data changed in the mapping process
 		if (!algo.getDStabNet().checkData(msg)) {
-			IpssLogger.getLogger().warning("DStab network data checking failed");
+			IpssLogger.getLogger()
+					.warning("DStab network data checking failed");
 			return false;
-		}		
+		}
 		return true;
 	}
 }
