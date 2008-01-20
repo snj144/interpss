cea6cd333d815ac0b3ce1df9c18d30745a723683
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-01-20 02:30:52 +0000

    create DclfOutFunc class

--- ipss.plugin/src/org/interpss/display/DclfOutFunc.java
+++ ipss.plugin/src/org/interpss/display/DclfOutFunc.java
@@ -1,5 +1,23 @@
 package org.interpss.display;
 
+import com.interpss.common.datatype.Constants;
+import com.interpss.common.util.Number2String;
+import com.interpss.core.dclf.DclfAlgorithm;
+import com.interpss.core.net.Bus;
+import com.interpss.core.net.Network;
+
 
 public class DclfOutFunc {
+	public static String dclfResults(Network net, DclfAlgorithm algo) {
+		String str = "BudId          VoltAng(deg)\n";
+		str += "=================================\n";
+		for (Bus bus : net.getBusList()) {
+			int n = bus.getSortNumber();
+			double angle = algo.getB1PAngleMatrix().getBi(n);
+			str += Number2String.toFixLengthStr(8, bus.getId())
+					+ "        "
+					+ Number2String.toStr(angle * Constants.RtoD) + "\n";
+		}
+		return str;
+	}
 }
