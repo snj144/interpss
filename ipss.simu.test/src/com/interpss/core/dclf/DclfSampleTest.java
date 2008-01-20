7ddc7acc76d7cfc98bb72ab61a5b0650a192ea7d
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-01-20 20:10:35 +0000

    update DclfAlgo interface

--- ipss.simu.test/src/com/interpss/core/dclf/DclfSampleTest.java
+++ ipss.simu.test/src/com/interpss/core/dclf/DclfSampleTest.java
@@ -28,13 +28,10 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import com.interpss.common.SpringAppContext;
-import com.interpss.common.datatype.Constants;
-import com.interpss.common.util.Number2String;
 import com.interpss.BaseTestSetup;
+import com.interpss.common.SpringAppContext;
 import com.interpss.core.CoreObjectFactory;
 import com.interpss.core.aclf.AclfNetwork;
-import com.interpss.core.net.Bus;
 import com.interpss.core.util.sample.SampleCases;
 
 public class DclfSampleTest extends BaseTestSetup {
@@ -49,26 +46,24 @@ public class DclfSampleTest extends BaseTestSetup {
 
 		DclfAlgorithm algo = CoreObjectFactory.createDclfAlgorithm(net);
 		if (algo.checkCondition(SpringAppContext.getIpssMsgHub()))
-			algo.calculateAngle(SpringAppContext.getIpssMsgHub());
-
-		System.out.println(algo.getB1PAngleMatrix());
-
+			algo.calculateDclf(SpringAppContext.getIpssMsgHub());
+/*
 		for (Bus bus : net.getBusList()) {
 			int n = bus.getSortNumber();
 			System.out.println("Bus id, dAng : "
 					+ bus.getId()
 					+ ", "
-					+ Number2String.toStr(algo.getB1PAngleMatrix().getBi(n)
+					+ Number2String.toStr(algo.getBusAngle(n)
 							* Constants.RtoD));
 		}
-
-		assertTrue(Math.abs(algo.getB1PAngleMatrix().getBi(1) - 0.41933) < 0.0001);
-		assertTrue(Math.abs(algo.getB1PAngleMatrix().getBi(2)) < 0.0001);
-		assertTrue(Math.abs(algo.getB1PAngleMatrix().getBi(3) + 0.06122) < 0.0001);
-		assertTrue(Math.abs(algo.getB1PAngleMatrix().getBi(4) - 0.34433) < 0.0001);
-		assertTrue(Math.abs(algo.getB1PAngleMatrix().getBi(5) + 0.069) < 0.0001);
+*/
+		assertTrue(Math.abs(algo.getBusAngle(1) - 0.41933) < 0.0001);
+		assertTrue(Math.abs(algo.getBusAngle(2)) < 0.0001);
+		assertTrue(Math.abs(algo.getBusAngle(3) + 0.06122) < 0.0001);
+		assertTrue(Math.abs(algo.getBusAngle(4) - 0.34433) < 0.0001);
+		assertTrue(Math.abs(algo.getBusAngle(5) + 0.069) < 0.0001);
 		
-		algo.sentivities(SensitivityCalType.PANGLE, "3", SpringAppContext.getIpssMsgHub());
+		algo.calculateSensitivity(DclfSensitivityType.PANGLE, "3", SpringAppContext.getIpssMsgHub());
 //		System.out.println(algo.getB1PAngleMatrix());
 /*
 b(1): -0.030000000000000034
@@ -77,13 +72,17 @@ b(3): -0.03000000000000002
 b(4): -0.030000000000000034
 b(5): -0.030000000000000006
  */
-		assertTrue(Math.abs(algo.getB1PAngleMatrix().getBi(1) + 0.03) < 0.0001);
-		assertTrue(Math.abs(algo.getB1PAngleMatrix().getBi(2)) < 0.0001);
-		assertTrue(Math.abs(algo.getB1PAngleMatrix().getBi(3) + 0.03) < 0.0001);
-		assertTrue(Math.abs(algo.getB1PAngleMatrix().getBi(4) + 0.03) < 0.0001);
-		assertTrue(Math.abs(algo.getB1PAngleMatrix().getBi(5) + 0.03) < 0.0001);
+/*
+		for (int i = 1; i <= 5; i++)
+			System.out.println(i + ", " + algo.getBusSensitivity(DclfSensitivityType.PANGLE,i));
+*/		
+		assertTrue(Math.abs(algo.getBusSensitivity(DclfSensitivityType.PANGLE,1) + 0.03) < 0.0001);
+		assertTrue(Math.abs(algo.getBusSensitivity(DclfSensitivityType.PANGLE,2)) < 0.0001);
+		assertTrue(Math.abs(algo.getBusSensitivity(DclfSensitivityType.PANGLE,3) + 0.03) < 0.0001);
+		assertTrue(Math.abs(algo.getBusSensitivity(DclfSensitivityType.PANGLE,4) + 0.03) < 0.0001);
+		assertTrue(Math.abs(algo.getBusSensitivity(DclfSensitivityType.PANGLE,5) + 0.03) < 0.0001);
 
-		algo.sentivities(SensitivityCalType.QVOLTAGE, "3", SpringAppContext.getIpssMsgHub());
+		algo.calculateSensitivity(DclfSensitivityType.QVOLTAGE, "3", SpringAppContext.getIpssMsgHub());
 //		System.out.println(algo.getB11QVoltageMatrix());
 		/*
 b(1): -0.0
@@ -92,10 +91,14 @@ b(3): -0.01311612109013261
 b(4): -0.002126974748964943
 b(5): -0.02934145973738819
 		 */
-		assertTrue(Math.abs(algo.getB11QVoltageMatrix().getBi(1)) < 0.0001);
-		assertTrue(Math.abs(algo.getB11QVoltageMatrix().getBi(2)) < 0.0001);
-		assertTrue(Math.abs(algo.getB11QVoltageMatrix().getBi(3) + 0.013116) < 0.0001);
-		assertTrue(Math.abs(algo.getB11QVoltageMatrix().getBi(4) + 0.002127) < 0.0001);
-		assertTrue(Math.abs(algo.getB11QVoltageMatrix().getBi(5) + 0.029342) < 0.0001);
+/*		
+		for (int i = 1; i <= 5; i++)
+			System.out.println(i + ", " + algo.getBusSensitivity(DclfSensitivityType.QVOLTAGE,i));
+*/
+		assertTrue(Math.abs(algo.getBusSensitivity(DclfSensitivityType.QVOLTAGE,1)) < 0.0001);
+		assertTrue(Math.abs(algo.getBusSensitivity(DclfSensitivityType.QVOLTAGE,2)) < 0.0001);
+		assertTrue(Math.abs(algo.getBusSensitivity(DclfSensitivityType.QVOLTAGE,3) + 0.013116) < 0.0001);
+		assertTrue(Math.abs(algo.getBusSensitivity(DclfSensitivityType.QVOLTAGE,4) + 0.002127) < 0.0001);
+		assertTrue(Math.abs(algo.getBusSensitivity(DclfSensitivityType.QVOLTAGE,5) + 0.029342) < 0.0001);
 	}
 }
