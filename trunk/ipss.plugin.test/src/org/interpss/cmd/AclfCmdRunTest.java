4a2fb78dd92b022b4f92dd547b9ae383d9a86aee
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-01-28 21:05:40 +0000

    add cmd run test case

---
+++ trunk/ipss.plugin.test/src/org/interpss/cmd/AclfCmdRunTest.java
@@ -0,0 +1,26 @@
+package org.interpss.cmd;
+
+import static org.junit.Assert.assertTrue;
+
+import org.interpss.BaseTestSetup;
+import org.interpss.InterPSS;
+import org.junit.Test;
+
+import com.interpss.simu.SimuContext;
+
+public class AclfCmdRunTest  extends BaseTestSetup {
+	@Test
+	public void runIEEE14Aclf() throws Exception {
+		SimuContext simuCtx = (SimuContext)CmdLineRunner.cmdLineRun(
+				"testData/ipssdata/Ieee14.ipssdat");
+		assertTrue(simuCtx.getAclfAdjNet().isLfConverged());
+	}			
+
+	@Test
+	public void runIEEE14Dclfr() throws Exception {
+		CmdLineRunner.cmdLineRun(
+				"testData/ipssdata/Ieee14.ipssdat", 
+				InterPSS.RunDclfStr,
+				null);
+	}			
+}
