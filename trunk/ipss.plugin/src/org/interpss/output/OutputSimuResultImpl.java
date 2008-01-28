c1cb3e5c5f5185f7dafab3086786187456be5423
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-01-28 20:55:01 +0000

    added SimuResultOutput spring bean

---
+++ trunk/ipss.plugin/src/org/interpss/output/OutputSimuResultImpl.java
@@ -0,0 +1,53 @@
+ /*
+  * @(#)OutputSimuResultImpl.java   
+  *
+  * Copyright (C) 2006 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 11/27/2007
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
+package org.interpss.output;
+
+import org.interpss.display.AclfOutFunc;
+import org.interpss.display.DclfOutFunc;
+
+import com.interpss.core.aclf.AclfNetwork;
+import com.interpss.core.aclfadj.AclfAdjNetwork;
+import com.interpss.core.dclf.DclfAlgorithm;
+
+public class OutputSimuResultImpl implements IOutputSimuResult {
+
+	@Override
+	public boolean outAclfResult(AclfNetwork net, String outFilename) {
+	  	System.out.println(AclfOutFunc.loadFlowSummary(net));
+		return false;
+	}
+
+	@Override
+	public boolean outAclfResult(AclfAdjNetwork net, String outFilename) {
+	  	System.out.println(AclfOutFunc.loadFlowSummary(net));
+		return true;
+	}
+
+	@Override
+	public boolean outDclfResult(DclfAlgorithm algo, String outFilename) {
+	  	System.out.println(DclfOutFunc.dclfResults(algo));
+		return true;
+	}
+}
