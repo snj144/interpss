9926c8184311b0d2d4797739188123877706907e
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-02-04 00:46:02 +0000

    added PluginSpringContext.xml

--- trunk/ipss/src/org/interpss/cmd/CmdLineRunner.java
+++ trunk/ipss/src/org/interpss/cmd/CmdLineRunner.java
@@ -24,13 +24,24 @@
 
 package org.interpss.cmd;
 
+import java.io.File;
+
 import org.interpss.AppConstants;
 import org.interpss.InterPSS;
-import org.interpss.editor.SimuAppSpringAppContext;
-import org.interpss.editor.SimuAppSpringAppCtxUtil;
+import org.interpss.PluginSpringAppContext;
+import org.interpss.editor.runAct.xml.XmlScriptAclfRun;
+import org.interpss.editor.runAct.xml.XmlScriptAcscRun;
+import org.interpss.editor.runAct.xml.XmlScriptDStabRun;
+import org.interpss.editor.runAct.xml.XmlScriptDclfRun;
+import org.interpss.gridgain.util.IpssGridGainUtil;
 import org.interpss.output.IOutputSimuResult;
+import org.interpss.schema.AnalysisRunTypeXmlData;
+import org.interpss.schema.RunStudyCaseXmlType;
+import org.interpss.xml.IpssXmlParser;
+import org.interpss.xml.XmlNetParamModifier;
 
 import com.interpss.common.SpringAppContext;
+import com.interpss.common.datatype.SimuRunType;
 import com.interpss.common.msg.IPSSMsgHub;
 import com.interpss.common.util.IpssLogger;
 import com.interpss.common.util.StringUtil;
@@ -42,24 +53,26 @@ import com.interpss.simu.SimuCtxType;
 import com.interpss.simu.io.IpssFileAdapter;
 
 public class CmdLineRunner {
+	private static SimuRunType runType = SimuRunType.NotDefined;
+	
 	/**
 	 * Run InterPSS simulation in cmd line mode
 	 * 
 	 * @param inputFilename input file name. Its extension will be used to determine input file load adapter
-	 * @param runType Dclf|Aclf|Acsc|DStab to overide the default run type, which is decided by the input data network type
+	 * @param runTypeStr Dclf|Aclf|Acsc|DStab to overide the default run type, which is decided by the input data network type
 	 * @param xmlContrlFile an xml file to control interpss run
 	 * @param outFilename output file name
 	 * @return returning a object (Net, SimuCtx) for testing purpose
 	 */
 	public static Object cmdLineRun(
 			String inputFilename, 
-			String runType, 
+			String runTypeStr, 
 			String xmlControlFile,
 			String outFilename) {
 
 		SimuContext simuCtx = inputData(inputFilename);
 		if (simuCtx != null) {
-			if (runStudyCase(simuCtx, runType)) {
+			if (runStudyCase(simuCtx, runTypeStr, xmlControlFile)) {
 				String f = outFilename;
 				if (f == null || f.trim().equals("")) {
 					String str = StringUtil.getFileNameNoExt(inputFilename);
@@ -91,7 +104,7 @@ public class CmdLineRunner {
 			}
 			
 			String ext = StringUtil.getFileExt(filename);
-			IpssFileAdapter adapter = SimuAppSpringAppCtxUtil
+			IpssFileAdapter adapter = PluginSpringAppContext
 					.getCustomFileAdapter(ext);
 			if (adapter == null) {
 				IpssLogger.getLogger().severe("Wrong file type, no adapter is available, ext: " + ext);
@@ -106,23 +119,65 @@ public class CmdLineRunner {
 		}
 	}
 
-	private static boolean runStudyCase(SimuContext simuCtx, String runType) {
+	private static boolean runStudyCase(SimuContext simuCtx, String runTypeStr, String xmlControlFile) {
 		IPSSMsgHub msg = SpringAppContext.getIpssMsgHub();
-		if (runType != null && !runType.equals("")) {
-			if (InterPSS.RunAclfStr.equals(runType)) {
+		if (xmlControlFile != null && !xmlControlFile.equals("")) {
+			IpssLogger.getLogger().info("Run CmdLine using xml control file, " + xmlControlFile);
+		
+			IpssXmlParser parser;
+			try {
+				parser = new IpssXmlParser(new File(xmlControlFile));
+			} catch (Exception e) {
+				IpssLogger.logErr(e);
+				msg.sendErrorMsg("Error to load control Xml file, " + e.toString());
+				return false;
+			}
+			
+			// Apply the modification to the base Network object
+			if (parser.getModification() != null)
+				XmlNetParamModifier.applyModification2Net(simuCtx.getNetwork(),
+						parser.getModification());
+
+			RunStudyCaseXmlType xmlStudyCase = parser.getRunStudyCase();
+			IpssGridGainUtil.RemoteNodeDebug = xmlStudyCase.getGridRun() != null
+					&& xmlStudyCase.getGridRun().getRemoteNodeDebug();
+			if (xmlStudyCase.getAnalysisRunType() == AnalysisRunTypeXmlData.RUN_ACLF) {
+				runType = SimuRunType.Aclf;
+				return XmlScriptAclfRun.runAclf(parser, simuCtx.getAclfAdjNet(),
+						msg);
+			} else if (xmlStudyCase.getAnalysisRunType() == AnalysisRunTypeXmlData.RUN_DCLF) {
+				runType = SimuRunType.Dclf;
+				return XmlScriptDclfRun.runDclf(parser, simuCtx.getAclfNet(),
+						msg);
+			} else if (xmlStudyCase.getAnalysisRunType() == AnalysisRunTypeXmlData.RUN_ACSC) {
+				runType = SimuRunType.Acsc;
+				return XmlScriptAcscRun.runAcsc(parser, simuCtx.getAcscFaultNet(),
+						msg);
+			} else if (xmlStudyCase.getAnalysisRunType() == AnalysisRunTypeXmlData.RUN_D_STAB) {
+				runType = SimuRunType.DStab;
+				return XmlScriptDStabRun.runDStab(parser, simuCtx, msg);
+			}
+			return true;			
+		}
+		else if (runTypeStr != null && !runTypeStr.equals("")) {
+			IpssLogger.getLogger().info("Run CmdLine, runType = " + runTypeStr);
+			if (InterPSS.RunAclfStr.equals(runTypeStr)) {
 				// create the default loadflow algorithm
 				LoadflowAlgorithm algo = CoreObjectFactory
 						.createLoadflowAlgorithm(simuCtx.getAclfAdjNet());
 				// use the loadflow algorithm to perform loadflow calculation
 				algo.loadflow(msg);
-			} else if (runType != null && InterPSS.RunDclfStr.equals(runType)) {
+				runType = SimuRunType.Aclf;
+			} else if (runTypeStr != null && InterPSS.RunDclfStr.equals(runTypeStr)) {
 				DclfAlgorithm algo = CoreObjectFactory
 						.createDclfAlgorithm(simuCtx.getAclfAdjNet());
 				simuCtx.setDclfAlgorithm(algo);
 				if (algo.checkCondition(msg))
 					algo.calculateDclf(msg);
+				runType = SimuRunType.Dclf;
 			}
 		} else {
+			IpssLogger.getLogger().info("Run CmdLine according network type");
 			if (simuCtx.getNetType() == SimuCtxType.ACLF_NETWORK
 					|| simuCtx.getNetType() == SimuCtxType.ACLF_ADJ_NETWORK) {
 				// create the default loadflow algorithm
@@ -130,6 +185,7 @@ public class CmdLineRunner {
 						.createLoadflowAlgorithm(simuCtx.getAclfAdjNet());
 				// use the loadflow algorithm to perform loadflow calculation
 				algo.loadflow(msg);
+				runType = SimuRunType.Aclf;
 			}
 			else {
 				IpssLogger.getLogger().warning("InterPSS CmdLine mode currently only supput Dclf and Aclf run type");
@@ -139,16 +195,17 @@ public class CmdLineRunner {
 		return true;
 	}
 
-	private static void outputResult(SimuContext simuCtx, String runType,
+	private static void outputResult(SimuContext simuCtx, SimuRunType runType,
 			String outFilename) {
-		IOutputSimuResult out = SimuAppSpringAppContext.getSimuResultOutput();
-		if (runType != null && !runType.equals("")) {
+		IOutputSimuResult out = PluginSpringAppContext.getSimuResultOutput();
+		if (runType == SimuRunType.Dclf) {
 			if (InterPSS.RunAclfStr.equals(runType)) {
 				out.outAclfResult(simuCtx.getAclfAdjNet(), outFilename);
 			} else if (runType != null && InterPSS.RunDclfStr.equals(runType)) {
 				out.outDclfResult(simuCtx.getDclfAlgorithm(), outFilename);
 			}
-		} else {
+		} 
+		else if (runType == SimuRunType.Aclf) {
 			if (simuCtx.getNetType() == SimuCtxType.ACLF_NETWORK
 					|| simuCtx.getNetType() == SimuCtxType.ACLF_ADJ_NETWORK) {
 				out.outAclfResult(simuCtx.getAclfAdjNet(), outFilename);
