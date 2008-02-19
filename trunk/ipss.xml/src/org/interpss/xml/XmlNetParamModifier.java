41af7b37fbb48e5acf606dbfcef7bd659902f092
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-02-19 18:45:52 +0000

    swingVoltageChange schema changed

--- trunk/ipss.xml/src/org/interpss/xml/XmlNetParamModifier.java
+++ trunk/ipss.xml/src/org/interpss/xml/XmlNetParamModifier.java
@@ -30,22 +30,30 @@ import org.interpss.schema.BusRecXmlType;
 import org.interpss.schema.ComplexValueChangeXmlType;
 import org.interpss.schema.ComplexXmlType;
 import org.interpss.schema.ModificationXmlType;
+import org.interpss.schema.ValueChangeXmlType;
+import org.interpss.schema.ModificationXmlType.BusChangeRecList.BusChangeRec.AclfBusChangeData.LoadChangeData;
 import org.interpss.schema.UnitXmlData;
 
 import com.interpss.common.SpringAppContext;
 import com.interpss.common.datatype.UnitType;
+import com.interpss.common.msg.IPSSMsgHub;
 import com.interpss.common.util.IpssLogger;
 import com.interpss.core.aclf.AclfBranch;
 import com.interpss.core.aclf.AclfBus;
 import com.interpss.core.aclf.AclfLoadCode;
 import com.interpss.core.aclf.AclfNetwork;
+import com.interpss.core.aclf.SwingBusAdapter;
 import com.interpss.core.net.Branch;
 import com.interpss.core.net.Bus;
 import com.interpss.core.net.Network;
 
 public class XmlNetParamModifier {
-	private static enum ParamType {
+	private static enum ComplexValueType {
 		Power, Z
+	};
+
+	private static enum ValueType {
+		Voltage
 	};
 
 	/**
@@ -56,8 +64,17 @@ public class XmlNetParamModifier {
 	 * @param mod
 	 *            the modification record
 	 */
-	public static boolean applyModification2Net(Network net,
-			ModificationXmlType mod) {
+	public static boolean applyModification(Network net, ModificationXmlType mod, IPSSMsgHub msg) {
+		if (!applyModification2Net(net, mod, msg))
+			return false;
+
+		if (applyModification2AclfNet(net, mod, msg))
+			return false;
+		
+		return true;
+	}
+
+	private static boolean applyModification2Net(Network net, ModificationXmlType mod, IPSSMsgHub msg) {
 		// apply the Network-level changes
 		if (mod.getBusChangeRecList() != null) {
 			for (ModificationXmlType.BusChangeRecList.BusChangeRec busRec : mod
@@ -85,7 +102,10 @@ public class XmlNetParamModifier {
 				}
 			}
 		}
-
+		return true;
+	}
+	
+	private static boolean applyModification2AclfNet(Network net, ModificationXmlType mod, IPSSMsgHub msg) {
 		// apply the AclfNetwork-level changes
 		if (net instanceof AclfNetwork) {
 			AclfNetwork aclfNet = (AclfNetwork) net;
@@ -108,22 +128,40 @@ public class XmlNetParamModifier {
 
 							// modify load code
 							if (busRec.getAclfBusChangeData().getLoadChangeData().getCodeChange() != null) {
-								ModificationXmlType.BusChangeRecList.BusChangeRec.AclfBusChangeData.LoadChangeData.CodeChange 
-										codeChange = busRec.getAclfBusChangeData().getLoadChangeData().getCodeChange();
+								LoadChangeData.CodeChange codeChange = busRec.getAclfBusChangeData().getLoadChangeData().getCodeChange();
 								bus.setLoadCode(codeChange.getLoadCode() == 
-										ModificationXmlType.BusChangeRecList.BusChangeRec.AclfBusChangeData.LoadChangeData.CodeChange.LoadCode.CONST_P ? AclfLoadCode.CONST_P
-												: (codeChange.getLoadCode() == ModificationXmlType.BusChangeRecList.BusChangeRec.AclfBusChangeData.LoadChangeData.CodeChange.LoadCode.CONST_I ? 
-														AclfLoadCode.CONST_I : (codeChange.getLoadCode() == 
-															ModificationXmlType.BusChangeRecList.BusChangeRec.AclfBusChangeData.LoadChangeData.CodeChange.LoadCode.CONST_Z ? AclfLoadCode.CONST_Z
-																: (codeChange.getLoadCode() == 
-																	ModificationXmlType.BusChangeRecList.BusChangeRec.AclfBusChangeData.LoadChangeData.CodeChange.LoadCode.EXPONENTIAL ? AclfLoadCode.EXPONENTIAL
-																		: AclfLoadCode.NON_LOAD))));
+									LoadChangeData.CodeChange.LoadCode.CONST_P ? AclfLoadCode.CONST_P
+											: (codeChange.getLoadCode() == LoadChangeData.CodeChange.LoadCode.CONST_I ? 
+												AclfLoadCode.CONST_I : (codeChange.getLoadCode() == 
+													LoadChangeData.CodeChange.LoadCode.CONST_Z ? AclfLoadCode.CONST_Z
+														: (codeChange.getLoadCode() == 
+															LoadChangeData.CodeChange.LoadCode.EXPONENTIAL ? AclfLoadCode.EXPONENTIAL
+																: AclfLoadCode.NON_LOAD))));
 								if (bus.getLoadCode() == AclfLoadCode.EXPONENTIAL) {
 									bus.setExpLoadP(codeChange.getExpLoadP());
 									bus.setExpLoadQ(codeChange.getExpLoadQ());
 								}
 							}
 						}
+
+						if (busRec.getAclfBusChangeData() != null &&
+								busRec.getAclfBusChangeData().getGenChangeData() != null) {
+							// swing bus voltage modification
+							if (busRec.getAclfBusChangeData().getGenChangeData().getSwingVoltageChange() != null) {
+								if (bus.isSwing()) {
+						  			final SwingBusAdapter gen = (SwingBusAdapter)bus.adapt(SwingBusAdapter.class);
+									double x = applyValueChangeRec(gen.getVoltMag(UnitType.PU),
+											busRec.getAclfBusChangeData().getGenChangeData().getSwingVoltageChange(),
+											ValueType.Voltage, bus.getBaseVoltage());
+									gen.setVoltMag(x, UnitType.PU);
+								}
+								else {
+									msg.sendErrorMsg("Error: try to set swing bus voltage of a non-swing bus, id: " + bus.getId());
+									return false;
+								}
+							}
+						}
+					
 					} else {
 						return false;
 					}
@@ -137,8 +175,8 @@ public class XmlNetParamModifier {
 					if (branch != null) {
 						if (braRec.getAclfBranchChangeData() != null &&
 								braRec.getAclfBranchChangeData().getBranchZChange() != null) {
-							Complex z = applyComplexParamChangeRec(branch
-									.getZ(), braRec.getAclfBranchChangeData().getBranchZChange(), ParamType.Z,
+							Complex z = applyComplexValueChangeRec(branch
+									.getZ(), braRec.getAclfBranchChangeData().getBranchZChange(), ComplexValueType.Z,
 									aclfNet.getBaseKva(), branch
 											.getHigherBaseVoltage());
 							branch.setZ(z);
@@ -186,26 +224,37 @@ public class XmlNetParamModifier {
 
 	private static Complex applyPowerChangeRec(Complex original,
 			ComplexValueChangeXmlType changeRec, double baseKva) {
-		return applyComplexParamChangeRec(original, changeRec, ParamType.Power,
+		return applyComplexValueChangeRec(original, changeRec, ComplexValueType.Power,
 				baseKva, 1.0);
 	}
 
-	private static Complex applyComplexParamChangeRec(Complex original,
-			ComplexValueChangeXmlType changeRec, ParamType ptype,
+	/**
+	 * 
+	 * 
+	 * @param original the origianl value in pu
+	 * @param changeRec
+	 * @param ptype
+	 * @param baseKva
+	 * @param busBaseVolt
+	 * @return
+	 */
+	private static Complex applyComplexValueChangeRec(Complex original,
+			ComplexValueChangeXmlType changeRec, ComplexValueType ptype,
 			double baseKva, double busBaseVolt) {
 		if (changeRec.getChangeAction() == ComplexValueChangeXmlType.ChangeAction.ADD
 				|| changeRec.getChangeAction() == ComplexValueChangeXmlType.ChangeAction.SET) {
 			// for add/set, use value and unit (PU or power unit)
 			ComplexXmlType c = changeRec.getValue();
 			double re = c.getRe(), im = c.getIm();
+			// the original is in PU
 			if (changeRec.getUnit() != UnitXmlData.PU) {
 				// convert set/add value to PU
 				byte unit = IpssXmlParser.mapXmlUnitType2IpssUnitType(changeRec
 						.getUnit());
-				if (ptype == ParamType.Power) {
+				if (ptype == ComplexValueType.Power) {
 					re = UnitType.pConversion(re, baseKva, unit, UnitType.PU);
 					im = UnitType.pConversion(im, baseKva, unit, UnitType.PU);
-				} else if (ptype == ParamType.Z) {
+				} else if (ptype == ComplexValueType.Z) {
 					Complex z = UnitType.zConversion(new Complex(re, im),
 							busBaseVolt, baseKva, unit, UnitType.PU);
 					re = z.getReal();
@@ -230,6 +279,39 @@ public class XmlNetParamModifier {
 					* (1.0 + factor));
 		}
 
+		IpssLogger.getLogger().warning(
+				"Wrong ChangeAction, changeRec: " + changeRec.toString());
+		return original;
+	}
+
+	private static double applyValueChangeRec(double original,
+			ValueChangeXmlType changeRec, ValueType ptype, double busBaseVolt) {
+		if (changeRec.getChangeAction() == ValueChangeXmlType.ChangeAction.ADD
+				|| changeRec.getChangeAction() ==ValueChangeXmlType.ChangeAction.SET) {
+			// for add/set, use value and unit (PU or power unit)
+			double c = changeRec.getValue();
+			if (changeRec.getUnit() != UnitXmlData.PU) {
+				// convert set/add value to PU
+				byte unit = IpssXmlParser.mapXmlUnitType2IpssUnitType(changeRec
+						.getUnit());
+				if (ptype == ValueType.Voltage) {
+					c = UnitType.vConversion(c, busBaseVolt, unit, UnitType.PU);
+				} 
+			}
+			if (changeRec.getChangeAction() == ValueChangeXmlType.ChangeAction.ADD) {
+				c += original;
+			}
+			return c;
+		} else if (changeRec.getChangeAction() == ValueChangeXmlType.ChangeAction.INCREASE
+				|| changeRec.getChangeAction() == ValueChangeXmlType.ChangeAction.DECREASE) {
+			// for increase/decrease, use percent and unit (PU or percent)
+			double factor = changeRec.getPercent();
+			if (changeRec.getUnit() == UnitXmlData.PERCENT)
+				factor *= 0.01;
+			if (changeRec.getChangeAction() == ValueChangeXmlType.ChangeAction.DECREASE)
+				factor = -factor;
+			return original * (1.0 + factor);
+		}
 		IpssLogger.getLogger().warning(
 				"Wrong ChangeAction, changeRec: " + changeRec.toString());
 		return original;
