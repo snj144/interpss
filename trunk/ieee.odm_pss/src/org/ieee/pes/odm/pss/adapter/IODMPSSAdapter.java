09ffc1caf43381a7b45cb76b112a85c31e474797
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-02-23 13:35:14 +0000

    add IODMAdapter interface

---
+++ trunk/ieee.odm_pss/src/org/ieee/pes/odm/pss/adapter/IODMPSSAdapter.java
@@ -0,0 +1,54 @@
+/*
+ * @(#)IeeeCDFAdapter.java   
+ *
+ * Copyright (C) 2006 www.interpss.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @Author Mike Zhou
+ * @Version 1.0
+ * @Date 02/11/2008
+ * 
+ *   Revision History
+ *   ================
+ *
+ */
+
+package org.ieee.pes.odm.pss.adapter;
+
+import java.util.List;
+
+import org.ieee.pes.odm.pss.model.IEEEODMPSSModelParser;
+
+public interface IODMPSSAdapter {
+	/**
+	 * parse the input xml file according the the ODM schema
+	 * 
+	 * @param filename file name
+	 * @return
+	 * @throws Exception
+	 */
+	IEEEODMPSSModelParser parseXmlFile(String filename) throws Exception;
+	
+	/**
+	 * get parsing status
+	 * 
+	 * @return
+	 */
+	boolean getParseStatus();
+	
+	/**
+	 * If parsing staus = false, get error massages
+	 * 
+	 * @return
+	 */
+	List<String> errMessages();
+}
