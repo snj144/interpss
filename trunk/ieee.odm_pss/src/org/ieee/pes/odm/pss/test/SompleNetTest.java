8eaf13a176fa7d24680f4535714fd61b06f87357
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-02-25 22:06:38 +0000

    added XMLUnit test

---
+++ trunk/ieee.odm_pss/src/org/ieee/pes/odm/pss/test/SompleNetTest.java
@@ -0,0 +1,39 @@
+package org.ieee.pes.odm.pss.test;
+
+import java.io.File;
+
+import org.custommonkey.xmlunit.XMLUnit;
+import org.ieee.pes.odm.pss.model.IEEEODMPSSModelParser;
+import org.junit.Test;
+
+public class SompleNetTest extends ODMXmlTestCaseBase {
+	@Test
+	public void testValidXml() throws Exception {
+		new IEEEODMPSSModelParser(new File("testdata/sample/SimpleNet.xml"));
+	}
+
+	@Test 
+	public void testInvalidXml() {
+		try {
+			new IEEEODMPSSModelParser(new File("testdata/sample/SimpleNetNotValid.xml"));
+			assertTrue(false);
+		} catch (Exception e) {
+		}
+	}
+	
+	@Test
+    public void testXPathValues() throws Exception {
+    	IEEEODMPSSModelParser parser = new IEEEODMPSSModelParser(new File("testdata/sample/SimpleNet.xml"));
+        String xmlStr = parser.toString();
+        
+        XMLUnit.setXpathNamespaceContext(getNamespaceCtx());
+        assertXpathEvaluatesTo("V1.00", "/pss:PSSStudyCase/pss:schemaVersion", xmlStr);
+        assertXpathEvaluatesTo("100.0", "/pss:PSSStudyCase/pss:baseCase/pss:baseKva", xmlStr);
+
+        assertXpathEvaluatesTo("Bus1", XPath_BusList+"pss:bus[1]/pss:id", xmlStr);
+        assertXpathEvaluatesTo("Bus2", XPath_BusList+"pss:bus[2]/pss:id", xmlStr);
+        
+        assertXpathEvaluatesTo("Bus1", XPath_BranchList+"pss:branch[1]/pss:fromBus/pss:idRef", xmlStr);
+        assertXpathEvaluatesTo("Bus2", XPath_BranchList+"pss:branch[1]/pss:toBus/pss:idRef", xmlStr);
+    }
+}
