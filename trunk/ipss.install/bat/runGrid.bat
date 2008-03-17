935196a0d420f956888a1e7bcb118c2db514b7ee
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-03-17 01:47:21 +0000

    added Gridgain path

--- trunk/ipss.install/bat/runGrid.bat
+++ trunk/ipss.install/bat/runGrid.bat
@@ -1,4 +1,9 @@
 @echo off
+REM
+REM set Gridgain installation location
+REM
+set GRIDGAIN_HOME=C:\Program Files\GridGain-1.6.1
+
 set IPSS_HOME=$INSTALL_PATH
 cd %IPSS_HOME%
 call bin\setCmdPath.bat
