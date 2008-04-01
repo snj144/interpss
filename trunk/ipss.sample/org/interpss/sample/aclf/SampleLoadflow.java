380682f71f09245a505780f1da82007dd23dd0f0
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-04-01 21:04:48 +0000

    added another sample

--- trunk/ipss.sample/org/interpss/sample/aclf/SampleLoadflow.java
+++ trunk/ipss.sample/org/interpss/sample/aclf/SampleLoadflow.java
@@ -158,4 +158,65 @@ public class SampleLoadflow {
 
 		loadflowWithAdjustment(msg);
 	}	
+
+	public static void simpleLoadflow1(IPSSMsgHub msg) {
+		// Create an AclfNetwork object
+		AclfNetwork net = CoreObjectFactory.createAclfNetwork();
+
+		double baseKva = 100000.0;
+		
+		// set system basekva for loadflow calculation
+		net.setBaseKva(baseKva);
+	  	
+		// create a AclfBus object
+  		AclfBus bus1 = CoreObjectFactory.createAclfBus("Bus1");
+  		// set bus name and description attributes
+  		bus1.setAttributes("Bus 1", "");
+  		// set bus base voltage 
+  		bus1.setBaseVoltage(4000.0);
+  		// set bus to be a swing bus
+  		bus1.setGenCode(AclfGenCode.SWING);
+  		// adapt the bus object to a swing bus object
+  		SwingBusAdapter swingBus = (SwingBusAdapter)bus1.adapt(SwingBusAdapter.class);
+  		// set swing bus attributes
+  		swingBus.setVoltMag(1.0, UnitType.PU);
+  		swingBus.setVoltAng(0.0, UnitType.Deg);
+  		// add the bus into the network
+  		net.addBus(bus1);
+  		
+  		AclfBus bus2 = CoreObjectFactory.createAclfBus("Bus2");
+  		bus2.setAttributes("Bus 2", "");
+  		bus2.setBaseVoltage(4000.0);
+  		// set the bus to a non-generator bus
+  		bus2.setGenCode(AclfGenCode.NON_GEN);
+  		// set the bus to a constant power load bus
+  		bus2.setLoadCode(AclfLoadCode.CONST_P);
+  		// adapt the bus object to a Load bus object
+  		LoadBusAdapter loadBus = (LoadBusAdapter)bus2.adapt(LoadBusAdapter.class);
+  		// set load to the bus
+  		loadBus.setLoad(new Complex(1.0, 0.8), UnitType.PU, baseKva);
+  		net.addBus(bus2);
+  		
+  		// create an AclfBranch object
+  		AclfBranch branch = CoreObjectFactory.createAclfBranch();
+  		// set branch name, description and circuit number
+  		branch.setAttributes("Branch 1", "", "1");
+  		// set branch to a Line branch
+  		branch.setBranchCode(AclfBranchCode.LINE);
+  		// adapte the branch object to a line branch object
+		LineAdapter lineBranch = (LineAdapter)branch.adapt(LineAdapter.class);
+		// set branch parameters
+  		lineBranch.setZ(new Complex(0.05, 0.1), UnitType.PU, 4000.0, baseKva, msg);
+  		// add the branch from Bus1 to Bus2
+  		net.addBranch(branch, "Bus1", "Bus2");
+	  	
+	  	// create the default loadflow algorithm
+	  	LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
+
+	  	// use the loadflow algorithm to perform loadflow calculation
+	  	algo.loadflow(msg);
+	  	
+	  	// output loadflow calculation results
+	  	System.out.println(AclfOutFunc.loadFlowSummary(net));
+    }	
 }
