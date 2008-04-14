d72f6cd0e443f841d95f6d513fedce530ff6568f
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-04-14 02:18:49 +0000

    refactor ProtectionRule

--- trunk/ipss.plugin.test/src/org/interpss/schema/ProtectionCaseTest.java
+++ trunk/ipss.plugin.test/src/org/interpss/schema/ProtectionCaseTest.java
@@ -41,6 +41,7 @@ import com.interpss.common.SpringAppContext;
 import com.interpss.common.mapper.IpssMapper;
 import com.interpss.common.util.SerializeEMFObjectUtil;
 import com.interpss.core.CoreObjectFactory;
+import com.interpss.core.aclf.AclfLoadCode;
 import com.interpss.core.aclfadj.AclfAdjNetwork;
 import com.interpss.core.algorithm.LoadflowAlgorithm;
 import com.interpss.simu.SimuContext;
@@ -48,45 +49,6 @@ import com.interpss.simu.SimuCtxType;
 import com.interpss.simu.SimuObjectFactory;
 
 public class ProtectionCaseTest extends BaseTestSetup {
-	@Test
-	public void run3WXfrOffCaseTest() throws Exception {
-		SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.ACLF_ADJ_NETWORK, msg);
-		loadCaseData("testData/aclf/IEEE-14Bus.ipss", simuCtx);
-  		// save net to a String
-  		String netStr = SerializeEMFObjectUtil.saveModel(simuCtx.getAclfAdjNet());
-
-		File xmlFile = new File("testData/xml/IEEE14Bus_W3XfrOff.xml");
-  		IpssXmlParser parser = new IpssXmlParser(xmlFile);
-  		//System.out.println("----->" + parser.getRootElem().toString());
-
-	  	assertTrue(parser.getRunStudyCase().getAnalysisRunType() == RunStudyCaseXmlType.AnalysisRunType.RUN_ACLF);
-  		
-		AclfAdjNetwork net = (AclfAdjNetwork)SerializeEMFObjectUtil.loadModel(netStr);
-		net.getAclfBranch("0005->0006(1)").setRatingMva1(70.0);
-
-		AclfStudyCase aclfCase = parser.getRunStudyCase().getRunAclfStudyCase().getAclfStudyCaseList().getAclfStudyCaseArray()[0];
-  		IpssMapper mapper = PluginSpringAppContext.getIpssXmlMapper();
-		mapper.mapping(aclfCase.getModification(), net, ModificationXmlType.class);
-
-
-		LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
-		mapper = new RunForm2AlgorithmMapper();
-		mapper.mapping(aclfCase.getAclfAlgorithm(), algo, AclfAlgorithmXmlType.class);
-  		
-  		assertTrue(algo.getMaxIterations() == 20);
-  		assertTrue(algo.getTolerance() == 1.0E-4);
-  		assertTrue(algo.loadflow(SpringAppContext.getIpssMsgHub()));
-  		//System.out.println(net.net2String());  	
-  		
-	  	assertTrue(!net.getAclfBus("0007").isActive());
-	  	assertTrue(!net.getAclfBus("0008").isActive());
-
-	  	assertTrue(!net.getAclfBranch("0004->0007(1)").isActive());
-	  	assertTrue(!net.getAclfBranch("0004->0009(1)").isActive());
-	  	assertTrue(!net.getAclfBranch("0007->0009(1)").isActive());
-	  	assertTrue(!net.getAclfBranch("0007->0008(1)").isActive());
-	}
-	
 	@Test
 	public void runAclfProtectCaseTest() throws Exception {
 		SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.ACLF_ADJ_NETWORK, msg);
@@ -128,7 +90,57 @@ public class ProtectionCaseTest extends BaseTestSetup {
 	  	assertTrue(ProtectionRuleHanlder.evlBusCondition(cond, net, 1.0, 0.8, msg));
 	  	assertTrue(ProtectionRuleHanlder.evlBusCondition(cond, net, 1.2, 1.05, msg));
 	  	
-	  	ProtectionRuleHanlder.applyAclfRuleSet(net, parser.getRunAclfStudyCase().getAclfRuleBase(), 1, 1.2, 0.8, msg);
+	  	assertTrue(ProtectionRuleHanlder.applyAclfRuleSet(net, parser.getRunAclfStudyCase().getAclfRuleBase(), 1, 1.2, 0.8, msg));
 	  	assertTrue(!net.getAclfBranch("0010->0009(1)").isActive());
 	}		
+
+	@Test
+	public void run3WXfrOffCaseTest() throws Exception {
+		SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.ACLF_ADJ_NETWORK, msg);
+		loadCaseData("testData/aclf/IEEE-14Bus.ipss", simuCtx);
+  		// save net to a String
+  		String netStr = SerializeEMFObjectUtil.saveModel(simuCtx.getAclfAdjNet());
+
+		File xmlFile = new File("testData/xml/IEEE14Bus_W3XfrOff.xml");
+  		IpssXmlParser parser = new IpssXmlParser(xmlFile);
+  		//System.out.println("----->" + parser.getRootElem().toString());
+
+	  	assertTrue(parser.getRunStudyCase().getAnalysisRunType() == RunStudyCaseXmlType.AnalysisRunType.RUN_ACLF);
+  		
+		AclfAdjNetwork net = (AclfAdjNetwork)SerializeEMFObjectUtil.loadModel(netStr);
+		net.getAclfBranch("0005->0006(1)").setRatingMva1(70.0);
+
+		AclfStudyCase aclfCase = parser.getRunStudyCase().getRunAclfStudyCase().getAclfStudyCaseList().getAclfStudyCaseArray()[0];
+  		IpssMapper mapper = PluginSpringAppContext.getIpssXmlMapper();
+		mapper.mapping(aclfCase.getModification(), net, ModificationXmlType.class);
+
+
+		LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
+		mapper = new RunForm2AlgorithmMapper();
+		mapper.mapping(aclfCase.getAclfAlgorithm(), algo, AclfAlgorithmXmlType.class);
+  		
+  		assertTrue(algo.getMaxIterations() == 20);
+  		assertTrue(algo.getTolerance() == 1.0E-4);
+  		assertTrue(algo.loadflow(SpringAppContext.getIpssMsgHub()));
+  		//System.out.println(net.net2String());  	
+  		
+	  	assertTrue(!net.getAclfBus("0007").isActive());
+	  	assertTrue(!net.getAclfBus("0008").isActive());
+
+	  	assertTrue(!net.getAclfBranch("0004->0007(1)").isActive());
+	  	assertTrue(!net.getAclfBranch("0004->0009(1)").isActive());
+	  	assertTrue(!net.getAclfBranch("0007->0009(1)").isActive());
+	  	assertTrue(!net.getAclfBranch("0007->0008(1)").isActive());
+	  	
+	  	assertTrue(ProtectionRuleHanlder.applyAclfRuleSet(net, 
+	  				parser.getRunAclfStudyCase().getAclfRuleBase(), 1, 1.2, 0.8, msg));
+  		assertTrue(algo.loadflow(SpringAppContext.getIpssMsgHub()));
+	  	
+  		assertTrue(ProtectionRuleHanlder.applyAclfRuleSet(net, 
+  					parser.getRunAclfStudyCase().getAclfRuleBase(), 2, 1.2, 0.8, msg));
+  		assertTrue(algo.loadflow(SpringAppContext.getIpssMsgHub()));
+
+  		assertTrue(net.getAclfBus("0014").getLoadCode() == AclfLoadCode.NON_LOAD);
+	  	assertTrue(net.getAclfBus("0013").getLoadCode() == AclfLoadCode.NON_LOAD);
+	}
 }
