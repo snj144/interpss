cd5008d872014e20b5b23ce69d22880690fc6dd0
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-04-20 18:54:26 +0000

    working on CustomScriptRun for PSS/E contingency analysis, completed 1st phase

--- ipss.plugin/src/org/interpss/custom/run/psseCon/PSSEContingencyAnalysis.java
+++ ipss.plugin/src/org/interpss/custom/run/psseCon/PSSEContingencyAnalysis.java
@@ -1,8 +1,10 @@
 package org.interpss.custom.run.psseCon;
 
 import org.interpss.custom.run.CustomRunScriptPluginBase;
+import org.interpss.schema.InterPSSXmlType;
 
 import com.interpss.common.util.IpssLogger;
+import com.interpss.common.util.StringUtil;
 import com.interpss.simu.SimuContext;
 
 
@@ -11,8 +13,18 @@ public class PSSEContingencyAnalysis extends CustomRunScriptPluginBase {
 	@Override
 	public boolean runCase(String scripts, SimuContext simuCtx) {
 		IpssLogger.getLogger().info("Run custom scripts with plugin: PSSEContingencyAnalysis");
+		String[] strAry = StringUtil.strToken2Array(scripts, System.getProperty("line.separator"));
+		//for(String s : strAry) System.out.println(s);
+		
+		InterPSSXmlType ipssXmlDoc = null;
+		try {
+			ipssXmlDoc = ContingencyFileParser.parseControlFile(strAry);
+		} catch (Exception e) {
+			IpssLogger.getLogger().severe(e.toString());
+			return false;
+		}
+		System.out.println(ipssXmlDoc.toString());
+		
 		return true;
 	}
-	
-	
 }
