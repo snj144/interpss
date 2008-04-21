c90b8d3cf0ef6696cf53664b29c3cf527ad28479
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-04-21 01:11:35 +0000

    Completed CustomScriptRun for PSS/E contingency analysis.

--- trunk/ipss.plugin.test/src/org/interpss/edispatch/ContingencyControlFileCaseTest.java
+++ trunk/ipss.plugin.test/src/org/interpss/edispatch/ContingencyControlFileCaseTest.java
@@ -2,13 +2,56 @@ package org.interpss.edispatch;
 
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+
 import org.interpss.BaseTestSetup;
+import org.interpss.PluginSpringAppContext;
+import org.interpss.custom.IpssFileAdapter;
 import org.interpss.custom.run.psseCon.ContingencyFileParser;
+import org.interpss.editor.mapper.RunForm2AlgorithmMapper;
+import org.interpss.mapper.IpssXmlMapper;
+import org.interpss.schema.AclfAlgorithmXmlType;
 import org.interpss.schema.InterPSSXmlType;
+import org.interpss.schema.ModificationXmlType;
 import org.interpss.schema.RunStudyCaseXmlType.RunAclfStudyCase.AclfStudyCaseList.AclfStudyCase;
+import org.interpss.xml.IpssXmlParser;
 import org.junit.Test;
 
+import com.interpss.common.SpringAppContext;
+import com.interpss.common.mapper.IpssMapper;
+import com.interpss.common.util.SerializeEMFObjectUtil;
+import com.interpss.core.CoreObjectFactory;
+import com.interpss.core.aclfadj.AclfAdjNetwork;
+import com.interpss.core.algorithm.LoadflowAlgorithm;
+import com.interpss.simu.SimuContext;
+
 public class ContingencyControlFileCaseTest extends BaseTestSetup {
+	@Test
+	public void cr113BusCaseTest() throws Exception {
+		IpssFileAdapter adapter = PluginSpringAppContext.getCustomFileAdapter("psse");
+		SimuContext simuCtx = adapter.load("testData/edispatch/CR113Bus.raw", SpringAppContext.getIpssMsgHub());
+// 		System.out.println(simuCtx.getAclfNet().net2String());
+
+  		String netStr = SerializeEMFObjectUtil.saveModel(simuCtx.getAclfAdjNet());
+
+		File xmlFile = new File("testData/edispatch/RunCR113Bus.xml");
+  		IpssXmlParser parser = new IpssXmlParser(xmlFile);
+  		//System.out.println("----->" + parser.getRootElem().toString());
+
+	  	for ( AclfStudyCase aclfCase : parser.getRunStudyCase().getRunAclfStudyCase().getAclfStudyCaseList().getAclfStudyCaseArray()) {
+			AclfAdjNetwork net = (AclfAdjNetwork)SerializeEMFObjectUtil.loadModel(netStr);
+	  		LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
+		  	IpssMapper mapper = new IpssXmlMapper();
+		  	mapper.mapping(aclfCase.getModification(), net, ModificationXmlType.class);
+		  	
+		  	mapper = new RunForm2AlgorithmMapper();
+		  	mapper.mapping( parser.getRunStudyCase().getRunAclfStudyCase().getDefaultAclfAlgorithm(), 
+	  							algo, AclfAlgorithmXmlType.class);
+	  	
+	  		assertTrue(algo.loadflow(SpringAppContext.getIpssMsgHub()));
+	  	}	
+	}
+	
 	@Test
 	public void simpleCaseTest() throws Exception {
 		InterPSSXmlType ipssDoc = ContingencyFileParser.parseControlFile("testData/edispatch/contingency.con");
