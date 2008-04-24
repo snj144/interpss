f1b876d885c0262c32393b2199d50185fb0e998d
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-04-24 18:21:03 +0000

    Initial import.

---
+++ ipss.ws/src/org/interpss/editor/ws/impl/IpssProjectImpl.java
@@ -0,0 +1,43 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.interpss.editor.ws.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.interpss.editor.ws.IpssProject;
+import org.interpss.editor.ws.IpssWsPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Ipss Project</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class IpssProjectImpl extends IpssWsItemContainerImpl implements IpssProject {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected IpssProjectImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return IpssWsPackage.Literals.IPSS_PROJECT;
+	}
+
+} //IpssProjectImpl
---
+++ ipss.ws/src/org/interpss/editor/ws/impl/IpssWorkSpaceImpl.java
@@ -0,0 +1,43 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id$
+ */
+package org.interpss.editor.ws.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.interpss.editor.ws.IpssWorkSpace;
+import org.interpss.editor.ws.IpssWsPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Ipss Work Space</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * </p>
+ *
+ * @generated
+ */
+public class IpssWorkSpaceImpl extends IpssWsItemContainerImpl implements IpssWorkSpace {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected IpssWorkSpaceImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return IpssWsPackage.Literals.IPSS_WORK_SPACE;
+	}
+
+} //IpssWorkSpaceImpl
