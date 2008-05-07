1f89844d298e1c231fc30231a752cd50f9a724a9
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-05-07 03:05:12 +0000

    impl contingency analysis

---
+++ trunk/ipss.plugin.test/src/org/interpss/edispatch/ContingencyXmlCaseTest.java
@@ -0,0 +1,56 @@
+package org.interpss.edispatch;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.interpss.BaseTestSetup;
+import org.interpss.mapper.IpssXmlMapper;
+import org.interpss.schema.AclfAlgorithmXmlType;
+import org.interpss.schema.AclfStudyCaseXmlType;
+import org.interpss.schema.ModificationXmlType;
+import org.interpss.schema.RunStudyCaseXmlType;
+import org.interpss.xml.IpssXmlParser;
+import org.junit.Test;
+
+import com.interpss.common.SpringAppContext;
+import com.interpss.common.mapper.IpssMapper;
+import com.interpss.common.util.SerializeEMFObjectUtil;
+import com.interpss.core.CoreObjectFactory;
+import com.interpss.core.aclfadj.AclfAdjNetwork;
+import com.interpss.core.algorithm.LoadflowAlgorithm;
+import com.interpss.simu.SimuContext;
+import com.interpss.simu.SimuCtxType;
+import com.interpss.simu.SimuObjectFactory;
+import com.interpss.simu.multicase.ContingencyAnalysis;
+import com.interpss.simu.multicase.StudyCase;
+
+public class ContingencyXmlCaseTest extends BaseTestSetup {
+	@Test
+	public void simpleCaseTest() throws Exception {
+		SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.ACLF_ADJ_NETWORK, msg);
+		loadCaseData("testData/aclf/IEEE-14Bus.ipss", simuCtx);
+  		String netStr = SerializeEMFObjectUtil.saveModel(simuCtx.getAclfAdjNet());
+
+		File xmlFile = new File("testData/xml/contingency/simpleTest.xml");
+  		IpssXmlParser parser = new IpssXmlParser(xmlFile);
+  		//System.out.println("----->" + parser.getRootElem().toString());
+  		
+	  	assertTrue(parser.getRunStudyCase().getAnalysisRunType() == RunStudyCaseXmlType.AnalysisRunType.CONTINGENCY_ANALYSIS);
+  		
+	  	ContingencyAnalysis mscase = SimuObjectFactory.createContingencyAnalysis(SimuCtxType.ACLF_ADJ_NETWORK);
+	  	int cnt = 0;
+	  	for ( AclfStudyCaseXmlType aclfCase : parser.getContingencyAnalysis().getAclfStudyCaseList().getAclfStudyCaseArray()) {
+			AclfAdjNetwork net = (AclfAdjNetwork)SerializeEMFObjectUtil.loadModel(netStr);
+	  		LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
+		  	IpssMapper mapper = new IpssXmlMapper();
+		  	mapper.mapping(aclfCase.getModification(), net, ModificationXmlType.class);
+		  	mapper.mapping(parser.getContingencyAnalysis().getDefaultAclfAlgorithm(), algo, AclfAlgorithmXmlType.class);
+		  	
+	  		assertTrue(algo.loadflow(SpringAppContext.getIpssMsgHub()));
+	  		
+	  		StudyCase scase = SimuObjectFactory.createStudyCase(aclfCase.getRecId(), aclfCase.getRecName(), ++cnt, mscase);
+	  		scase.setNetModelString(SerializeEMFObjectUtil.saveModel(net));
+	  	}
+	}
+}
