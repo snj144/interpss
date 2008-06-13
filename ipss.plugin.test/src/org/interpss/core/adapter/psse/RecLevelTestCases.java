0cbe8910ed5619bc37ae75082a6a67f530aadb0b
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-06-13 15:22:14 +0000

    refactor PSSE adapter

--- ipss.plugin.test/src/org/interpss/core/adapter/psse/RecLevelTestCases.java
+++ ipss.plugin.test/src/org/interpss/core/adapter/psse/RecLevelTestCases.java
@@ -28,6 +28,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.interpss.BaseTestSetup;
 import org.interpss.custom.exchange.psse.PSSEDataRec;
+import org.interpss.custom.exchange.psse.PSSEShuntDataRec;
 import org.junit.Test;
 
 public class RecLevelTestCases extends BaseTestSetup {
@@ -40,16 +41,16 @@ public class RecLevelTestCases extends BaseTestSetup {
 */		
 		PSSEDataRec.VersionNo version = PSSEDataRec.VersionNo.PSS_E_30;
 		String lineStr = "   621,1,1.05000,0.95000,     0,  100.0,'            ',    3.00, 1,   1.20, 1,   1.80";
-		PSSEDataRec.ShuntRec rec = new PSSEDataRec.ShuntRec(lineStr, version);
+		PSSEShuntDataRec rec = new PSSEShuntDataRec(lineStr, version);
 		assertTrue(new Double(rec.binit).doubleValue() == 3.0);
 		
 		lineStr = "   640,1,1.05000,0.95000,     0,  100.0,'            ',   16.00, 1,  16.00";
-		rec = new PSSEDataRec.ShuntRec(lineStr, version);
+		rec = new PSSEShuntDataRec(lineStr, version);
 		assertTrue(new Double(rec.binit).doubleValue() == 16.0);
 		
 		
 		lineStr = "   645,1,1.05000,0.95000,     0,  100.0,'            ',  100.00, 1,  25.00, 1,  25.00, 1,  25.00, 1,  25.00";
-		rec = new PSSEDataRec.ShuntRec(lineStr, version);
+		rec = new PSSEShuntDataRec(lineStr, version);
 		assertTrue(new Double(rec.binit).doubleValue() == 100.0);
 	}
 
@@ -60,11 +61,11 @@ public class RecLevelTestCases extends BaseTestSetup {
 
 		PSSEDataRec.VersionNo version = PSSEDataRec.VersionNo.Old;
 		String lineStr = "80017 0  1.000  1.000     0       0.00   2   19.84                                                                                       /* [SHEPPARD] */";
-		PSSEDataRec.ShuntRec rec = new PSSEDataRec.ShuntRec(lineStr, version);
+		PSSEShuntDataRec rec = new PSSEShuntDataRec(lineStr, version);
 		assertTrue(new Double(rec.binit).doubleValue() == 0.0);
 
 		lineStr = "80024 0  1.000  1.000     0       0.00   1   18.94  1   19.76  1   21.60";
-		rec = new PSSEDataRec.ShuntRec(lineStr, version);
+		rec = new PSSEShuntDataRec(lineStr, version);
 		assertTrue(new Double(rec.binit).doubleValue() == 0.0);
 	}
 }
