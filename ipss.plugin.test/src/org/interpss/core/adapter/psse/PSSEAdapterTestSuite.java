7261f623142f174d8386d2d49f4676091fba03bb
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-06-14 01:54:32 +0000

    completed refactor PSSE adapter

---
+++ ipss.plugin.test/src/org/interpss/core/adapter/psse/PSSEAdapterTestSuite.java
@@ -0,0 +1,13 @@
+package org.interpss.core.adapter.psse;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	CR_UserTestCases.class,
+	RecLevelTestCases.class,
+})
+public class PSSEAdapterTestSuite {
+}
