9045bd6adccd55b7420ad52fcde99999d6f1c064
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-07-23 04:04:48 +0000

    backup

--- trunk/ipss.plugin.test/src/org/interpss/spring/SpringTestSuite.java
+++ trunk/ipss.plugin.test/src/org/interpss/spring/SpringTestSuite.java
@@ -3,7 +3,7 @@ package org.interpss.spring;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
+  
 @RunWith(Suite.class)
 @SuiteClasses({
 	CustomFileAdapterTest.class,
