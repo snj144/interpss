338a08ee27fa5f8e74a4320695aff902cf5d9f2c
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-09-11 12:31:37 +0000

    implementing Ge Shunt and ControlledShunt

---
+++ ipss.plugin.test/src/org/interpss/core/adapter/ge/SCEdisonTestCases.java
@@ -0,0 +1,51 @@
+ /*
+  * @(#)CR_UserTestCases.java   
+  *
+  * Copyright (C) 2008 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 02/15/2008
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
+package org.interpss.core.adapter.ge;
+
+import static org.junit.Assert.assertTrue;
+
+import org.interpss.BaseTestSetup;
+import org.interpss.PluginSpringAppContext;
+import org.interpss.custom.IpssFileAdapter;
+import org.junit.Test;
+
+import com.interpss.common.SpringAppContext;
+import com.interpss.common.datatype.UnitType;
+import com.interpss.core.CoreObjectFactory;
+import com.interpss.core.aclf.AclfBus;
+import com.interpss.core.aclf.SwingBusAdapter;
+import com.interpss.core.algorithm.LoadflowAlgorithm;
+import com.interpss.ext.ge.aclf.GeAclfNetwork;
+import com.interpss.simu.SimuContext;
+
+public class SCEdisonTestCases extends BaseTestSetup {
+	@Test
+	public void testCase2() throws Exception {
+		IpssFileAdapter adapter = PluginSpringAppContext.getCustomFileAdapter("ge");
+		SimuContext simuCtx = adapter.load("testData/ge/lftest_083008.epc", SpringAppContext.getIpssMsgHub());
+		GeAclfNetwork net = (GeAclfNetwork)simuCtx.getAclfAdjNet();
+	}	
+}
+
