fbe3a546498e226e728410e3ccbe54414ec8a4cd
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-09-17 13:04:44 +0000

    working on IpssXmlUtilFunc.java

---
+++ ipss.xml/src/org/interpss/xml/IpssXmlUtilFunc.java
@@ -0,0 +1,31 @@
+package org.interpss.xml;
+
+import org.interpss.schema.BranchRecXmlType;
+
+import com.interpss.common.util.NetUtilFunc;
+
+public class IpssXmlUtilFunc {
+	public static void setBranchRec(BranchRecXmlType braRec, String id) {
+		braRec.setFromBusId(NetUtilFunc.findFromID(id));
+		braRec.setToBusId(NetUtilFunc.findToID(id));
+		braRec.setCircuitNumber(NetUtilFunc.findCirNo(id));
+	}
+	
+	public static String getBranchId(BranchRecXmlType braRec) {
+		if (braRec.getCircuitNumber() != null)
+			return NetUtilFunc.formBranchId(braRec.getFromBusId(), 
+					braRec.getToBusId(), braRec.getCircuitNumber());
+		else
+			return NetUtilFunc.formBranchId(braRec.getFromBusId(), 
+					braRec.getToBusId());
+	}
+	
+	public static String[] getBranchIdAry(BranchRecXmlType[] braRecList) {
+		String[] sAry = new String[braRecList.length];
+		int cnt = 0;
+		for (BranchRecXmlType braRec : braRecList) {
+			sAry[cnt++] = getBranchId(braRec);
+		}
+		return sAry;
+	}
+}
