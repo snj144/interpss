3e8bd1e102e68c41cbfd11b7d9195378a0d51460
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-10-19 01:53:36 +0000

    working on impl RunAcscStudyCase and RunDStabStudyCase using schema.

--- ipss.plugin.test/src/org/interpss/cmd/RunExecTest.java
+++ ipss.plugin.test/src/org/interpss/cmd/RunExecTest.java
@@ -10,11 +10,13 @@ import com.interpss.common.util.IpssJavaExec;
 public class RunExecTest  extends BaseTestSetup {
 	@Test
 	public void runExec() throws Exception {
-    	String ipssRootDir = "c:/eclipse/GEditor/";
+    	/*
+    	 String ipssRootDir = "c:/eclipse/GEditor/";
     	String[] args = {
             	ipssRootDir+"bin/ipssCmd.bat", 
             	"-in", ipssRootDir+"sample_ws/custom/ieee14.ipssdat",
     		    "-out", ipssRootDir+"output/ieee14.out"};
     	assertTrue(IpssJavaExec.exec(args));
+    	 */
     }		
 }
