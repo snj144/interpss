65334ea37a6d6dd665cd4a33e8ebada11f57ad6d
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2008-10-20 13:48:42 +0000

    initial import from Richard

---
+++ ipss.wb.cim.xmlnavigator/src/com/interpss/wb/opencim/xmlnavigator/Constants.java
@@ -0,0 +1,7 @@
+package com.interpss.wb.opencim.xmlnavigator;
+
+
+public class Constants {
+	public final static String OPENCIM_NAMESPACE = "http://www.interpss.com/opencim";
+	public final static String PROJECT_EXPLOER_VIEW_ID = "org.eclipse.ui.navigator.ProjectExplorer";
+}
