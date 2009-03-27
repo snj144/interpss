47be15f15e22fcd0b09a50b9b787b37e26d1166d
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2009-03-27 20:02:06 +0000

    DSL Implementation - created ipss.pssl package

---
+++ ipss.pssl.py/pssl/sample/SenAnalysis.py
@@ -0,0 +1,24 @@
+from com.interpss.pssl.simu           import IpssAclf;
+from com.interpss.pssl.simu           import IpssPTrading;
+from com.interpss.pssl.plugin         import IpssAdapter;
+
+from com.interpss.core.dclf           import SenAnalysisType;
+
+# load a data in IEEE common format and create an AclfNetwork object
+aclfNet = IpssAdapter.importAclfNet("sampleData/ieee14.ieee")  \
+			.setFormat(IpssAdapter.Format.IEEECommonFormat)    \
+			.load();
+
+# create a DClfAlgorithm and perform sensitivity analysis for the injection
+# and withdraw buses.			
+dclfAlgo = IpssPTrading.createDclfAlgorithm(aclfNet)           \
+			.setSenAnalysisType(SenAnalysisType.PANGLE)        \
+			.setInjectionBusId("No2").withdrawBusId("No14")    \
+			.runSenAnalysis();			
+			
+# output PTDF for the branch No2->No14			
+print "Power Transfer Distribution Factor: ",                  \
+      IpssPTrading.wrapAlgorithm(dclfAlgo)                     \
+			.setInjectionBusId("No2").withdrawBusId("No14")    \
+			.setBranchFromBusId("No4").toBusId("No7")          \
+			.getPTransferDistFactor();			
