ed4d1296758619b35ccf557186a272267b61025e
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2009-06-09 02:35:40 +0000

    complete first phase of PSS V30 Adapter and unit testing

--- ieee.odm_pss/src/org/ieee/pes/odm/pss/test/ODMAdapterTestSuite.java
+++ ieee.odm_pss/src/org/ieee/pes/odm/pss/test/ODMAdapterTestSuite.java
@@ -3,6 +3,8 @@ package org.ieee.pes.odm.pss.test;
 import org.ieee.pes.odm.pss.test.bpa.BPA_ODMTest;
 import org.ieee.pes.odm.pss.test.ge.GE_ODMTest;
 import org.ieee.pes.odm.pss.test.ieeecdf.IEEECDF_ODMTest;
+import org.ieee.pes.odm.pss.test.psse.PSSEV26_ODMTest;
+import org.ieee.pes.odm.pss.test.psse.PSSEV30_ODMTest;
 import org.ieee.pes.odm.pss.test.ucte.UCTE_ODMTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -16,7 +18,10 @@ import org.junit.runners.Suite.SuiteClasses;
 	
 	BPA_ODMTest.class,
 	
-	GE_ODMTest.class
+	GE_ODMTest.class,
+	
+	PSSEV30_ODMTest.class,
+	PSSEV26_ODMTest.class,
 })
 public class ODMAdapterTestSuite {
 }
