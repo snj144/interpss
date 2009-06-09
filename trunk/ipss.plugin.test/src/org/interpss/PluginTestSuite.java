286e1257b5107aa10ef8ee35182c641bf96798ab
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2009-06-09 02:36:18 +0000

    complete first phase of PSS V30 Adapter and unit testing

--- trunk/ipss.plugin.test/src/org/interpss/PluginTestSuite.java
+++ trunk/ipss.plugin.test/src/org/interpss/PluginTestSuite.java
@@ -1,5 +1,11 @@
 package org.interpss;
 
+import org.ieee.pes.odm.pss.test.bpa.BPA_ODMTest;
+import org.ieee.pes.odm.pss.test.ge.GE_ODMTest;
+import org.ieee.pes.odm.pss.test.ieeecdf.IEEECDF_ODMTest;
+import org.ieee.pes.odm.pss.test.psse.PSSEV26_ODMTest;
+import org.ieee.pes.odm.pss.test.psse.PSSEV30_ODMTest;
+import org.ieee.pes.odm.pss.test.ucte.UCTE_ODMTest;
 import org.interpss.cmd.AclfCmdRunTest;
 import org.interpss.cmd.RunExecTest;
 import org.interpss.contigency.ContingencyControlFileCaseTest;
@@ -100,9 +106,16 @@ import org.junit.runners.Suite.SuiteClasses;
 	AclfCmdRunTest.class,
 	RunExecTest.class,
 	
-	
 	// dclf
 	DclfIeee14BusCaseTest.class,
+	
+	// odm test cases
+	IEEECDF_ODMTest.class,
+	UCTE_ODMTest.class,
+	BPA_ODMTest.class,
+	GE_ODMTest.class,
+	PSSEV30_ODMTest.class,
+	PSSEV26_ODMTest.class,	
 })
 public class PluginTestSuite {
 }
