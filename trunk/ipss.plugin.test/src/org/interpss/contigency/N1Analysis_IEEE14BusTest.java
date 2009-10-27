fc9182ebfc09a1e2acf15dbf4304ccbda3f22c5c
erzhuanzhou <erzhuanzhou@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2009-10-27 15:29:29 +0000

    before impl StudyCaseList as LinkedList

--- trunk/ipss.plugin.test/src/org/interpss/contigency/N1Analysis_IEEE14BusTest.java
+++ trunk/ipss.plugin.test/src/org/interpss/contigency/N1Analysis_IEEE14BusTest.java
@@ -24,18 +24,24 @@
 
 package org.interpss.contigency;
 
+import org.eclipse.emf.ecore.change.util.ChangeRecorder;
 import org.interpss.BaseTestSetup;
 import org.interpss.display.ContingencyOutFunc;
+import org.interpss.schema.BranchChangeRecXmlType;
+import org.interpss.schema.ModificationXmlType;
 import org.junit.Test;
 
 import com.interpss.common.SpringAppContext;
 import com.interpss.core.CoreObjectFactory;
 import com.interpss.core.aclfadj.AclfAdjNetwork;
 import com.interpss.core.algorithm.LoadflowAlgorithm;
+import com.interpss.core.net.Branch;
 import com.interpss.simu.SimuContext;
 import com.interpss.simu.SimuCtxType;
 import com.interpss.simu.SimuObjectFactory;
 import com.interpss.simu.multicase.ContingencyAnalysis;
+import com.interpss.simu.multicase.ContingencyAnalysisType;
+import com.interpss.simu.multicase.aclf.AclfStudyCase;
 
 public class N1Analysis_IEEE14BusTest extends BaseTestSetup {
 	@Test
@@ -46,13 +52,51 @@ public class N1Analysis_IEEE14BusTest extends BaseTestSetup {
 		AclfAdjNetwork net = simuCtx.getAclfAdjNet();
 		
 	  	ContingencyAnalysis mscase = SimuObjectFactory.createContingencyAnalysis(SimuCtxType.ACLF_ADJ_NETWORK, net);
-
 	  	
 		LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net, SpringAppContext.getIpssMsgHub());
 		algo.setNonDivergent(true);
+		algo.setTolerance(0.001);
 		
-		mscase.N_1_Analysis(algo);
+		mscase.analysis(algo, ContingencyAnalysisType.N1);
 
+		//System.out.println(ContingencyOutFunc.securityMargin(mscase));		
+	}
+	
+	@Test
+	public void qImplTest() throws Exception {
+		SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.ACLF_ADJ_NETWORK, msg);
+		loadCaseData("testData/aclf/IEEE-14Bus.ipss", simuCtx);
+
+		AclfAdjNetwork net = simuCtx.getAclfAdjNet();
+		
+	  	ContingencyAnalysis mscase = SimuObjectFactory.createContingencyAnalysis(SimuCtxType.ACLF_ADJ_NETWORK, net);
+	  	
+		LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net, SpringAppContext.getIpssMsgHub());
+		algo.setNonDivergent(true);
+		algo.setTolerance(0.001);
+		
+		int cnt = 0;
+		for (Branch branch : net.getBranchList()) {
+			if (branch.isActive()) {
+				String caseId = "[x]"+branch.getId();
+				String caseName = "Open Branch "+branch.getId();
+				AclfStudyCase scase = SimuObjectFactory.createAclfStudyCase(caseId, caseName, ++cnt, mscase);
+				
+				ModificationXmlType mod = ModificationXmlType.Factory.newInstance();
+				mod.addNewBranchChangeRecList();
+				BranchChangeRecXmlType braChange = mod.getBranchChangeRecList().addNewBranchChangeRec();
+				braChange.setRecId("OpenLine_" + branch.getId());
+				braChange.setFromBusId(branch.getFromBus().getId());
+				braChange.setToBusId(branch.getToBus().getId());
+				braChange.setCircuitNumber(branch.getCircuitNumber());
+				braChange.setOffLine(true);
+
+				scase.setModifyModelString(mod.xmlText());
+			}
+		}
+		
+		//java.util.Queue<AclfStudyCase> q = new java.util.
+		
 		System.out.println(ContingencyOutFunc.securityMargin(mscase));		
 	}
 }
