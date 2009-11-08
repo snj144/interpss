3fcb0e897ce89212ab36aed665b80979967070af
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2009-11-08 13:33:42 +0000

    deleted EQ0907 case

--- trunk/ipss.plugin.test/src/org/interpss/core/adapter/psse/PSSEAdapterTestSuite.java
+++ trunk/ipss.plugin.test/src/org/interpss/core/adapter/psse/PSSEAdapterTestSuite.java
@@ -8,8 +8,6 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({
 	CR_UserTestCases.class,
 	RecLevelTestCases.class,
-	
-	EQ0907_TestCases.class,
 })
 public class PSSEAdapterTestSuite {
 }
