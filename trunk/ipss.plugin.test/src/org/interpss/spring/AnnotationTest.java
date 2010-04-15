46d9088c55efcf545788c059876449ea47115621
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-04-15 03:39:43 +0000

    change to classpath based spring configuration

--- trunk/ipss.plugin.test/src/org/interpss/spring/AnnotationTest.java
+++ trunk/ipss.plugin.test/src/org/interpss/spring/AnnotationTest.java
@@ -2,6 +2,7 @@ package org.interpss.spring;
 
 import static org.junit.Assert.assertTrue;
 
+import org.interpss.editor.jgraph.ui.app.IAppStatus;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -15,5 +16,9 @@ public class AnnotationTest {
 		
 		assertTrue(SpringAppCtx.getBean(Constants.SID_IpssMsgHub) != null);
 		//System.out.println(SpringAppCtx.getBeansOfType(IPSSMsgHub.class).toString());
+
+		//assertTrue(SpringAppCtx.getBean(Constants.SID_StdoutMsgListener) != null);
+		//assertTrue(SpringAppCtx.getBean("nBStatusPanel") != null);
+		System.out.println(SpringAppCtx.getBeansOfType(IAppStatus.class).toString());
 	}
 }
