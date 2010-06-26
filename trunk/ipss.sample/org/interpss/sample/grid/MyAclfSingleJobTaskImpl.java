a3d28fa7bc8ca46bcfac3dcb030bdb0ecf447b94
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-06-26 22:54:03 +0000

    add Grid extension example

---
+++ trunk/ipss.sample/org/interpss/sample/grid/MyAclfSingleJobTaskImpl.java
@@ -0,0 +1,18 @@
+
+package org.interpss.sample.grid;
+
+import org.interpss.gridgain.task.singleJob.AclfSingleJobTask;
+
+import com.interpss.ext.gridgain.AbstractGridGainJob;
+import com.interpss.ext.gridgain.RemoteMessageTable;
+
+public class MyAclfSingleJobTaskImpl extends AclfSingleJobTask {
+	private static final long serialVersionUID = 1;
+	
+	@Override
+	protected AbstractGridGainJob createJob(RemoteMessageTable remoteMsg) {
+		// plugon the custom class MyGridAclfJobImpl, where loadflow is actually 
+		// implemented
+		return new MyGridAclfJobImpl(remoteMsg);
+	}	
+}
