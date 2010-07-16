bd17bbefee4dd92a8099fccff704d3673d2469a9
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-07-16 15:57:33 +0000

    added N-1 sample

--- trunk/ipss.sample/src/org/interpss/sample/grid/GridUCTE2000WinterOffPeakCase.java
+++ trunk/ipss.sample/src/org/interpss/sample/grid/GridUCTE2000WinterOffPeakCase.java
@@ -1,23 +1,19 @@
 package org.interpss.sample.grid;
 
-import java.io.Serializable;
-import java.util.UUID;
-
 import org.gridgain.grid.Grid;
-import org.gridgain.grid.GridMessageListener;
 import org.interpss.PluginSpringAppContext;
 import org.interpss.custom.IpssFileAdapter;
 import org.interpss.display.AclfOutFunc;
 import org.interpss.gridgain.GridRunner;
 import org.interpss.gridgain.msg.RemoteMessageTable;
 import org.interpss.gridgain.util.GridUtil;
+import org.interpss.sample.grid.impl.GridHelper;
 import org.interpss.sample.grid.impl.MyAclfSingleJobTaskImpl;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.interpss.common.SpringAppContext;
 import com.interpss.common.datatype.Constants;
+import com.interpss.core.CoreObjectFactory;
 import com.interpss.core.aclf.AclfNetwork;
-import com.interpss.core.aclf.adj.AclfAdjNetwork;
 import com.interpss.simu.SimuContext;
 
 /**
@@ -28,16 +24,14 @@ import com.interpss.simu.SimuContext;
  *
  */
 public class GridUCTE2000WinterOffPeakCase {
-	public static String GridGainHome = "c:/Program Files (x86)/gridgain-2.1.1";
-
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		SpringAppContext.SpringAppCtx = new ClassPathXmlApplicationContext(Constants.SpringConfigPath_Plugin);
+		SpringAppContext.setAppContext(Constants.SpringConfigPath_Plugin);
 		
 		// init grid computing env
-		Grid grid = initGridEnv();
+		Grid grid = GridHelper.initGridEnv();
     	if (grid != null) {
     		// randomly select a remote grid node 
 	    	MyAclfSingleJobTaskImpl.RemoteNodeId = GridUtil.getAnyRemoteNodeId();
@@ -53,10 +47,9 @@ public class GridUCTE2000WinterOffPeakCase {
         		RemoteMessageTable result = new GridRunner(grid).executeTask(MyAclfSingleJobTaskImpl.class, adjNet, 0);
         		
         		// de-serialized the returning results
-        		adjNet = (AclfAdjNetwork) adjNet.deserialize(result.getSerializedAclfNet());
-        		adjNet.rebuildLookupTable();
+        		adjNet = CoreObjectFactory.createAclfNetwork(result.getSerializedAclfNet());
 
-        		// output simulaiton results
+        		// output simulation results
         		System.out.println(AclfOutFunc.loadFlowSummary(adjNet));
     		} catch (Exception e) {
     			e.printStackTrace();
@@ -65,41 +58,5 @@ public class GridUCTE2000WinterOffPeakCase {
 		
 		System.out.println("Stop InterPSS Grid env ...");
 		GridUtil.stopDefaultGrid();		
-	}
-
-	private static Grid initGridEnv() {
-		// start interpss grid env
-		GridUtil.startDefaultGrid(GridGainHome);
-		if (!GridUtil.isGridEnabled()) {
-			System.out.println("Cannot start grid env");
-			return null;
-		}
-		
-		if (GridUtil.getDefaultGrid().getAllNodes().size() <= 1) {
-			System.out.println("Please start a least one Gridgain agent for the test");
-			return null;
-		}
-
-		// set interpss grid env 
-		Grid grid = GridUtil.getDefaultGrid();
-		GridRunner.MasterNodeId = grid.getLocalNode().getId().toString();
-		GridRunner.RemoteNodeDebug = false;
-
-		// make sure Grid env is setup properly
-		String[] list = GridUtil.gridNodeNameList(grid, false);
-		if ( list.length <= 0) {
-			System.out.println("InterPSS grid env is not started properly");
-			return null;
-		}
-		
-		// set remote message listener, so that message from remote note are printed out
-    	grid.addMessageListener(new GridMessageListener() {
-    		public void onMessage(UUID arg0, Serializable arg1) {
-    			System.out.println(arg1);
-    		}        		
-    	});
-
-    	System.out.println("InterPSS Grid Env started successfully");
-    	return grid;
 	}
 }
