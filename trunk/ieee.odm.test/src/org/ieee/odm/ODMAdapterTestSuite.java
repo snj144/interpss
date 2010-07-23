6b24714add0573a8b34bcefcca9b0de8c4d45d62
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-07-23 17:21:53 +0000

    migrated PSS/E adatper to the new schema format

--- trunk/ieee.odm.test/src/org/ieee/odm/ODMAdapterTestSuite.java
+++ trunk/ieee.odm.test/src/org/ieee/odm/ODMAdapterTestSuite.java
@@ -3,11 +3,11 @@ package org.ieee.odm;
 import org.ieee.odm.bpa.BPA_ODMTest;
 import org.ieee.odm.ge.GE_ODMTest;
 import org.ieee.odm.ieeecdf.IEEECDF_ODMTest;
-import org.ieee.odm.psse.old.PSSEV26_ODMTest;
-import org.ieee.odm.psse.old.PSSEV30_GuideSampleTest;
-import org.ieee.odm.psse.old.PSSEV30_NEISO_ODMTest;
-import org.ieee.odm.psse.old.PSSEV30_ODMTest;
-import org.ieee.odm.psse.old.PSSEV30_SegmentTest;
+import org.ieee.odm.psse.PSSEV26_ODMTest;
+import org.ieee.odm.psse.PSSEV30_GuideSampleTest;
+import org.ieee.odm.psse.PSSEV30_NEISO_ODMTest;
+import org.ieee.odm.psse.PSSEV30_ODMTest;
+import org.ieee.odm.psse.PSSEV30_SegmentTest;
 import org.ieee.odm.ucte.JaxbUCTE_ODMTest;
 import org.ieee.odm.ucte.UCTE_ODMTest;
 import org.junit.runner.RunWith;
@@ -27,6 +27,12 @@ import org.junit.runners.Suite.SuiteClasses;
 	
 	GE_ODMTest.class,
 	
+	org.ieee.odm.psse.old.PSSEV30_ODMTest.class,
+	org.ieee.odm.psse.old.PSSEV30_NEISO_ODMTest.class,
+	org.ieee.odm.psse.old.PSSEV26_ODMTest.class,
+	org.ieee.odm.psse.old.PSSEV30_GuideSampleTest.class,
+	org.ieee.odm.psse.old.PSSEV30_SegmentTest.class,
+
 	PSSEV30_ODMTest.class,
 	PSSEV30_NEISO_ODMTest.class,
 	PSSEV26_ODMTest.class,
