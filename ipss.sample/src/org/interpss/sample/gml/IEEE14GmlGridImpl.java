138a1a706c1159a159a187226e80ed00dc454112
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-08-19 13:30:26 +0000

    added multi-case sample

--- ipss.sample/src/org/interpss/sample/gml/IEEE14GmlGridImpl.java
+++ ipss.sample/src/org/interpss/sample/gml/IEEE14GmlGridImpl.java
@@ -49,14 +49,19 @@ public class IEEE14GmlGridImpl {
 		
 		int caseNo = 0;		
 		for (GraphType graph : glist) {
+			// for each Gml graph, create a grid computing study case
 			String caseId = graph.getId() != null? graph.getId() : ("CaseId_");
 			AclfStudyCase studyCase = SimuObjectFactory.createAclfStudyCase(caseId+(++caseNo), 
 					graph.getDesc(), caseNo, mCaseContainer);
+			// use the modification object to send the graph object to remote node
 			Modification mod = SimuObjectFactory.createModification();
 			mod.setAny(GmlHelper.toXmlString(graph));
 			studyCase.setModification(mod);			
 		}
 		
+		// perform multi-case grid computing. The case will distributed to slave grid nodes
+		// in random fashion. The actual work is done by the MyAclfMultiJobTask class, which
+		// has to be implemented by the implementor
 		Grid grid = GridHelper.initGridEnv();
 		if (grid != null) {
 			long timeout = 0;
