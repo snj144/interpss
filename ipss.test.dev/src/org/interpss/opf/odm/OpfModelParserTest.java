8f1c1c635f6bdc8247ccfc543e71d84a0b69b458
scutparis <scutparis@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-08-29 13:13:58 +0000

    opf test;  find a problem with the output operation "toxmldoc()" <pss:ratingLimit /> not properly output

---
+++ ipss.test.dev/src/org/interpss/opf/odm/OpfModelParserTest.java
@@ -0,0 +1,22 @@
+package com.interpss.opf.odm;
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileInputStream;
+import org.ieee.odm.model.opf.*;
+import org.ieee.odm.schema.OpfNetworkXmlType;
+import org.junit.Test;
+public class OpfModelParserTest {
+	@Test
+	public void testOpfModelParser()throws Exception{
+	
+	FileInputStream in =new FileInputStream("testdata/ieee_odm/opf_3bus_test.xml");
+	OpfModelParser OpfParser = new OpfModelParser(in);//("testdata/opf_3bus_test.xml");
+	System.out.println(OpfParser.toXmlDoc(false));	
+	//OpfModelParser OpfParser = new OpfModelParser("testdata/opf_3bus_test.xml");
+    OpfNetworkXmlType opfNet=OpfParser.getOpfNet();
+    assertTrue(opfNet.getBusList().getBus().size()==3);
+    assertTrue(opfNet.getAnglePenaltyFactor()==1);
+    assertTrue(opfNet.getBranchList().getBranch().size()==3);
+	
+    }
+}
