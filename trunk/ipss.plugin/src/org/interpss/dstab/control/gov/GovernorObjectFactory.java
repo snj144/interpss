6bb1e92e9e695e529764d45a7d0f91941539f498
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-08-30 17:23:54 +0000

    working on DStab ODM to InterPSS mapper

--- trunk/ipss.plugin/src/org/interpss/dstab/control/gov/GovernorObjectFactory.java
+++ trunk/ipss.plugin/src/org/interpss/dstab/control/gov/GovernorObjectFactory.java
@@ -1,5 +1,37 @@
+/*
+ * @(#)GovernorObjectFactory.java   
+ *
+ * Copyright (C) 2008-2010 www.interpss.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @Author Mike Zhou
+ * @Version 1.0
+ * @Date 08/15/2010
+ * 
+ *   Revision History
+ *   ================
+ *
+ */
+
 package org.interpss.dstab.control.gov;
 
-public class GovernorObjectFactory {
+import org.interpss.dstab.control.gov.simple.SimpleGovernor;
 
+import com.interpss.dstab.mach.Machine;
+
+public class GovernorObjectFactory {
+	public static SimpleGovernor createSimpleGovernor(String id, String name, Machine machine) {
+		SimpleGovernor gov = new SimpleGovernor(id, name, "InterPSS");
+		gov.setMachine(machine); 
+		return gov;
+  	}
 }
