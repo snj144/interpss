ffb7fda4db68190130e19544020aec33128d2fd3
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-09-04 14:12:57 +0000

    working on SC mapper

--- trunk/ipss.plugin/src/org/interpss/mapper/odm/impl/ODMAcscDataMapperImpl.java
+++ trunk/ipss.plugin/src/org/interpss/mapper/odm/impl/ODMAcscDataMapperImpl.java
@@ -24,11 +24,21 @@
 
 package org.interpss.mapper.odm.impl;
 
+import org.apache.commons.math.complex.Complex;
+import org.ieee.odm.schema.GroundingXmlType;
+import org.ieee.odm.schema.ShortCircuitBusEnumType;
 import org.ieee.odm.schema.ShortCircuitBusXmlType;
 import org.ieee.odm.schema.ShortCircuitNetXmlType;
+import org.ieee.odm.schema.ZXmlType;
+import org.interpss.mapper.odm.ODMXmlHelper;
 
+import com.interpss.common.datatype.Constants;
+import com.interpss.common.exp.InterpssException;
 import com.interpss.core.acsc.AcscBus;
 import com.interpss.core.acsc.AcscNetwork;
+import com.interpss.core.acsc.BusGroundCode;
+import com.interpss.core.acsc.BusScCode;
+import com.interpss.core.acsc.SequenceCode;
 
 
 public class ODMAcscDataMapperImpl {
@@ -43,7 +53,54 @@ public class ODMAcscDataMapperImpl {
 		net.setPositiveSeqDataOnly(xmlNet.isPositiveSeqDataOnly());
 	}	
 
-	public static void setAcdcBusData(ShortCircuitBusXmlType acscBusXml, AcscBus acscBus) {
-		//if (dstabBusXml.getMachine());
+	/**
+	 * Set SC bus info only
+	 * 
+	 * @param acscBusXml
+	 * @param acscBus
+	 */
+	public static void setAcdcBusData(ShortCircuitBusXmlType acscBusXml, AcscBus acscBus) throws InterpssException {
+		if (acscBusXml.getScCode() == ShortCircuitBusEnumType.CONTRIBUTING) {
+			setContributeBusFormInfo(acscBusXml, acscBus);
+		} else { // non-contributing
+			setNonContributeBusFormInfo(acscBus);
+		} 
+	}
+	
+	private static void setNonContributeBusFormInfo(AcscBus acscBus) {
+		acscBus.setScCode(BusScCode.NON_CONTRI);
+		acscBus.setZ(Constants.LargeBusZ, SequenceCode.POSITIVE);
+		acscBus.setZ(Constants.LargeBusZ, SequenceCode.NEGATIVE);
+		acscBus.setZ(Constants.LargeBusZ, SequenceCode.ZERO);
+		acscBus.getGrounding().setCode(BusGroundCode.UNGROUNDED);
+		acscBus.getGrounding().setZ(Constants.LargeBusZ);
+	}
+	
+	
+	private static void setContributeBusFormInfo(ShortCircuitBusXmlType busData, AcscBus acscBus) {
+		acscBus.setScCode(BusScCode.CONTRIBUTE);
+		if (busData.getScGenData() != null) {
+			setBusScZ(acscBus, acscBus.getNetwork().getBaseKva(), 
+					busData.getScGenData().getPotiveZ(),
+					busData.getScGenData().getNegativeZ(),
+					busData.getScGenData().getZeroZ());
+			setBusScZg(acscBus, acscBus.getBaseVoltage(), acscBus.getNetwork().getBaseKva(), 
+					busData.getScGenData().getGrounding());
+		}
+	}
+	
+	private static void setBusScZ(AcscBus bus, double baseKVA, 
+			ZXmlType z1, ZXmlType z2, ZXmlType z0) {
+		byte zUnit = ODMXmlHelper.toUnit(z1.getUnit());
+		bus.setZ(new Complex(z1.getRe(), z1.getIm()), SequenceCode.POSITIVE, zUnit);
+		bus.setZ(new Complex(z2.getRe(), z2.getIm()), SequenceCode.NEGATIVE, zUnit);
+		bus.setZ(new Complex(z0.getRe(), z0.getIm()), SequenceCode.ZERO, zUnit);
+	}
+
+	private static void setBusScZg(AcscBus bus, double baseV, double baseKVA, GroundingXmlType g) {
+		ZXmlType z = g.getGroundZ();
+		byte zgUnit = ODMXmlHelper.toUnit(z.getUnit());
+		bus.getGrounding().setCode(ODMXmlHelper.toBusGroundCode(g.getConnection()));
+		bus.getGrounding().setZ(new Complex(z.getRe(), z.getIm()), zgUnit, baseV, baseKVA);
 	}
 }
