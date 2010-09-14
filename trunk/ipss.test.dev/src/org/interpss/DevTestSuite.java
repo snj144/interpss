91d86a76b0fcd5edd66b4ab1005e71e702012e5e
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-09-14 17:02:17 +0000

    added DevTestSuite.java

---
+++ trunk/ipss.test.dev/src/org/interpss/DevTestSuite.java
@@ -0,0 +1,13 @@
+package org.interpss;
+
+import org.interpss.acsc.odm.Acsc_5BusTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	Acsc_5BusTest.class
+})
+public class DevTestSuite {
+}
