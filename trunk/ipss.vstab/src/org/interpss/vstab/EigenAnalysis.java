dacea318bbbec6b974c057ab7da988ce276dc88b
scutparis <scutparis@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-09-15 17:39:44 +0000

    test Vstab ESA method, error with jacobi matrix formation ,needed to work on

--- trunk/ipss.vstab/src/org/interpss/vstab/EigenAnalysis.java
+++ trunk/ipss.vstab/src/org/interpss/vstab/EigenAnalysis.java
@@ -3,7 +3,6 @@ package org.interpss.vstab;
 import org.apache.commons.math.linear.RealMatrix;
 
 public interface EigenAnalysis {
-	public void setJacobiMatrix(RealMatrix jacobi);
 	public void runEigenStrAnalysis();
 	public ESAResult getESAResult();
 
