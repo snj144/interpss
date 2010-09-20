d24f6427e0d28711f1d22c4b60468b1fd295d748
scutparis <scutparis@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-09-20 16:55:48 +0000

    a simple OpfFuncOut implementation finished

--- trunk/ipss.opf/src/org/interpss/opf/dc/OpfFuncOut.java
+++ trunk/ipss.opf/src/org/interpss/opf/dc/OpfFuncOut.java
@@ -1,21 +1,31 @@
 package org.interpss.opf.dc;
 
 import com.interpss.core.net.Bus;
+import com.interpss.opf.OpfGenBus;
 import com.interpss.opf.OpfNetwork;
 
 public class OpfFuncOut {
 	public static String opfResultSummary(OpfNetwork opfnet) {
+		 double minTVC=0;
+		 double minTotalCost=0;
 		 final StringBuffer str = new StringBuffer("");
 		 str.append("           ---DCOPF SOLUTION RESULT SUMMARY---            \n");
-		 str.append("busID    isOpfGen     optimGen   optimBusAngle \n");
-		 str.append("-----------------------------------------------\n");
+		 str.append("busID    isOpfGen     optimGen(PU)   optimBusAngle \n");
+		 str.append("----------------------------------------------------\n");
 		 for(Bus b:opfnet.getBusList()) {
 			 str.append(b.getId());
-			 if(opfnet.isOpfGenBus(b))str.append(   "Ture"   );
-			 else str.append(   "False"   );
-			 
-			 
+			 if(opfnet.isOpfGenBus(b)){
+				 OpfGenBus opfBus=(OpfGenBus) b;
+				 
+				 str.append("   Ture      "   +opfBus.getGenP());
+				 minTVC+=opfBus.getCoeffA()*opfBus.getGenP()+opfBus.getCoeffB()*Math.pow(opfBus.getGenP(), 2);
+				 minTotalCost=minTVC+opfBus.getFixedCost();	
+			 }
+			 else str.append("     False                    ");
+			 str.append(((OpfGenBus)b).getVoltageAng()+"\n");	 
 		 }
+		 str.append("Minimun Total Variable Cost: "+minTVC+"\n");
+		 str.append("Minimun Total Cost: "+minTotalCost+"\n");
 		 return str.toString();
 	}
 //	public static String opfSolutionSummary(OpfQuadProgAlgorithm opfAlgo) {
