bd7ea9e05bf7e07f57778e539b41ce5b9e3b6eb3
scutparis <scutparis@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-09-21 10:10:35 +0000

    1)opfOutFunc update

--- trunk/ipss.opf/src/org/interpss/opf/dc/OpfOutFunc.java
+++ trunk/ipss.opf/src/org/interpss/opf/dc/OpfOutFunc.java
@@ -4,28 +4,32 @@ import com.interpss.core.net.Bus;
 import com.interpss.opf.OpfGenBus;
 import com.interpss.opf.OpfNetwork;
 
-public class OpfFuncOut {
+public class OpfOutFunc {
 	public static String opfResultSummary(OpfNetwork opfnet) {
 		 double minTVC=0;
 		 double minTotalCost=0;
 		 final StringBuffer str = new StringBuffer("");
 		 str.append("           ---DCOPF SOLUTION RESULT SUMMARY---            \n");
-		 str.append("busID    isOpfGen     optimGen(PU)   optimBusAngle \n");
+		 str.append("----------------------------------------------------\n");
+		 str.append("busID    isOpfGen     optimGen(PU)   optimAngle(DEG) \n");
 		 str.append("----------------------------------------------------\n");
 		 for(Bus b:opfnet.getBusList()) {
 			 str.append(b.getId());
 			 if(opfnet.isOpfGenBus(b)){
 				 OpfGenBus opfBus=(OpfGenBus) b;
 				 
-				 str.append("   Ture      "   +opfBus.getGenP());
+				 str.append(String.format("%10s","Ture"));
+				 str.append(String.format("%14.3f",opfBus.getGenP()));
 				 minTVC+=opfBus.getCoeffA()*opfBus.getGenP()+opfBus.getCoeffB()*Math.pow(opfBus.getGenP(), 2);
 				 minTotalCost=minTVC+opfBus.getFixedCost();	
 			 }
-			 else str.append("     False                    ");
-			 str.append(((OpfGenBus)b).getVoltageAng()+"\n");	 
+			 else str.append(String.format("%10s","False"));
+			 str.append(String.format("%17.2f",((OpfGenBus)b).getVoltageAng()));	
+			 str.append("\n");
 		 }
-		 str.append("Minimun Total Variable Cost: "+minTVC+"\n");
-		 str.append("Minimun Total Cost: "+minTotalCost+"\n");
+		 str.append("----------------------------------------------------\n\n");
+		 str.append(String.format("Minimun Total Variable Cost: %6.3f",minTVC)+"\n");
+		 str.append(String.format("Minimun Total Cost: %6.3f",minTotalCost)+"\n");
 		 return str.toString();
 	}
 //	public static String opfSolutionSummary(OpfQuadProgAlgorithm opfAlgo) {
