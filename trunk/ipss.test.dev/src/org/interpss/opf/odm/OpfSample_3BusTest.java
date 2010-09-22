4d7ebc6d1da796b4df30f0885e0621cab11d6f21
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-09-22 16:38:16 +0000

    code refactor by Mike

--- trunk/ipss.test.dev/src/org/interpss/opf/odm/OpfSample_3BusTest.java
+++ trunk/ipss.test.dev/src/org/interpss/opf/odm/OpfSample_3BusTest.java
@@ -31,6 +31,7 @@ import org.ieee.odm.ODMObjectFactory;
 import org.ieee.odm.model.opf.OpfModelParser;
 import org.interpss.BaseTestSetup;
 import org.interpss.mapper.odm.IEEEODMMapper;
+import org.interpss.opf.dc.OpfOutFunc;
 import org.interpss.opf.dc.OpfQuadProgAlgorithm;
 import org.interpss.opf.dc.OpfQuadProgAlgorithmImpl;
 import org.junit.Test;
@@ -56,10 +57,12 @@ public class OpfSample_3BusTest  extends BaseTestSetup {
 			}	
 			
 			OpfNetwork opfNet = simuCtx.getOpfNet();
-			System.out.println(opfNet.net2String());
+			//System.out.println(opfNet.net2String());
 
 			OpfQuadProgAlgorithm opfAlgo=new OpfQuadProgAlgorithmImpl(opfNet,msg);
 			opfAlgo.runDCOPF();
+			
+			System.out.println(OpfOutFunc.opfResultSummary(opfNet));
 		}
 	}
 }
