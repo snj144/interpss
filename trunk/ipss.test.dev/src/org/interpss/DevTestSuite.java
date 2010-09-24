b33a2a6cf8d6e8108cb675e8f97426ee36fd6abe
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-09-24 16:47:16 +0000

    complete unit test

--- trunk/ipss.test.dev/src/org/interpss/DevTestSuite.java
+++ trunk/ipss.test.dev/src/org/interpss/DevTestSuite.java
@@ -1,13 +1,18 @@
 package org.interpss;
 
 import org.interpss.acsc.odm.Acsc_5BusTest;
+import org.interpss.opf.odm.OpfSample_3BusTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-	Acsc_5BusTest.class
+	//Acsc
+	Acsc_5BusTest.class,
+	
+	// OPF
+	OpfSample_3BusTest.class
 })
 public class DevTestSuite {
 }
