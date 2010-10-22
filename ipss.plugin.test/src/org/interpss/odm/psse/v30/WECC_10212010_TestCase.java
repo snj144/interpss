24112a78a4e91dc9086ed44e90b2fcb91b797b3d
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-10-22 01:17:25 +0000

    differentiate LoadflowAlgo and AclfAdjAlgo

--- ipss.plugin.test/src/org/interpss/odm/psse/v30/WECC_10212010_TestCase.java
+++ ipss.plugin.test/src/org/interpss/odm/psse/v30/WECC_10212010_TestCase.java
@@ -26,27 +26,41 @@ package org.interpss.odm.psse.v30;
 
 import static org.junit.Assert.assertTrue;
 
-import java.util.logging.Level;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
-
 import org.ieee.odm.adapter.IODMPSSAdapter;
 import org.ieee.odm.adapter.psse.v30.PSSEV30Adapter;
-import org.ieee.odm.model.aclf.AclfModelParser;
 import org.interpss.BaseTestSetup;
+import org.interpss.mapper.odm.IEEEODMMapper;
 import org.junit.Test;
 
 import com.interpss.common.util.IpssLogger;
+import com.interpss.core.CoreObjectFactory;
+import com.interpss.core.aclf.netAdj.AclfAdjNetwork;
+import com.interpss.core.algorithm.AclfMethod;
+import com.interpss.core.algorithm.LoadflowAlgorithm;
+import com.interpss.simu.SimuContext;
+import com.interpss.simu.SimuCtxType;
+import com.interpss.simu.SimuObjectFactory;
 
 public class WECC_10212010_TestCase extends BaseTestSetup { 
 	@Test
 	public void testCase1() throws Exception {
 		IODMPSSAdapter adapter = new PSSEV30Adapter(IpssLogger.getLogger());
-		assertTrue(adapter.parseInputFile("testdata/psse/PSSE_5Bus_Test.raw"));
+		assertTrue(adapter.parseInputFile("testdata/psse/v30/WECC_10212010_09hs2sbp.raw"));
 		
-		AclfModelParser parser = (AclfModelParser)adapter.getModel();
-		//parser.stdout();		
-		
+		IEEEODMMapper mapper = new IEEEODMMapper();
+		SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.ACLF_ADJ_NETWORK, msg);
+		if (!mapper.mapping(adapter.getModel(), simuCtx, SimuContext.class)) {
+  	  		System.out.println("Error: ODM model to InterPSS SimuCtx mapping error, please contact support@interpss.com");
+  	  		return;
+		}	
+  		simuCtx.setName("Sample");
+ 	  	simuCtx.setDesc("This project is created by input file adapter.getModel()");
+ 	  	AclfAdjNetwork net = simuCtx.getAclfAdjNet();
+ 	  	
+	  	LoadflowAlgorithm algo = CoreObjectFactory.createAclfAdjAlgorithm(net, msg);
+	  	algo.setLfMethod(AclfMethod.NR);
+	  	algo.setNonDivergent(true);
+	  	algo.loadflow(); 	  	
 	}
 }
 
