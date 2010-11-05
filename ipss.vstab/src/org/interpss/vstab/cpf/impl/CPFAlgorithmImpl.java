2cf05ca4df8a2c14292d81b425613057531d94e4
erzhuanzhou <erzhuanzhou@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-11-05 19:49:57 +0000

    correct package typo

---
+++ ipss.vstab/src/org/interpss/vstab/cpf/impl/CPFAlgorithmImpl.java
@@ -0,0 +1,128 @@
+package org.interpss.vstab.cpf.impl;
+
+import org.interpss.vstab.cpf.CPFAlgorithm;
+import org.interpss.vstab.cpf.CPFSolver;
+import org.interpss.vstab.cpf.CpfStopCriteria;
+import org.interpss.vstab.cpf.GenDispPatten;
+import org.interpss.vstab.cpf.LoadIncPatten;
+import org.interpss.vstab.cpf.CpfStopCriteria.AnalysisStopCriteria;
+
+import com.interpss.common.datatype.UnitType;
+import com.interpss.common.msg.IPSSMsgHub;
+import com.interpss.core.aclf.AclfNetwork;
+
+public class CPFAlgorithmImpl implements CPFAlgorithm{
+	
+    protected int maxInterations;
+    protected IPSSMsgHub msg=null;
+    protected double tolerance;
+    protected AclfNetwork acNetwork=null;
+    protected AnalysisStopCriteria stopCriteria=null;
+    protected GenDispPatten genDispPtn=null;
+    protected LoadIncPatten loadIncPtn=null;
+    protected CPFSolver cpfSolver=null;
+  
+    
+
+	@Override
+	public int getMaxIterations() {
+		
+		return this.maxInterations;
+	}
+
+	@Override
+	public IPSSMsgHub getMsgHub() {
+		return this.msg;
+	}
+
+	@Override
+	public double getTolerance(byte unit) {
+		if(unit==UnitType.mVA) {
+			return this.tolerance*this.acNetwork.getBaseKva()/1000;
+		}
+		else if(unit==UnitType.PU) {
+			return this.tolerance;
+		}
+		else {
+			getMsgHub().sendErrorMsg("unit parameter must be either UnitType.mVA or PU");
+			return 0;
+		}
+		
+	}
+	@Override
+	public void setTolerance(double tol, byte unit) {
+		if(unit==UnitType.mVA) {
+			this.tolerance=tol/this.acNetwork.getBaseKva()*1000;
+		}
+		else if(unit==UnitType.PU) {
+			this.tolerance=tol;
+		}
+		else {
+			getMsgHub().sendErrorMsg("unit parameter must be either UnitType.mVA or PU");
+		}
+		
+	}
+
+	@Override
+	public boolean runCPF() {
+		cpfSolver= new CPFSolverImpl(acNetwork, loadIncPtn,genDispPtn);
+		cpfSolver.solveCPF();
+		return cpfSolver.isCPFConverged();
+	}
+
+	@Override
+	public void setAnalysisCode(AnalysisStopCriteria newStopCriteria) {
+		this.stopCriteria=newStopCriteria;
+		
+	}
+
+	@Override
+	public void setGenDispPatten(GenDispPatten newGenDispPtn) {
+		this.genDispPtn=newGenDispPtn;
+		
+	}
+
+	@Override
+	public void setLoadIncPatten(LoadIncPatten newLoadIncPtn) {
+		this.loadIncPtn=newLoadIncPtn;
+		
+	}
+
+	@Override
+	public void setMaxIterations(int maxIter) {
+		this.maxInterations=maxIter;
+		
+	}
+
+	@Override
+	public void setMsgHub(IPSSMsgHub paramIPSSMsgHub) {
+		this.msg=paramIPSSMsgHub;
+		
+	}
+
+	@Override
+	public AnalysisStopCriteria getAnalysisCode() {
+		return this.stopCriteria;
+	}
+
+	@Override
+	public GenDispPatten getGenDispPatten() {
+		return this.genDispPtn;
+		
+	}
+
+	@Override
+	public LoadIncPatten getLoadIncPatten() {
+		return this.loadIncPtn;
+		
+	}
+
+	@Override
+	public double getTolerance() {
+		
+		return this.tolerance;
+	}
+
+
+
+}
---
+++ ipss.vstab/src/org/interpss/vstab/cpf/impl/CpfHelper.java
@@ -0,0 +1,52 @@
+package org.interpss.vstab.cpf.impl;
+/**
+ * This is a helper class to conduct internal operations of Continuous Power Flow(CPF),
+ * including prediction, correction and automatic step size control.
+ */
+
+import org.apache.commons.math.linear.RealMatrix;
+
+import com.interpss.core.aclf.AclfNetwork;
+
+public class CpfHelper {
+	private RealMatrix operatingLFResult=null;
+	private RealMatrix predResult=null;
+	private RealMatrix corrResult=null;
+	private double stepSize=0;
+	
+	private AclfNetwork net=null;
+    
+	public CpfHelper(AclfNetwork _net){
+		this.net=_net;
+	}
+    public RealMatrix getPredictedResult(double stepLength){
+    	 //1. get the jacobian matrix 
+    	
+    	// 2. augment the Jacobi with the differentiation of load flow equation to Lamda(the load ang generation increase index) 
+        
+    	
+    	// 3. solve Jau*[dx,dLamda]T=[0,+-1]
+    	
+    	/* 4. get the predicted result by solving the following:
+    	   new[x,Lambda]=lastsetp[x,Lambda]+stepLength*[dx,dLambda]
+    	*/
+		return predResult;
+    
+    }
+    
+    public RealMatrix getCorrectedResult(){
+    	/*
+    	 * 1. augment the traditional power flow equation with continuous parameter equation
+    	 * 2. solve the augmented equation:[f(x,lambda),xl-xl_fixed]T=0
+    	 */  
+    	return corrResult;
+    	
+    }
+    
+    public double getOptimStep(){
+    	
+    	return stepSize;
+    	
+    }
+    
+}
