7df91dda3df99ca861022139e0bf848e3244952b
erzhuanzhou <erzhuanzhou@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-11-23 23:46:42 +0000

    working DcNetworkParser

--- ieee.odm_pss/src/org/ieee/odm/ODMObjectFactory.java
+++ ieee.odm_pss/src/org/ieee/odm/ODMObjectFactory.java
@@ -2,16 +2,18 @@ package org.ieee.odm;
 
 import org.ieee.odm.model.aclf.AclfModelParser;
 import org.ieee.odm.model.acsc.AcscModelParser;
+import org.ieee.odm.model.dc.DcSystemModelParser;
 import org.ieee.odm.model.dstab.DStabModelParser;
 import org.ieee.odm.model.opf.OpfModelParser;
 
 public class ODMObjectFactory {
 	public static AclfModelParser createAclfModelParser() {
 		AclfModelParser parser = new AclfModelParser();
-//		ProxyFactory pf = new ProxyFactory();
-//		pf.setTarget(parser);
-//		pf.addAdvice(new ODMThrowableAdvice());
-//		return (AclfModelParser)pf.getProxy();		
+		return parser;
+	}
+
+	public static DcSystemModelParser createDcSystemModelParser() {
+		DcSystemModelParser parser = new DcSystemModelParser();
 		return parser;
 	}
 
@@ -24,6 +26,7 @@ public class ODMObjectFactory {
 		DStabModelParser parser = new DStabModelParser();
 		return parser;
 	}
+	
 	public static AcscModelParser createAcscModelParser() {
 		AcscModelParser parser = new AcscModelParser();
 		return parser;
