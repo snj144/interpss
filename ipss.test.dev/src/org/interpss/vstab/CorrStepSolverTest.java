93827f6d914d2a44f51430d22877837baf64031a
scutparis <scutparis@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-11-27 08:36:53 +0000

    corrector step test failed, due to singularity (aii=0) while a bus voltage magnitude is chosen as the continuation parameter

--- ipss.test.dev/src/org/interpss/vstab/CorrStepSolverTest.java
+++ ipss.test.dev/src/org/interpss/vstab/CorrStepSolverTest.java
@@ -1,5 +1,7 @@
 package org.interpss.vstab;
 
+import static org.junit.Assert.assertTrue;
+
 import org.interpss.BaseTestSetup;
 import org.interpss.display.AclfOutFunc;
 import org.interpss.vstab.cpf.CPFAlgorithm;
@@ -14,6 +16,9 @@ import com.interpss.core.algorithm.LoadflowAlgorithm;
 import com.interpss.simu.util.sample.SampleCases;
 
 public class CorrStepSolverTest extends BaseTestSetup {
+	/**
+	 * Test Lambda as the continuation parameter;
+	 */
 	@Test
 	public void testCase1() {
     IPSSMsgHub msg = SpringAppContext.getIpssMsgHub();
@@ -22,16 +27,45 @@ public class CorrStepSolverTest extends BaseTestSetup {
 	AclfNetwork net = CoreObjectFactory.createAclfNetwork();
 	SampleCases.load_LF_5BusSystem(net, msg);
 	LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(msg);
-	net.accept(algo);
-	System.out.println(AclfOutFunc.loadFlowSummary(net)); // output load flow result 
+//	net.accept(algo);
+//	System.out.println(AclfOutFunc.loadFlowSummary(net)); // output load flow result 
 	
 	// create CPF algorithm;
 	CPFAlgorithm cpfAlgo = VStabObjectFactory.createCPFAlgorithmImpl(net, msg);
+	double fixVal=2.5;
+	cpfAlgo.setFixedValOfContPara(fixVal);// set fixval to 1.5 intentionally for testing
 	CorrectorStepSolver corSolver=cpfAlgo.getCorrStepSolver();
-	
 	algo.setNrSolver(corSolver);
 	net.accept(algo);
-	System.out.println("lambda="+cpfAlgo.getLambdaParam().getVal());
-	System.out.println(AclfOutFunc.loadFlowSummary(net));
+	assertTrue(net.isLfConverged());
+	assertTrue((cpfAlgo.getLambdaParam().getVal()-fixVal)<1e-5);
+//	System.out.println("lambda="+cpfAlgo.getLambdaParam().getVal());
+//	System.out.println(AclfOutFunc.loadFlowSummary(net));
+	}
+	
+	/**
+	 * Test the bus voltage magnitude as the continuation parameter;
+	 */
+	@Test
+	public void testCase2() {
+		    IPSSMsgHub msg = SpringAppContext.getIpssMsgHub();
+			
+			// create a sample 5-bus system for Loadflow 
+			AclfNetwork net = CoreObjectFactory.createAclfNetwork();
+			SampleCases.load_LF_5BusSystem(net, msg);
+			LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(msg);
+			
+			// create CPF algorithm;
+			CPFAlgorithm cpfAlgo = VStabObjectFactory.createCPFAlgorithmImpl(net, msg);
+			double fixVal=0.956;
+			cpfAlgo.setSorNumofContParam(4);// sortNumber=4 ,namely vmag of bus2 is selected as the continuation parameter
+			cpfAlgo.setFixedValOfContPara(fixVal);// set fixedVal to 1.5 intentionally for testing
+			CorrectorStepSolver corSolver=cpfAlgo.getCorrStepSolver();
+			algo.setNrSolver(corSolver);
+			net.accept(algo);
+			assertTrue(net.isLfConverged());
+			assertTrue((net.getAclfBus("2").getVoltageMag()-fixVal)<1e-5);// bus2
+//			System.out.println("lambda="+cpfAlgo.getLambdaParam().getVal());
+//			System.out.println(AclfOutFunc.loadFlowSummary(net));
 	}
 }
