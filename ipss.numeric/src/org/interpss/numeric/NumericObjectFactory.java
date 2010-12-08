95ce46c88b3abdadd56c1be1cfa589872f8dc1d3
erzhuanzhou <erzhuanzhou@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-12-08 13:38:40 +0000

    added comments and copy right text

--- ipss.numeric/src/org/interpss/numeric/NumericObjectFactory.java
+++ ipss.numeric/src/org/interpss/numeric/NumericObjectFactory.java
@@ -1,9 +1,44 @@
+/*
+ * @(#)NumericObjectFactory.java   
+ *
+ * Copyright (C) 2006-2010 www.interpss.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @Author Mike Zhou
+ * @Version 1.0
+ * @Date 12/10/2010
+ * 
+ *   Revision History
+ *   ================
+ *
+ */
+
 package org.interpss.numeric;
 
 import org.interpss.numeric.sparse.SparseEqnDouble;
 import org.interpss.spring.NumericSpringCtx;
 
+/**
+ * for creating object using its default implementation
+ * 
+ * @author mzhou
+ *
+ */
 public class NumericObjectFactory {
+	/**
+	 * create a SparseEqnDouble object
+	 * 
+	 * @return
+	 */
 	public static SparseEqnDouble createSparseEqnDouble() {
 		return NumericSpringCtx.getSparseEqnDouble();
 	}
--- ipss.numeric/src/org/interpss/numeric/util/NumericUtil.java
+++ ipss.numeric/src/org/interpss/numeric/util/NumericUtil.java
@@ -1,11 +1,41 @@
+/*
+ * @(#)NumericUtil.java   
+ *
+ * Copyright (C) 2006-2010 www.interpss.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @Author Mike Zhou
+ * @Version 1.0
+ * @Date 12/10/2010
+ * 
+ *   Revision History
+ *   ================
+ *
+ */
+
 package org.interpss.numeric.util;
 
 public class NumericUtil {
+	private static final double ERR = 1.0e-10;
+	
 	public static boolean equals(int x, int y) {
 		return x == y;
 	}
 
 	public static boolean equals(double x, double y) {
-		return Math.abs(x - y) < 1.0e-10;
+		return equals(x, y, ERR);
+	}
+
+	public static boolean equals(double x, double y, double err) {
+		return Math.abs(x - y) < err;
 	}
 }
