fdb9f177779c77872064ffde7f97afe8b8a63ecc
erzhuanzhou <erzhuanzhou@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-12-10 13:19:02 +0000

    added comments

--- ipss.sample/src/org/interpss/sample/net/IEEE14_WalkThrouhg.java
+++ ipss.sample/src/org/interpss/sample/net/IEEE14_WalkThrouhg.java
@@ -1,3 +1,26 @@
+ /*
+  * @(#)IEEE14_WalkThrouhg.java   
+  *
+  * Copyright (C) 2006-2010 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 12/10/2010
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
 package org.interpss.sample.net;
 
 import java.util.logging.Level;
@@ -20,11 +43,11 @@ public class IEEE14_WalkThrouhg {
 	 * @param args
 	 */
 	public static void main(String[] args) throws Exception {
-		// InterPSS core simulation engine configuration
+		// InterPSS plugin initialization
 		IpssPlugin.init(Level.WARNING);
 
     	/*
-    	 * step-1 Build the base case
+    	 * step-1 input the base case 
     	 */
 		AclfNetwork net = PluginObjectFactory
 				.getFileAdapter(IpssFileAdapter.FileFormat.IEEECDF)
@@ -37,6 +60,10 @@ public class IEEE14_WalkThrouhg {
 	  	IAclfNetBVisitor algo = CoreObjectFactory.createLfAlgoVisitor();;
 	  	net.accept(algo);
 
+	  	/*
+	  	 * Step-3 define the walk through algorithm. The algorithm is nothing other then 
+	  	 *        print out bus and branch id
+	  	 */
   		ActivePowerWalkThrough walkAlgo = CoreObjectFactory.createActivePowerWalkThrough();
   		walkAlgo.setBusVisitor(new IBusBVisitor() {
 			@Override
@@ -53,10 +80,16 @@ public class IEEE14_WalkThrouhg {
 			}
   		});
   		
+  		/*
+  		 * Step-4 Walk through the network from source to load along the active power flow direction
+  		 */
 		System.out.println("Source to Load direction");
   		walkAlgo.setDirection(ActivePowerWalkDirectionEnum.SOURCE_TO_LOAD);
   		net.accept(walkAlgo);
   		
+  		/*
+  		 * Step-5 Walk through the network from load to source along the reverse active power flow direction 
+  		 */
 		System.out.println("\nLoad to Source direction");
   		walkAlgo.setDirection(ActivePowerWalkDirectionEnum.LOAD_TO_SOURCE);
   		net.accept(walkAlgo);
