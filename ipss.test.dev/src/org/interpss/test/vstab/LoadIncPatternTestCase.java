748e6aa2c4cc8c4df6a59c724bf8c6b6a42d6b01
scutparis <scutparis@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-12-20 03:19:19 +0000

    Both LoadIncPattern & LoadIncrease Class unit tests

--- ipss.test.dev/src/org/interpss/test/vstab/LoadIncPatternTestCase.java
+++ ipss.test.dev/src/org/interpss/test/vstab/LoadIncPatternTestCase.java
@@ -1,24 +1,26 @@
 package org.interpss.test.vstab;
 
 import static org.junit.Assert.assertTrue;
-
+import org.interpss.custom.IpssFileAdapter;
+import org.interpss.PluginObjectFactory;
 import org.interpss.test.DevTestSetup;
 import org.interpss.vstab.cpf.LoadIncPattern;
-import org.interpss.vstab.cpf.LoadIncPattern.LoadIncPtn;
+import org.interpss.vstab.cpf.LoadIncPattern.LoadIncScope;
+import org.interpss.vstab.cpf.LoadIncPattern.LoadIncType;
 import org.junit.Test;
 
-import com.interpss.common.msg.IPSSMsgHub;
+import com.interpss.common.exp.InterpssException;
 import com.interpss.core.CoreObjectFactory;
+import com.interpss.core.aclf.AclfBus;
 import com.interpss.core.aclf.AclfNetwork;
-import com.interpss.pssl.simu.IpssAclf;
+import com.interpss.core.net.Bus;
 import com.interpss.simu.util.sample.SampleCases;
-import com.interpss.spring.CoreCommonSpringCtx;
+
 
 public class LoadIncPatternTestCase extends DevTestSetup {
 	@Test
 	public void testCase1() throws Exception {
-       IPSSMsgHub msg = IpssAclf.getMsgHub();
-		
+
 		// create a sample 5-bus system for Loadflow 
      
   		AclfNetwork net = CoreObjectFactory.createAclfNetwork();
@@ -30,14 +32,44 @@ public class LoadIncPatternTestCase extends DevTestSetup {
              PVBus(net, "4", 1000.0D, 1, 1, 5.0D, 2, 1.05D, 2);
              SwingBus(net, "5", 4000.0D, 2, 1, 1.05D, 2, 0D, 3);
 		 */
-		System.out.println(net.net2String());
+//		System.out.println(net.net2String());
+		
+		//1. test case: increase by Area scope and constant power factor Type
 		System.out.println("the number of Areas : "+net.getAreaList().size());
 		System.out.println("Area1 Number: "+net.getZoneList().get(0).getNumber());
-        LoadIncPattern ldPtn=new LoadIncPattern(net, msg);
-        Long[] incAreaAry = {1L,2L};    
-        ldPtn.defLoadIncPtn(LoadIncPtn.AREA, net, incAreaAry);
+		Long[] incAreaAry = {1L,2L}; 
+        LoadIncPattern ldPtn=new LoadIncPattern(net,LoadIncScope.AREA,LoadIncType.CONST_PF,incAreaAry);
         assertTrue(ldPtn.getIncBusList().size()==3); 
-        System.out.println(ldPtn.getIncBusList().size());
+        assertTrue(ldPtn.getLoadIncDir().get("1").getReal()-1.6<1e-6);
+
+        
+        //2. increase by network Scope and constant power factor Type
+        ldPtn=new LoadIncPattern(net,LoadIncScope.NETWORK,LoadIncType.ONLY_P,null);
+        assertTrue(ldPtn.getIncBusList().size()==3); 
+        assertTrue(ldPtn.getLoadIncDir().get("2").getImaginary()-0<1e-6);
+      
+        
 	}
+	
+	// 3. test increase  by Bus(s) Scope
+	@Test
+	public void testCase2() throws InterpssException, Exception{
+
+		AclfNetwork net = CoreObjectFactory.createAclfNetwork();
+    	SampleCases.load_LF_5BusSystem(net);
+//		Bus[] bAry={net.getBus("2")};
+		LoadIncPattern ldPtn=new LoadIncPattern(net,LoadIncScope.BUS,LoadIncType.CONST_PF,net.getBusList().toArray());
+		assertTrue(ldPtn.getIncBusList().size()==3);
+		assertTrue(ldPtn.getLoadIncDir().get("2").getReal()-2<1e-6);
+
+	}
+//	public static void main(String[]args ) throws InterpssException, Exception{
+//		
+//		AclfNetwork net=PluginObjectFactory.getFileAdapter(IpssFileAdapter.FileFormat.IEEECDF).load("testData/ieee_cdf/ieee14.ieee").getAclfNet();
+//		Bus[] bAry={net.getBus("Bus4")};
+//		LoadIncPattern ldPtn=new LoadIncPattern(net,LoadIncScope.BUS,LoadIncType.CONST_PF,bAry);
+//		System.out.print(ldPtn.getIncBusList().size());
+//		
+//	}
 
 }
