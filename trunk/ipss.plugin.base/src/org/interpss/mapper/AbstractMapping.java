47510672e9b5d268e96e4ef14627b5268f5cd7e9
erzhuanzhou <erzhuanzhou@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2010-12-31 15:02:32 +0000

    code refactor, moved mapping interface to this package

---
+++ trunk/ipss.plugin.base/src/org/interpss/mapper/AbstractMapping.java
@@ -0,0 +1,59 @@
+package org.interpss.mapper;
+
+import java.util.logging.Logger;
+
+
+import com.interpss.common.exp.InterpssException;
+import com.interpss.common.msg.IPSSMsgHub;
+import com.interpss.common.util.IpssLogger;
+
+public abstract class AbstractMapping<Tf, Tt> implements IMapping<Tf, Tt> {
+	/**
+	 */
+	protected IPSSMsgHub msg = null;
+
+	/**
+	 */
+	protected AbstractMapping() {
+	}
+	
+	/**
+	 */
+	protected AbstractMapping(IPSSMsgHub msg) {
+		this.msg = msg;
+	}
+
+	/**
+	 */
+	public IPSSMsgHub getMsg() {
+		return msg;
+	}
+
+	public Logger getLogger() {
+		return IpssLogger.getLogger();
+	}
+	
+	@Override
+	public boolean map2Model(Tf from, Tt model) {
+		// needs to be implemented by the concrete child adapters
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Tt map2Model(Tf from) throws InterpssException {
+		// needs to be implemented by the concrete child adapters
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public boolean map2ODM(Tf from, Tt odm) {
+		// needs to be implemented by the concrete child adapters
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Tt map2ODM(Tf from)  throws InterpssException {
+		// needs to be implemented by the concrete child adapters
+		throw new UnsupportedOperationException();
+	}
+}
