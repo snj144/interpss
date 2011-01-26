07bbfc16538d719234033ab46cc4423ff670a54c
erzhuanzhou <erzhuanzhou@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-01-26 22:24:09 +0000

    documentation, no code change

--- trunk/ipss.numeric/src/org/interpss/numeric/sparse/impl/SparseEqnDoubleCommonMathImpl.java
+++ trunk/ipss.numeric/src/org/interpss/numeric/sparse/impl/SparseEqnDoubleCommonMathImpl.java
@@ -24,10 +24,6 @@
 
 package org.interpss.numeric.sparse.impl;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
-import java.util.Locale;
-
 import org.apache.commons.math.linear.Array2DRowRealMatrix;
 import org.apache.commons.math.linear.LUDecomposition;
 import org.apache.commons.math.linear.LUDecompositionImpl;
@@ -51,25 +47,30 @@ public class SparseEqnDoubleCommonMathImpl extends AbstractSparseEqnDouble {
 		this.setDimension(n);
 	}
 	
+	@Override
 	public void setDimension(int n) {
 		super.setDimension(n);
 		this.A = new Array2DRowRealMatrix( n, n );
 		this.B = new double[n];
 	}
 	
+	@Override
 	public void addToAij( final double x, final int i, final int j ) {
 		double x0 = this.A.getEntry(i, j);
 		this.A.setEntry(i, j, x0+x);
 	}
 
+	@Override
 	public void setAij( final double x, final int i, final int j ) {
 		this.A.setEntry(i, j, x);
 	}
 		
+	@Override
 	public double getAij( final int i, final int j ) {
 		return this.A.getEntry(i, j);
 	}
 
+	@Override
 	public double getXi( final int i ) {
 		return this.X[i];
 	}
@@ -88,26 +89,5 @@ public class SparseEqnDoubleCommonMathImpl extends AbstractSparseEqnDouble {
 	public void solveEqn() {
 		LUDecomposition lu = new LUDecompositionImpl(A);
 		this.X = lu.getSolver().solve(B);
-	}
-	public String toString(){
-		   
-		   StringBuffer sb=new StringBuffer();
-		   DecimalFormat format = new DecimalFormat();
-		   format.setDecimalFormatSymbols(new DecimalFormatSymbols(Locale.US));
-		   format.setMinimumIntegerDigits(1);
-		   format.setMaximumFractionDigits(3);
-		   format.setMinimumFractionDigits(1);
-		   for(int i=0;i<A.getRowDimension();i++) {
-			   for(int j=0;j<A.getColumnDimension();j++) {
-				   String s=format.format(A.getEntry(i, j));
-				   sb.append(s);
-				   int padding = Math.max(1,5-s.length()); // At _least_ 1 space
-		           for (int k = 0; k < padding; k++) sb.append(" ");
-		                    
-			   }
-			   sb.append("\n");
-		   }
-
-		   return sb.toString();
 	}
 }
