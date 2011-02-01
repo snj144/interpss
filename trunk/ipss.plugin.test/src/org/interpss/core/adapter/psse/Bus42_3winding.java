192d9ac2549734f402b025cee70e40fcdc2348ec
erzhuanzhou <erzhuanzhou@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-02-01 15:45:08 +0000

    working on 3W Xfr implementation

---
+++ trunk/ipss.plugin.test/src/org/interpss/core/adapter/psse/Bus42_3winding.java
@@ -0,0 +1,49 @@
+ /*
+  * @(#)CR_UserTestCases.java   
+  *
+  * Copyright (C) 2008 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 02/15/2008
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
+package org.interpss.core.adapter.psse;
+
+import org.interpss.PluginObjectFactory;
+import org.interpss.PluginTestSetup;
+import org.interpss.custom.IpssFileAdapter;
+import org.junit.Test;
+
+import com.interpss.core.aclf.AclfNetwork;
+
+public class Bus42_3winding extends PluginTestSetup {
+	@Test
+	public void testCase1() throws Exception {
+		AclfNetwork net = PluginObjectFactory
+				.getFileAdapter(IpssFileAdapter.FileFormat.PSSE, IpssFileAdapter.Version.PSSE_30)
+				.load("testData/psse/v30/42bus_3winding_from_PSSE_V30.raw")
+				.getAclfNet();	
+
+//		LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
+//	  	algo.loadflow();
+//  		//System.out.println(net.net2String());
+//	  	
+//  		assertTrue(net.isLfConverged());	
+	}
+}
+
