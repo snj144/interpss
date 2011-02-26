8744f02d51094feab57a95fc5160dba57ee7008a
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-02-26 17:20:13 +0000

    added ieee39 system test case

--- trunk/ipss.plugin.test/src/org/interpss/core/adapter/ieee/IEEECommonFormatTest.java
+++ trunk/ipss.plugin.test/src/org/interpss/core/adapter/ieee/IEEECommonFormatTest.java
@@ -41,7 +41,7 @@ import com.interpss.simu.SimuContext;
 
 public class IEEECommonFormatTest extends PluginTestSetup {
 	@Test 
-	public void testCase1() throws Exception {
+	public void bus14testCase() throws Exception {
 		AclfNetwork net = PluginObjectFactory
 				.getFileAdapter(IpssFileAdapter.FileFormat.IEEECDF)
 				.load("testdata/ieee_format/Ieee14Bus.ieee")
@@ -62,7 +62,29 @@ public class IEEECommonFormatTest extends PluginTestSetup {
 	}
 
 	@Test
-	public void testCase2() throws Exception{
+	public void bus39testCase() throws Exception{
+		IpssFileAdapter adapter = PluginObjectFactory.getFileAdapter(IpssFileAdapter.FileFormat.IEEECDF);
+		SimuContext simuCtx = adapter.load("testData/ieee_format/ieee039.DAT");
+
+		AclfNetwork net = simuCtx.getAclfNet();
+  		assertTrue(net.getBusList().size() == 39);
+
+	  	LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
+	  	algo.loadflow();
+  		//System.out.println(net.net2String());
+	  	
+  		assertTrue(net.isLfConverged());		
+ 		AclfBus swingBus = (AclfBus)net.getBus("Bus31");
+		SwingBusAdapter swing = (SwingBusAdapter)swingBus.getAdapter(SwingBusAdapter.class);
+//		  31 BUS-31  100   1  1  3 0.982 0.     9.2      4.6       572.8349207.0362 100.    .98200 999900 -99990    0.      0.        0                                                                                                                                                                            
+  		assertTrue( Math.abs(swing.getGenResults(UnitType.PU).getReal()-5.7286653)<0.0001);
+  		assertTrue( Math.abs(swing.getGenResults(UnitType.PU).getImaginary()-2.0766519)<0.0001);
+  		
+  		//System.out.println(AclfOut.lfResultsBusStyle(net));
+  	}
+
+	@Test
+	public void bus30testCase() throws Exception{
 		IpssFileAdapter adapter = PluginObjectFactory.getFileAdapter(IpssFileAdapter.FileFormat.IEEECDF);
 		SimuContext simuCtx = adapter.load("testData/ieee_format/ieee30.ieee");
 
@@ -83,7 +105,7 @@ public class IEEECommonFormatTest extends PluginTestSetup {
 	}
 
 	@Test
-	public void testCase3() throws Exception{
+	public void bus57testCase() throws Exception{
 		IpssFileAdapter adapter = PluginObjectFactory.getFileAdapter(IpssFileAdapter.FileFormat.IEEECDF);
 		SimuContext simuCtx = adapter.load("testData/ieee_format/ieee57.ieee");
 
@@ -106,7 +128,7 @@ public class IEEECommonFormatTest extends PluginTestSetup {
 	}
 
 	@Test
-	public void testCase4() throws Exception{
+	public void bus118testCase() throws Exception{
 		IpssFileAdapter adapter = PluginObjectFactory.getFileAdapter(IpssFileAdapter.FileFormat.IEEECDF);
 		SimuContext simuCtx = adapter.load("testData/ieee_format/ieee118.ieee");
 
