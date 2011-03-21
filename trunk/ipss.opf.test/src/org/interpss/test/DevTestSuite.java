9d7be242058171cace5ad1b133920825c24006cf
scutparis <scutparis@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-03-21 04:11:44 +0000

    make a clean,only opf related left

--- trunk/ipss.opf.test/src/org/interpss/test/DevTestSuite.java
+++ trunk/ipss.opf.test/src/org/interpss/test/DevTestSuite.java
@@ -1,8 +1,5 @@
 package org.interpss.test;
 
-import org.interpss.test.facts.Pass1_SimpleSVCTest;
-import org.interpss.test.odm.acsc.Acsc_5BusTest;
-import org.interpss.test.odm.dstab.DStab_Ipss5BusTest;
 import org.interpss.test.odm.opf.OpfSample;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -10,17 +7,9 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-	//Acsc
-	Acsc_5BusTest.class,
-	
+
 	// OPF
 	OpfSample.class,
-	
-	//DStab
-	DStab_Ipss5BusTest.class,	
-	
-	// FACTS
-	Pass1_SimpleSVCTest.class
 })
 public class DevTestSuite {
 }
