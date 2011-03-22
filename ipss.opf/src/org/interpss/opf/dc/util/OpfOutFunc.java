6c27e8346501537563ba273280a5c8f51020bcb4
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-03-22 19:09:57 +0000

    added LMP to the output

--- ipss.opf/src/org/interpss/opf/dc/util/OpfOutFunc.java
+++ ipss.opf/src/org/interpss/opf/dc/util/OpfOutFunc.java
@@ -8,30 +8,30 @@ import com.interpss.opf.OpfNetwork;
 public class OpfOutFunc {
 	public static String opfResultSummary(OpfNetwork opfnet) {
 		 final StringBuffer str = new StringBuffer("\n\n");
-		 str.append("           ---DCOPF SOLUTION RESULT SUMMARY---            \n");
-		 str.append("----------------------------------------------------\n");
-		 str.append("busID    isOpfGen     optimGen(PU)   optimAngle(RAD) \n");
-		 str.append("----------------------------------------------------\n");
+		 str.append("                     ---DCOPF SOLUTION RESULT SUMMARY---            \n");
+		 str.append("      -----------------------------------------------------------------\n");
+		 str.append("      busID    OpfGen     optimGen(PU)   optimAngle(RAD)    LMP($/MWh)\n");
+		 str.append("      -----------------------------------------------------------------\n");
 		 
 		 for(Bus b:opfnet.getBusList()) {
-			 str.append(b.getId());
+			 str.append(String.format("%12s", b.getId()));
 			 if(opfnet.isOpfGenBus(b)){
 				 OpfGenBus opfBus=(OpfGenBus) b;
 				 
-				 str.append(String.format("%10s","True"));
+				 str.append(String.format("%8s","True"));
 				 str.append(String.format("%14.3f",opfBus.getGenP()));
 				 //minTVC+=opfBus.getCoeffA()*opfBus.getGenP()+opfBus.getCoeffB()*Math.pow(opfBus.getGenP(), 2);
 				 //minTotalCost=minTVC+opfBus.getFixedCost();	
 			 }
 			 else {
-				 str.append(String.format("%10s","False"));
-			     str.append("              ");// just to control the output style, no meaning at all
+			     str.append("                      ");// just to control the output style, no meaning at all
 			 }
-			 str.append(String.format("%17.3f",((OpfBus)b).getVoltageAng()));	
+			 OpfBus bus = (OpfBus)b;
+			 str.append(String.format("%17.3f %15.2f", bus.getVoltageAng(), bus.getLMP()));	
 			 str.append("\n");
 		 }
 		 
-		 str.append("----------------------------------------------------\n\n");
+		 str.append("      -----------------------------------------------------------------\n\n");
 		 str.append(String.format("Minimun Total Variable Cost: %6.3f", 
 				 opfnet.getMinTotalVariableCost())+"\n");
 		 str.append(String.format("Minimun Total Cost: %6.3f",
