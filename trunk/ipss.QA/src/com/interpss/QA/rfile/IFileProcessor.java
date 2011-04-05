71dc06d445e0bf31d48e48e8254bb8a2a75d66cf
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-04-05 21:06:21 +0000

    fixed BAP Adapter Chinese char input issue

--- trunk/ipss.QA/src/com/interpss/QA/rfile/IFileProcessor.java
+++ trunk/ipss.QA/src/com/interpss/QA/rfile/IFileProcessor.java
@@ -2,10 +2,12 @@ package com.interpss.QA.rfile;
 
 import java.util.List;
 
+import org.ieee.odm.common.ODMException;
+
 import com.interpss.common.exp.InterpssException;
 
 public interface IFileProcessor {
-	boolean processLine(String lineStr) throws InterpssException ;
+	boolean processLine(String lineStr) throws InterpssException, ODMException ;
 	
 	List<String> getErrMsgList();
 	
