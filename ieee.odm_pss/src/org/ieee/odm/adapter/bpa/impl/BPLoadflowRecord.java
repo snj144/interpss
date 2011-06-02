af2d8f5b1f0454a4956da5727fa186d2e2139484
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-06-02 21:58:38 +0000

    code refactor

---
+++ ieee.odm_pss/src/org/ieee/odm/adapter/bpa/impl/BPLoadflowRecord.java
@@ -0,0 +1,152 @@
+/*
+ * @(#)BPLoadflowRecord.java   
+ *
+ * Copyright (C) 2006 www.interpss.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @Author Stephen Hou
+ * @Version 1.0
+ * @Date 08/11/2008
+ * 
+ *   Revision History
+ *   ================
+ *   
+ *   modified for Jaxb Mike Zhou 02/28/2011
+ *
+ */
+
+package org.ieee.odm.adapter.bpa.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ieee.odm.adapter.IFileReader;
+import org.ieee.odm.common.ODMException;
+import org.ieee.odm.common.ODMLogger;
+import org.ieee.odm.model.aclf.AclfModelParser;
+import org.ieee.odm.model.base.BaseDataSetter;
+import org.ieee.odm.schema.LoadflowNetXmlType;
+import org.ieee.odm.schema.NameValuePairListXmlType;
+
+/**
+ * BPA adapter is design to handle Loadflow data file and Loadflow+TransienStability data files
+ * 
+ * @author mzhou
+ *
+ */
+public class BPLoadflowRecord {
+	public final static String Token_CaseType = "Type";
+	public final static String Token_ProjectName = "Original Project Name";
+	public final static String Token_CaseId = "Case Identification";
+	public final static String Token_BN="Bus Name";
+	
+	public static void processLfData(AclfModelParser parser, final IFileReader din) throws Exception {
+		LoadflowNetXmlType baseCaseNet = parser.getAclfNet();
+		baseCaseNet.setId("Base_Case_from_BPA_loadflow_format");			
+
+		// we set default base MVA here, since MVA line is optional
+		baseCaseNet.setBasePower(BaseDataSetter.createPowerMvaValue(100.0));
+    	
+		NameValuePairListXmlType nvList = parser.getFactory().createNameValuePairListXmlType();
+		baseCaseNet.setNvPairList(nvList);
+		
+		int areaId=1;// used to arrange a number to each area 
+
+		// BAP Loadflow file does not guarantee that all bus records are put
+		// in front of branch records. Therefore the branch line are cached first
+		List<String> branchInputList = new ArrayList<String>(100);
+		
+		String str;
+		do{
+			str = din.readLine();					
+			if(!str.trim().equals("(END)")&&!str.trim().equals("(STOP)")){
+				try{
+					if(str.startsWith(".")||str.startsWith("C")){
+						// comment line
+						ODMLogger.getLogger().fine("load comment");
+					}
+					else if(str.startsWith("(POWERFLOW")||str.startsWith("/")
+							||str.startsWith(">")){
+						ODMLogger.getLogger().fine("load header data");
+						NetRecord.processNetData(str,nvList,baseCaseNet);
+					}
+					else if(str.startsWith("A")||str.trim().startsWith("I")){
+						NetRecord.processAreaData(str, parser,	baseCaseNet, areaId++);
+					}
+					else if(str.trim().startsWith("B")||str.trim().startsWith("+")
+							||str.trim().startsWith("X")){
+						ODMLogger.getLogger().fine("load AC bus data");						
+						BusRecord.processBusData(str, parser);
+//						System.out.println(str); //for test
+					}
+					else if( str.trim().startsWith("L") || str.trim().startsWith("E") ||
+							 str.trim().startsWith("T") || str.trim().startsWith("R") ||
+							 str.trim().startsWith("LD")||str.trim().startsWith("LM") ||
+							 str.trim().startsWith("BD")||str.trim().startsWith("BM")){
+						ODMLogger.getLogger().fine("load AC line data");
+						// since bus info could be defined at the branch info, we
+						// cache branch info for late processing
+						branchInputList.add(str);
+					}
+					// the gen and load data modification is usually defined  at the end of all network data
+					else if( str.trim().startsWith("PA")||str.trim().startsWith("PZ")||str.trim().startsWith("PO")
+							||str.trim().startsWith("PC")||str.trim().startsWith("PB")){
+						ODMLogger.getLogger().fine("load Gen AND Load modification data");
+						GenLoadDataModifyRecord.processGenLoadModificationData(str,parser);
+					}
+					else{
+						NetRecord.processReadComment(str, baseCaseNet);
+					}						
+				}
+				catch (final Exception e) {
+					ODMLogger.getLogger().severe("Error, input : " + str + "\n" + e.toString());
+					e.printStackTrace();
+				}					
+			}
+		} while(!str.trim().equals("(END)")&&!str.trim().equals("(STOP)"));
+		
+		// processing branch info after all bus info are processed 
+		processBranchInfo(branchInputList, parser);
+		
+		//process Gen and Load Modification Data 
+	}
+	
+	/**
+	 * process branch info
+	 * 
+	 * @param strList
+	 * @param parser
+	 * @throws ODMException
+	 */
+	private static void processBranchInfo(List<String> strList, AclfModelParser parser) throws ODMException {
+		for (String str : strList) {
+			if( str.trim().startsWith("L")||str.trim().startsWith("E")){
+				ODMLogger.getLogger().fine("load AC line data");
+				LineBranchRecord.processBranchData(str, parser);
+			}
+			else if( str.trim().startsWith("T")){
+				ODMLogger.getLogger().fine("load transformer data");
+				XfrBranchRecord.processXfrData(str, parser);
+			}
+			else if(str.trim().startsWith("R")){
+				ODMLogger.getLogger().fine("load transformer adjustment data");
+				XfrBranchRecord.processXfrAdjustData(str, parser);
+			}
+			else if( str.trim().startsWith("LD")||str.trim().startsWith("LM") ||
+					 str.trim().startsWith("BD")||str.trim().startsWith("BM")){
+				ODMLogger.getLogger().fine("load DC Line data");
+				// *** BPABranchRecord.processDCLineBranchData(str, parser.addNewBaseCaseDCLineBranch(),
+				// ***		parser,baseCaseNet, this);
+			}
+		}
+	}
+}
