9ec29f2cfc6b536ada5bb7eb8c99b29d04873746
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-06-02 13:56:41 +0000

    renamed transducer field elements

--- trunk/ipss.plugin/src/org/interpss/mapper/odm/impl/dstab/ExciterDataHelper.java
+++ trunk/ipss.plugin/src/org/interpss/mapper/odm/impl/dstab/ExciterDataHelper.java
@@ -208,9 +208,9 @@ public class ExciterDataHelper {
 
 			if(kv==0)  {
 				FVkv0Exciter exc = ExciterObjectFactory.createBPAFVKv0TypeExciter(mach.getId()+"_Exc", excXml.getName(), mach);
-				exc.getData().setRc(excXml.getRc());
-				exc.getData().setXc(excXml.getXc());
-				exc.getData().setTr(excXml.getTr().getValue());
+				exc.getData().setRc(excXml.getTransRc());
+				exc.getData().setXc(excXml.getTransXc());
+				exc.getData().setTr(excXml.getTransTr().getValue());
 				exc.getData().setT1(excXml.getT1().getValue());
 				exc.getData().setT2(excXml.getT2().getValue());
 				exc.getData().setT3(excXml.getT3().getValue());
@@ -226,9 +226,9 @@ public class ExciterDataHelper {
 		    else {
 		   
 		    	FVkv1Exciter exc=ExciterObjectFactory.createBPAFVKv1TypeExciter(mach.getId()+"_Exc", excXml.getName(), mach);			
-				exc.getData().setRc(excXml.getRc());
-				exc.getData().setXc(excXml.getXc());
-				exc.getData().setTr(excXml.getTr().getValue());
+				exc.getData().setRc(excXml.getTransRc());
+				exc.getData().setXc(excXml.getTransXc());
+				exc.getData().setTr(excXml.getTransTr().getValue());
 				exc.getData().setT1(excXml.getT1().getValue());
 				exc.getData().setT2(excXml.getT2().getValue());
 				exc.getData().setT3(excXml.getT3().getValue());
