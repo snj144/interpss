ff13b1e2c92c5da364bf30e0989008b28bb40156
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-06-05 03:27:11 +0000

    moved DStabOutFunc to plugin.dstab

--- trunk/ipss.plugin/src/org/interpss/display/NBOutputTextDialog.java
+++ trunk/ipss.plugin/src/org/interpss/display/NBOutputTextDialog.java
@@ -35,6 +35,7 @@ import java.util.List;
 
 import javax.swing.JFileChooser;
 
+import org.interpss.dstab.output.DStabOutFunc;
 import org.interpss.dstab.output.DStabSimuDBRecord;
 import org.interpss.editor.jgraph.ui.IGraphicEditor;
 import org.interpss.editor.ui.IOutputTextDialog;
