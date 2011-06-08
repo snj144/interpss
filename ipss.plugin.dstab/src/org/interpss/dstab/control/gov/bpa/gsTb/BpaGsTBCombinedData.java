5e6fed17a9989c13a222a550229a94f2b365e321
scutparis <scutparis@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-06-08 02:14:39 +0000

    BPA gs+Tb model update

--- ipss.plugin.dstab/src/org/interpss/dstab/control/gov/bpa/gsTb/BpaGsTBCombinedData.java
+++ ipss.plugin.dstab/src/org/interpss/dstab/control/gov/bpa/gsTb/BpaGsTBCombinedData.java
@@ -1,5 +1,34 @@
 package org.interpss.dstab.control.gov.bpa.gsTb;
 
+import org.interpss.dstab.control.gov.bpa.combinedType.BpaTBTurbineData;
+
 public class BpaGsTBCombinedData {
+	
+	private BpaTBTurbineData tbData=null;
+	private BpaGsSpeedGovData gsData=null;
+	/**
+	 * @return the tbData
+	 */
+	public BpaTBTurbineData getTbData() {
+		return tbData;
+	}
+	/**
+	 * @return the gsData
+	 */
+	public BpaGsSpeedGovData getGsData() {
+		return gsData;
+	}
+	/**
+	 * @param tbData the tbData to set
+	 */
+	public void setTbData(BpaTBTurbineData tbData) {
+		this.tbData = tbData;
+	}
+	/**
+	 * @param gsData the gsData to set
+	 */
+	public void setGsData(BpaGsSpeedGovData gsData) {
+		this.gsData = gsData;
+	}
 
 }
