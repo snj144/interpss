e62d3c3c6ba8236770a56735b26603e0fb5efb89
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-06-20 13:48:51 +0000

    working on loading Dstab file

--- trunk/ipss.editor.pad/src/org/interpss/editor/project/IpssNewCustomDialog.java
+++ trunk/ipss.editor.pad/src/org/interpss/editor/project/IpssNewCustomDialog.java
@@ -29,50 +29,12 @@ import org.interpss.editor.util.NamedInputStream;
 import org.interpss.editor.util.Utilities;
 import org.interpss.spring.PluginSpringCtx;
 
-
 public class IpssNewCustomDialog extends javax.swing.JDialog {
-	private JComboBox versionComboBox;
-
-	private JComboBox adapterComboBox;
-
-	private JLabel dirLabel;
-
-	private JPanel browsePanel;
-
-	private JPanel selectpanel;
-
-	private JLabel nameLabel;
-
-	private JPanel mainPanel;
-
-	private JPanel buttonPanel;
-
-	private IpssMessagePanel messagePanel;
-
-	private JButton cancelButton;
-
-	private JButton okButton;
-
-	private JButton browseButton;
-
-	private JTextField dirTextField;
-
-	private JTextField nameTextField;
-
 	private String docName;
-
 	private String filepath;
-
 	private String srcFileName;
-
-	protected GPGraphpad graphpad;
-
-	private Frame parentFrame;
-
-	private boolean isCancelExit;
-	
-	private IpssFileAdapter adapter;
-
+	private String dstabFilepath;
+	private String dstabSrcFileName;
 
 	public IpssNewCustomDialog(GPGraphpad graphpad, String title) {
 		// super(graphpad.getFrame(), title, true);
@@ -83,17 +45,20 @@ public class IpssNewCustomDialog extends javax.swing.JDialog {
 
 		setResizable(false);
 
-		// initLocalData();
 		initGuiComponents();
 		initListeners();
-		// setFontValue(initialFont);
+
 		setBrowsePanelVisable();
 		updateState();
-
+	}
+	
+	private void enableDStab(boolean b) {
+		dstabDirLabel.setEnabled(b);
+		dstabDirTextField.setEnabled(b);
+		dstabBrowseButton.setEnabled(b);
 	}
 
 	protected void initListeners() {
-
 		cancelButton.addActionListener(new ActionListener() {
 			public void actionPerformed(final ActionEvent e) {
 				setCancelExit(true);
@@ -110,8 +75,13 @@ public class IpssNewCustomDialog extends javax.swing.JDialog {
 
 		browseButton.addActionListener(new ActionListener() {
 			public void actionPerformed(final ActionEvent e) {
-				selectFile();
+				selectFile(false);
+			}
+		});
 
+		dstabBrowseButton.addActionListener(new ActionListener() {
+			public void actionPerformed(final ActionEvent e) {
+				selectFile(true);
 			}
 		});
 
@@ -124,147 +94,59 @@ public class IpssNewCustomDialog extends javax.swing.JDialog {
 		adapterComboBox.addActionListener(new ActionListener() {
 			public void actionPerformed(final ActionEvent e) {
 				setVersionComboBoxData();
+				Object adapterName = adapterComboBox.getSelectedItem();
+				enableDStab(adapterName.toString().equals("BPA Format DStab"));
 			}
 		});
+	}
 
+	public void selectFile(boolean dstabFile) {
+		try {
+			String fileExtension = adapter.getFileFilterString();
+			String extensionDescription = adapter.getDescription();
+			
+			NamedInputStream in = org.interpss.editor.util.Utilities.provideInput(fileExtension,extensionDescription);
+			
+			if (in != null) {
+				if (dstabFile) {
+					this.setDstabSrcFileName(in.getName());
+					dstabDirTextField.setText(in.getName());
+					setDstabOKActable();
+					if (this.getDstabSrcFileName() == null) {
+						showError("Can't open the selected custom data file.");
+						return;
+					}
+				}
+				else {
+					this.setSrcFileName(in.getName());
+					dirTextField.setText(in.getName());
+					setOKActable();
+					if (this.getSrcFileName() == null) {
+						showError("Can't open the selected custom data file.");
+						return;
+					}
+				}
+			}
+		} catch (Exception ex) {
+			showError(ex.toString());
+			ex.printStackTrace();
+		}
 	}
 
 	protected void setBrowsePanelVisable() {
 	}
 
-	protected void initGuiComponents() {
-
-		getContentPane().setLayout(
-				new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
-
-		messagePanel = new IpssMessagePanel();
-		getContentPane().add(messagePanel);
-		messagePanel.setPreferredSize(new Dimension(0, 30));
-
-		messagePanel.setBorder(new EmptyBorder(0, 5, 0, 0));
-		// messagePanel.getMessageLabel().setPreferredSize(new Dimension(0,
-		// 30));
-
-		getContentPane().add(Box.createVerticalStrut(10));
-
-		mainPanel = new JPanel();
-		mainPanel.setLayout(new BorderLayout());
-		mainPanel.setBorder(new EmptyBorder(0, 5, 0, 5));
-		getContentPane().add(mainPanel);
-
-		selectpanel = new JPanel();
-		mainPanel.add(selectpanel);
-		final GridLayout gridLayout = new GridLayout(0, 1);
-		selectpanel.setLayout(gridLayout);
-		selectpanel.setBorder(new TitledBorder(null, "Import Contexts",
-				TitledBorder.DEFAULT_JUSTIFICATION,
-				TitledBorder.DEFAULT_POSITION, null, null));
-
-		browsePanel = new JPanel();
-		browsePanel.setLayout(new BoxLayout(browsePanel, BoxLayout.Y_AXIS));
-		selectpanel.add(browsePanel);
-
-		final JPanel panel_2 = new JPanel();
-		browsePanel.add(panel_2);
-		panel_2.setLayout(new BorderLayout());
-
-		final JLabel adapterLabel = new JLabel();
-		adapterLabel.setDisplayedMnemonic(KeyEvent.VK_A);
-		adapterLabel.setText("Adapter:");
-		panel_2.add(adapterLabel, BorderLayout.WEST);
-
-		adapterComboBox = new JComboBox();
-		panel_2.add(adapterComboBox, BorderLayout.CENTER);
-
-		browsePanel.add(Box.createVerticalStrut(10));
-
-		final JPanel panel_2_1 = new JPanel();
-		panel_2_1.setLayout(new BorderLayout());
-		browsePanel.add(panel_2_1);
-
-		final JLabel versionLabel = new JLabel();
-		versionLabel.setDisplayedMnemonic(KeyEvent.VK_V);
-		versionLabel.setText("Version:");
-		panel_2_1.add(versionLabel, BorderLayout.WEST);
-
-		versionComboBox = new JComboBox();
-		panel_2_1.add(versionComboBox, BorderLayout.CENTER);
-
-		browsePanel.add(Box.createVerticalStrut(10));
-
-		final JPanel panel_1 = new JPanel();
-		browsePanel.add(panel_1);
-		panel_1.setLayout(new BorderLayout());
-
-		dirLabel = new JLabel();
-		panel_1.add(dirLabel, BorderLayout.WEST);
-		dirLabel.setName("dirLabel");
-		dirLabel.setDisplayedMnemonic(KeyEvent.VK_F);
-		dirLabel.setText("File:  ");
-
-		dirTextField = new JTextField();
-		panel_1.add(dirTextField);
-		dirTextField.setName("dirTextField");
-		dirTextField.setEditable(false);
-		dirTextField.setFocusAccelerator('f');
-
-		browseButton = new JButton();
-		panel_1.add(browseButton, BorderLayout.EAST);
-		browseButton.setName("browseButton");
-
-		browseButton.setMnemonic('B');
-		browseButton.setText("Browse...");
-
-		final JPanel panel = new JPanel();
-		mainPanel.add(panel, BorderLayout.NORTH);
-		panel.setLayout(new BorderLayout());
-
-		nameLabel = new JLabel();
-		panel.add(nameLabel, BorderLayout.WEST);
-		nameLabel.setDisplayedMnemonic(KeyEvent.VK_P);
-		nameLabel.setText("Custom Project Name:");
-
-		nameTextField = new JTextField();
-		panel.add(nameTextField, BorderLayout.CENTER);
-
-		nameTextField.setFocusAccelerator('p');
-
-		buttonPanel = new JPanel();
-
-		getContentPane().add(Box.createVerticalStrut(10));
-		getContentPane().add(buttonPanel);
-
-		okButton = new JButton();
-
-		okButton.setMnemonic('O');
-		buttonPanel.add(okButton);
-		okButton.setText("OK");
-
-		cancelButton = new JButton();
-
-		buttonPanel.add(cancelButton);
-		cancelButton.setMnemonic('C');
-		cancelButton.setText("Cancel");
-
-		adapterComboBox.setModel(new DefaultComboBoxModel(
-				PluginSpringCtx.getCustomFileAdapterNameList()));
-		setVersionComboBoxData();
-
-	}
 
 	private IpssFileAdapter getCustomFileAdapter() {
-
 		Object adapterName = adapterComboBox.getSelectedItem();
 		if (adapterName == null) {
 			return null;
 		}
 
-		return PluginSpringCtx.getCustomFileAdapterByName(adapterName
-				.toString());
+		return PluginSpringCtx.getCustomFileAdapterByName(adapterName.toString());
 	}
 
 	private void setVersionComboBoxData() {
-
 		adapter = getCustomFileAdapter();
 
 		if (adapter == null) {
@@ -281,7 +163,6 @@ public class IpssNewCustomDialog extends javax.swing.JDialog {
 		else {
 			setVersionEnabled(false);
 		}
-
 	}
 
 	private void setVersionEnabled(boolean b) {
@@ -321,6 +202,22 @@ public class IpssNewCustomDialog extends javax.swing.JDialog {
 		return srcFileName;
 	}
 
+	public void setDstabFilepath(String filepath) {
+		this.dstabFilepath = filepath;
+	}
+
+	public String getDstabFilepath() {
+		return dstabFilepath;
+	}
+
+	public void setDstabSrcFileName(String fileName) {
+		this.dstabSrcFileName = fileName;
+	}
+
+	public String getDstabSrcFileName() {
+		return dstabSrcFileName;
+	}
+
 	public void setCancelExit(boolean isCancelExit) {
 		this.isCancelExit = isCancelExit;
 	}
@@ -349,6 +246,15 @@ public class IpssNewCustomDialog extends javax.swing.JDialog {
 			setOKActable();
 	}
 
+	private void setDstabOKActable() {
+		if ((dstabDirTextField.getText() != null)
+				&& (dstabDirTextField.getText().length() > 0)) {
+			okButton.setEnabled(true);
+		} else {
+			okButton.setEnabled(false);
+		}
+	}
+
 	private void setOKActable() {
 		if ((dirTextField.getText() != null)
 				&& (dirTextField.getText().length() > 0)) {
@@ -356,7 +262,6 @@ public class IpssNewCustomDialog extends javax.swing.JDialog {
 		} else {
 			okButton.setEnabled(false);
 		}
-
 	}
 
 	public void showMessage(String messageInfo) {
@@ -370,6 +275,11 @@ public class IpssNewCustomDialog extends javax.swing.JDialog {
 	public String getFileName() {
 		return Utilities.getFilePathName(getFilepath(), nameTextField.getText()
 				+ "." + adapter.getExtension());
+	}
+
+	public String getDstabFileName() {
+		return Utilities.getFilePathName(getFilepath(), nameTextField.getText()
+				+ "." + adapter.getExtensionDstab());
 	}
 
 	public void updateState() {
@@ -395,36 +305,193 @@ public class IpssNewCustomDialog extends javax.swing.JDialog {
 		// showMessage(filePathName);
 	}
 
-	public void selectFile() {
-		try {
-
-// List adapterList = SimuAppSpringAppContext
-// .getCustomFileAdapterList();
-// NamedInputStream in = org.interpss.editor.util.Utilities
-// .provideInput(adapterList);
-
-	
-			String fileExtension = adapter.getFileFilterString();
-			String extensionDescription = adapter.getDescription();
-			
-			NamedInputStream in = org.interpss.editor.util.Utilities.provideInput(fileExtension,extensionDescription);
-			
-			if (in != null) {
-				this.setSrcFileName(in.getName());
-				dirTextField.setText(in.getName());
-				setOKActable();
-				if (this.getSrcFileName() == null) {
-					showError("Can't open the selected custom data file.");
-					return;
-				}
-			}
-		} catch (Exception ex) {
-			showError(ex.toString());
-			ex.printStackTrace();
-		}
-	}
-
 	public String getVersion() {
 		return 	(String)versionComboBox.getSelectedItem();
+	}
+
+	private JComboBox versionComboBox;
+
+	private JComboBox adapterComboBox;
+
+	private JPanel selectpanel;
+
+	private JLabel nameLabel;
+
+	private JPanel mainPanel;
+
+	private JPanel buttonPanel;
+
+	private IpssMessagePanel messagePanel;
+
+	private JButton cancelButton;
+
+	private JButton okButton;
+
+	private JPanel browsePanel;
+	private JLabel dirLabel;
+	private JButton browseButton;
+	private JTextField dirTextField;
+
+	private JLabel dstabDirLabel;
+	private JButton dstabBrowseButton;
+	private JTextField dstabDirTextField;
+
+	private JTextField nameTextField;
+
+	protected GPGraphpad graphpad;
+
+	private Frame parentFrame;
+
+	private boolean isCancelExit;
+	
+	private IpssFileAdapter adapter;
+
+	protected void initGuiComponents() {
+
+		getContentPane().setLayout(
+				new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+		messagePanel = new IpssMessagePanel();
+		getContentPane().add(messagePanel);
+		messagePanel.setPreferredSize(new Dimension(0, 30));
+
+		messagePanel.setBorder(new EmptyBorder(0, 5, 0, 0));
+		// messagePanel.getMessageLabel().setPreferredSize(new Dimension(0,
+		// 30));
+
+		getContentPane().add(Box.createVerticalStrut(10));
+
+		mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+		mainPanel.setBorder(new EmptyBorder(0, 5, 0, 5));
+		getContentPane().add(mainPanel);
+
+		selectpanel = new JPanel();
+		mainPanel.add(selectpanel);
+		final GridLayout gridLayout = new GridLayout(0, 1);
+		selectpanel.setLayout(gridLayout);
+		selectpanel.setBorder(new TitledBorder(null, "Import Contexts",
+				TitledBorder.DEFAULT_JUSTIFICATION,
+				TitledBorder.DEFAULT_POSITION, null, null));
+
+		browsePanel = new JPanel();
+		browsePanel.setLayout(new BoxLayout(browsePanel, BoxLayout.Y_AXIS));
+		selectpanel.add(browsePanel);
+
+		final JPanel panel_adapterType = new JPanel();
+		browsePanel.add(panel_adapterType);
+		panel_adapterType.setLayout(new BorderLayout());
+
+		final JLabel adapterLabel = new JLabel();
+		adapterLabel.setDisplayedMnemonic(KeyEvent.VK_A);
+		adapterLabel.setText("Adapter:");
+		panel_adapterType.add(adapterLabel, BorderLayout.WEST);
+
+		adapterComboBox = new JComboBox();
+		panel_adapterType.add(adapterComboBox, BorderLayout.CENTER);
+
+		browsePanel.add(Box.createVerticalStrut(10));
+
+		final JPanel panel_version = new JPanel();
+		panel_version.setLayout(new BorderLayout());
+		browsePanel.add(panel_version);
+
+		final JLabel versionLabel = new JLabel();
+		versionLabel.setDisplayedMnemonic(KeyEvent.VK_V);
+		versionLabel.setText("Version:");
+		panel_version.add(versionLabel, BorderLayout.WEST);
+
+		versionComboBox = new JComboBox();
+		panel_version.add(versionComboBox, BorderLayout.CENTER);
+
+		browsePanel.add(Box.createVerticalStrut(10));
+
+		/*
+		 * select file
+		 */
+		final JPanel panel_selectFile = new JPanel();
+		browsePanel.add(panel_selectFile);
+		panel_selectFile.setLayout(new BorderLayout());
+
+		dirLabel = new JLabel();
+		panel_selectFile.add(dirLabel, BorderLayout.WEST);
+		dirLabel.setName("dirLabel");
+		dirLabel.setDisplayedMnemonic(KeyEvent.VK_F);
+		dirLabel.setText("File:  ");
+
+		dirTextField = new JTextField();
+		panel_selectFile.add(dirTextField);
+		dirTextField.setName("dirTextField");
+		dirTextField.setEditable(false);
+		dirTextField.setFocusAccelerator('f');
+
+		browseButton = new JButton();
+		panel_selectFile.add(browseButton, BorderLayout.EAST);
+		browseButton.setName("browseButton");
+		browseButton.setMnemonic('B');
+		browseButton.setText("Browse...");
+
+		/*
+		 * select Dstab file
+		 */
+		final JPanel panel_selectDStabFile = new JPanel();
+		browsePanel.add(panel_selectDStabFile);
+		panel_selectDStabFile.setLayout(new BorderLayout());
+
+		dstabDirLabel = new JLabel();
+		panel_selectDStabFile.add(dstabDirLabel, BorderLayout.WEST);
+		dstabDirLabel.setName("dstabDirLabel");
+		dstabDirLabel.setDisplayedMnemonic(KeyEvent.VK_F);
+		dstabDirLabel.setText("Stability File:  ");
+
+		dstabDirTextField = new JTextField();
+		panel_selectDStabFile.add(dstabDirTextField);
+		dstabDirTextField.setName("dstabDirTextField");
+		dstabDirTextField.setEditable(false);
+		dstabDirTextField.setFocusAccelerator('f');
+
+		dstabBrowseButton = new JButton();
+		panel_selectDStabFile.add(dstabBrowseButton, BorderLayout.EAST);
+		dstabBrowseButton.setName("dstabBrowseButton");
+		dstabBrowseButton.setMnemonic('D');
+		dstabBrowseButton.setText("Browse...");
+
+		enableDStab(false);
+		
+		final JPanel panel = new JPanel();
+		mainPanel.add(panel, BorderLayout.NORTH);
+		panel.setLayout(new BorderLayout());
+
+		nameLabel = new JLabel();
+		panel.add(nameLabel, BorderLayout.WEST);
+		nameLabel.setDisplayedMnemonic(KeyEvent.VK_P);
+		nameLabel.setText("Custom Project Name:");
+
+		nameTextField = new JTextField();
+		panel.add(nameTextField, BorderLayout.CENTER);
+
+		nameTextField.setFocusAccelerator('p');
+
+		buttonPanel = new JPanel();
+
+		getContentPane().add(Box.createVerticalStrut(10));
+		getContentPane().add(buttonPanel);
+
+		okButton = new JButton();
+
+		okButton.setMnemonic('O');
+		buttonPanel.add(okButton);
+		okButton.setText("OK");
+
+		cancelButton = new JButton();
+
+		buttonPanel.add(cancelButton);
+		cancelButton.setMnemonic('C');
+		cancelButton.setText("Cancel");
+
+		adapterComboBox.setModel(new DefaultComboBoxModel(
+				PluginSpringCtx.getCustomFileAdapterNameList()));
+		setVersionComboBoxData();
+
 	}
 }
