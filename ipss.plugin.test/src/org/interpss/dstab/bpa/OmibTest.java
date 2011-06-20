b029e77290987970ecd082a0820ddea7af8ce0a6
scutparis <scutparis@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-06-20 02:46:30 +0000

    Add BPA OMIB test case, Mike, please help check the Bus1-mach1 Pe output

---
+++ ipss.plugin.test/src/org/interpss/dstab/bpa/OmibTest.java
@@ -0,0 +1,93 @@
+package org.interpss.dstab.bpa;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.apache.commons.math.complex.Complex;
+import org.ieee.odm.adapter.IODMAdapter;
+import org.ieee.odm.adapter.bpa.BPAAdapter;
+import org.ieee.odm.model.dstab.DStabModelParser;
+import org.interpss.dstab.ieeeModel.DStabTestSetupBase;
+import org.interpss.dstab.output.TextSimuOutputHandler;
+import org.interpss.mapper.odm.ODMDStabDataMapper;
+import org.interpss.numeric.NumericConstant;
+import org.junit.Test;
+
+import com.interpss.core.CoreObjectFactory;
+import com.interpss.core.acsc.fault.AcscBusFault;
+import com.interpss.core.acsc.fault.SimpleFaultCode;
+import com.interpss.dstab.DStabBus;
+import com.interpss.dstab.DStabObjectFactory;
+import com.interpss.dstab.DStabilityNetwork;
+import com.interpss.dstab.algo.DynamicSimuAlgorithm;
+import com.interpss.dstab.algo.DynamicSimuMethod;
+import com.interpss.dstab.devent.DynamicEvent;
+import com.interpss.dstab.devent.DynamicEventType;
+import com.interpss.simu.SimuContext;
+import com.interpss.simu.SimuCtxType;
+import com.interpss.simu.SimuObjectFactory;
+
+public class OmibTest extends DStabTestSetupBase{
+	@Test
+	public void OMIBTestCase() throws Exception {
+		IODMAdapter adapter = new BPAAdapter();
+		assertTrue(adapter.parseInputFile(IODMAdapter.NetType.DStabNet,
+				new String[] { "testdata/bpa/omib-2bus.dat", 
+				               "testdata/bpa/omib-2bus.swi"}));//"testdata/bpa/07c_onlyMach.swi"
+		//assertTrue(adapter.parseInputFile("testdata/bpa/07c.dat" ));
+		DStabModelParser parser=(DStabModelParser) adapter.getModel();
+		//AclfModelParser parser = (AclfModelParser)adapter.getModel();
+		
+		//parser.stdout();
+		String xml=parser.toXmlDoc(false);
+		FileOutputStream out=new FileOutputStream(new File("testdata/ieee_odm/OMIB.xml"));
+		out.write(xml.getBytes());
+		out.flush();
+		out.close();
+		
+		SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.DSTABILITY_NET, msg);
+		if (!new ODMDStabDataMapper(msg)
+					.map2Model(parser, simuCtx)) {
+			System.out.println("Error: ODM model to InterPSS SimuCtx mapping error, please contact support@interpss.com");
+			return;
+		}	
+		
+		DynamicSimuAlgorithm dstabAlgo = simuCtx.getDynSimuAlgorithm();
+	
+		
+		dstabAlgo.setSimuMethod(DynamicSimuMethod.MODIFIED_EULER);
+		dstabAlgo.setSimuStepSec(0.001);
+		dstabAlgo.setTotalSimuTimeSec(0.01);
+		
+		dstabAlgo.setSimuOutputHandler(new TextSimuOutputHandler());
+		if (dstabAlgo.getSolver().initialization()) {
+			System.out.println("Running DStab simulation ...");
+			dstabAlgo.performSimulation(msg);
+		}
+				
+	}
+	private void create3PFaultEvent(DStabilityNetwork net, String busId, 
+			String busName, double startTime,double duration) {
+		// define a bus fault event
+		DynamicEvent event1 = DStabObjectFactory.createDEvent(
+				"BusFault3P@"+busId, "Bus Fault 3P @"+busName, 
+				DynamicEventType.BUS_FAULT, net);
+		event1.setStartTimeSec(startTime);
+		event1.setDurationSec(duration);
+		
+		// define a 3P fault
+		DStabBus faultBus = net.getDStabBus(busId);
+		AcscBusFault fault = CoreObjectFactory.createAcscBusFault("Bus Fault 3P@"+busId, net);
+  		fault.setAcscBus(faultBus);
+		fault.setFaultCode(SimpleFaultCode.GROUND_3P);
+		fault.setZLGFault(NumericConstant.SmallScZ);
+		fault.setZLLFault(new Complex(0.0, 0.0));
+		
+		// add the fault to the event
+		event1.setBusFault(fault);		
+	}
+
+
+}
