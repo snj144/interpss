36d216914841991e4691b0de077ab49a3a51fe96
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-06-27 14:46:52 +0000

    change ExcUtil.java to ExciterUtil.java

--- ipss.plugin.dstab/src/org/interpss/dstab/control/exc/bpa/fvkv0/FVkv0Exciter.java
+++ ipss.plugin.dstab/src/org/interpss/dstab/control/exc/bpa/fvkv0/FVkv0Exciter.java
@@ -12,7 +12,7 @@ import org.interpss.dstab.control.cml.block.FilterControlBlock;
 import org.interpss.dstab.control.cml.block.GainBlock;
 import org.interpss.dstab.control.cml.block.PIControlBlock;
 import org.interpss.dstab.control.cml.block.WashoutControlBlock;
-import org.interpss.dstab.control.exc.ExcUtil;
+import org.interpss.dstab.control.exc.ExciterUtil;
 
 import com.interpss.dstab.DStabBus;
 import com.interpss.dstab.controller.AnnotateExciter;
@@ -114,7 +114,7 @@ public class FVkv0Exciter extends AnnotateExciter {
 		      DStabBus dbus = mach.getDStabBus();
 		      double vt = mach.getVdq(dbus).abs();
 		      //double ifd = mach.calculateIfd(dbus);
-		      double ifd_Exc_pu=ExcUtil.getExciterBasedIfd(mach);
+		      double ifd_Exc_pu=ExciterUtil.getExciterBasedIfd(mach);
 		      return vt * vrlimit - kc * ifd_Exc_pu;
 		  }
 	   };
