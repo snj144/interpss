df15d4d29a014e2dff2612e529e88e5aa8a98e7b
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-06-28 20:52:30 +0000

    added for dist testing

---
+++ ipss.plugin.test/src/org/interpss/dist/PDCBook_2_12_Test.java
@@ -0,0 +1,64 @@
+ /*
+  * @(#)Sample2BusTest.java   
+  *
+  * Copyright (C) 2011 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 02/01/2011
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
+package org.interpss.dist;
+
+import static org.junit.Assert.assertTrue;
+
+import org.interpss.PluginTestSetup;
+import org.interpss.display.AclfOutFunc;
+import org.junit.Test;
+
+import com.interpss.core.CoreObjectFactory;
+import com.interpss.core.algo.LoadflowAlgorithm;
+import com.interpss.dist.DistBus;
+import com.interpss.dist.DistNetwork;
+import com.interpss.simu.SimuContext;
+import com.interpss.simu.SimuCtxType;
+import com.interpss.simu.SimuObjectFactory;
+
+public class PDCBook_2_12_Test  extends PluginTestSetup { 
+	@Test
+	public void lfTest() throws Exception {
+		SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.DISTRIBUTE_NET, msg);
+		loadCaseData("testData/geditor/dist/PDCBook_2-12.ipss", simuCtx);
+		
+	  	DistNetwork distNet = simuCtx.getDistNet();		
+	  	assertTrue(distNet.getFaultNet().isLfDataLoaded());
+	  	assertTrue(distNet.getFaultNet().isScDataLoaded());
+	  	
+	  	LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(distNet.getAclfNet());
+	  	algo.loadflow();
+	  	assertTrue(distNet.getAclfNet().isLfConverged());
+	  	//System.out.println(AclfOutFunc.lfResultsBusStyle(distNet.getAclfNet()));	  	
+	  	
+	  	DistBus bus = (DistBus)distNet.getBus("0001");
+	  	//System.out.println(bus.getAcscBus().getGenResults().getReal());
+	  	//System.out.println(bus.getAcscBus().getGenResults().getImaginary());
+	  	assertTrue(Math.abs(bus.getAcscBus().getGenResults().getReal() + 0.034149) < 0.001);
+	  	assertTrue(Math.abs(bus.getAcscBus().getGenResults().getImaginary() + 0.023523) < 0.0001);
+	}
+	
+}
+
