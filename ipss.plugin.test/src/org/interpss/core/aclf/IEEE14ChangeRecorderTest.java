f922366f06fee4b07f8185467a9eb11306c87f4c
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-07-10 17:39:15 +0000

    added comments

--- ipss.plugin.test/src/org/interpss/core/aclf/IEEE14ChangeRecorderTest.java
+++ ipss.plugin.test/src/org/interpss/core/aclf/IEEE14ChangeRecorderTest.java
@@ -14,6 +14,7 @@ import com.interpss.core.aclf.AclfNetwork;
 import com.interpss.core.aclf.adpter.SwingBusAdapter;
 import com.interpss.core.algo.AclfMethod;
 import com.interpss.core.algo.LoadflowAlgorithm;
+import com.interpss.core.net.Bus;
 import com.interpss.simu.SimuContext;
 import com.interpss.simu.SimuCtxType;
 import com.interpss.simu.SimuObjectFactory;
@@ -26,8 +27,10 @@ public class IEEE14ChangeRecorderTest  extends PluginTestSetup {
 		
 		AclfNetwork net = simuCtx.getAclfNet();
 		
+		// set a bookmark to the AclfNetwork object
 		ChangeRecorder recorder = new ChangeRecorder(net);
 		
+		// run loadflow, this will modify the network object
 	  	LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
 	  	algo.setLfMethod(AclfMethod.NR);
 	  	algo.setNonDivergent(true);
@@ -39,11 +42,20 @@ public class IEEE14ChangeRecorderTest  extends PluginTestSetup {
   		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getReal()-2.32394)<0.0001);
   		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getImaginary()+0.20674)<0.0001);
   		
+  		// make more changes to the network object
   		net.getAclfBus("0001").setGenCode(AclfGenCode.GEN_PV);
   		net.getBus("0008").setStatus(false);
   		net.getBranch("0007", "0008").setStatus(false);
   		
+  		// roll back all recorded changes to the bookmark point
   		recorder.endRecording().apply();
+  		
+		//System.out.println(net.net2String());
+  		// when the network loaded into InterPSS, all voltage ang = 0.0
+  		for (Bus b : net.getBusList()) {
+  			AclfBus bus = (AclfBus)b;
+  	  		assertTrue(Math.abs(bus.getVoltageAng())<0.0001);
+  		}
 
   		assertTrue(algo.loadflow());
   		swingBus = (AclfBus)net.getBus("0001");
