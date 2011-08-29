bddeff7b75a61a087c5860eb32ca24340ce4e000
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-08-29 17:39:39 +0000

    documentation

--- trunk/ipss.gams.test/src/test/ieee14_ed1.java
+++ trunk/ipss.gams.test/src/test/ieee14_ed1.java
@@ -9,6 +9,14 @@ import com.interpss.gams.file.ModelIncFileCreator;
 import com.interpss.gams.util.GAMSHelper;
 import com.interpss.gams.util.IReadResult;
 
+/*
+ * To run this example,
+ * 
+ *    - put ieee14_java1.gms and ieee14_java.template in the current dir
+ *    - include file ieee14_java.inc and gdx file inputdata.gdx are created during the
+ *      running process
+ */
+
 public class ieee14_ed1 {
     public static void main(String[] args) {
     	GAMS.GamsDir = "c:\\Program Files (x86)\\GAMS23.7";
