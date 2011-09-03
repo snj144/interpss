db68120f1a512a646aecdf4668c4ee91038c3430
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-09-03 15:15:02 +0000

    code refactor

--- ipss.gams.test/src/test/example2_1.java
+++ ipss.gams.test/src/test/example2_1.java
@@ -5,6 +5,7 @@ import com.interpss.gams.GAMS;
 import com.interpss.gams.common.GAMSException;
 import com.interpss.gams.common.GAMSLogger;
 import com.interpss.gams.util.GAMSHelper;
+import com.interpss.gams.util.IReadResult;
 
 public class example2_1 {
     public static void main(String[] args) {
@@ -17,11 +18,10 @@ public class example2_1 {
    	 		WriteModelData("demanddata.gdx");
 
    	 		GAMSHelper helper = new GAMSHelper();
-   	   	    helper.CallGams("model2.gms", 2, 1);
+   	   	    helper.CallGams("model2.gms");
 
-   	   	    helper.getReadResult("results.gdx", 
-   	   	        		new String[] {"result"}, 
-   	   	        		helper.getResultReader2D());   	   	        
+   	   	    helper.readResult("results.gdx", 
+   	   	        		new String[] {"result"}, reader);   	   	        
    	 	} catch (GAMSException e) {
    	 		GAMSLogger.getLogger().severe(e.toString());
    	 	} finally {
@@ -40,4 +40,24 @@ public class example2_1 {
         GAMS.gdxDataWriteDone();
         GAMS.gdxClose();
     }
+    
+	private static IReadResult reader = new IReadResult() {
+       	public void readResult(int dimension) {
+            String[] Indx = new String[gamsglobals.maxdim];
+            double[] Values = new double[gamsglobals.val_max];
+            int[] N = new int[1];
+            
+            while(GAMS.gdxDataReadStr(Indx, Values, N) != 0){
+                if(Values[gamsglobals.val_level] == 0) //skip level = 0.0 is default
+                    continue;
+                for(int D = 0; D < dimension; D++){
+                    System.out.print(Indx[D]);
+                    if(D < dimension-1)
+                        System.out.print(".");
+                }
+                System.out.println(" = " + Values[gamsglobals.val_level]);
+            }
+            System.out.println("All solution values shown");
+        }
+    };    
 }
