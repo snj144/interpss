182e4de12755eb5215f080b45f09b3506b45a266
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-09-12 20:51:29 +0000

    set default level to WARN

--- ipss.numeric/src/org/interpss/numeric/NumericLogger.java
+++ ipss.numeric/src/org/interpss/numeric/NumericLogger.java
@@ -24,14 +24,17 @@
 
 package org.interpss.numeric;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class NumericLogger {
 	private static Logger logger = null;
 	
 	public static Logger getLogger() { 
-		if (logger == null) 
+		if (logger == null) {
 			logger = Logger.getLogger("ipss.numeric");
+			logger.setLevel(Level.WARNING);
+		}
 		return logger; 
 	}
 }
