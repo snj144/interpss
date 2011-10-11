2057adb05bc6d1f6b7e317f69374c105292a849f
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-10-11 22:06:17 +0000

    added interface method test case

---
+++ trunk/ipss.plugin.test/src/org/interpss/core/aclf/IEEE14InterfaceCaseTest.java
@@ -0,0 +1,71 @@
+package org.interpss.core.aclf;
+
+import static org.junit.Assert.assertTrue;
+
+import org.interpss.PluginTestSetup;
+import org.junit.Test;
+
+import com.interpss.common.datatype.UnitType;
+import com.interpss.core.CoreObjectFactory;
+import com.interpss.core.aclf.AclfBus;
+import com.interpss.core.aclf.AclfNetwork;
+import com.interpss.core.aclf.Interface;
+import com.interpss.core.aclf.InterfaceBranch;
+import com.interpss.core.aclf.InterfaceLimit;
+import com.interpss.core.aclf.adpter.SwingBusAdapter;
+import com.interpss.core.algo.AclfMethod;
+import com.interpss.core.algo.LoadflowAlgorithm;
+import com.interpss.simu.SimuContext;
+import com.interpss.simu.SimuCtxType;
+import com.interpss.simu.SimuObjectFactory;
+
+public class IEEE14InterfaceCaseTest  extends PluginTestSetup {
+	@Test
+	public void runTest() throws Exception {
+		SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.ACLF_NETWORK, msg);
+		loadCaseData("testData/aclf/IEEE-14Bus.ipss", simuCtx);
+		
+		AclfNetwork net = simuCtx.getAclfNet();
+		Interface inf = CoreObjectFactory.createInterface(net, "interface");
+		
+		InterfaceLimit onPeak = CoreObjectFactory.createInterfaceLimit();
+		inf.setOnPeakLimit(onPeak);
+		onPeak.setRefDirExportLimit(1.0);
+		onPeak.setOppsiteRefDirImportLimit(-1.0);
+		
+		InterfaceLimit offPeak = CoreObjectFactory.createInterfaceLimit();
+		inf.setOffPeakLimit(offPeak);
+		offPeak.setRefDirExportLimit(0.5);
+		offPeak.setOppsiteRefDirImportLimit(-0.5);
+		
+		InterfaceBranch b = CoreObjectFactory.createInterfaceBranch(inf);
+		b.setAclfBranch(net.getAclfBranch("0004", "0007", "1"));
+		b.setWeight(1.0);
+		
+		b = CoreObjectFactory.createInterfaceBranch(inf);
+		b.setAclfBranch(net.getAclfBranch("0004", "0009", "1"));
+		b.setWeight(1.0);
+
+		b = CoreObjectFactory.createInterfaceBranch(inf);
+		b.setAclfBranch(net.getAclfBranch("0005", "0006", "1"));
+		b.setWeight(1.0);
+
+		LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
+	  	algo.setLfMethod(AclfMethod.NR);
+	  	algo.setNonDivergent(true);
+  		assertTrue(algo.loadflow());
+  		AclfBus swingBus = (AclfBus)net.getBus("0001");
+		SwingBusAdapter swing = swingBus.toSwingBus();
+		//System.out.println(ComplexFunc.toString(swing.getGenResults(UnitType.PU)));
+  		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getReal()-2.32394)<0.0001);
+  		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getImaginary()+0.20674)<0.0001);
+  		
+		//System.out.println("Flow export: " + inf.flowExport());
+		//System.out.println("Flow import: " + inf.flowImport());
+  		assertTrue(Math.abs(inf.flowExport()-0.88234)<0.0001);
+  		assertTrue(Math.abs(inf.flowImport()+0.88234)<0.0001);
+
+  		assertTrue(!inf.onPeakViolation());
+  		assertTrue(inf.offPeakViolation());
+	}			
+}
