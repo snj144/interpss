850f8d476f5a84a96d3256a347e1aa9d53c14a65
scutparis <scutparis@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2011-11-26 05:45:24 +0000

    OMIB test case update

--- ipss.plugin.test/src/org/interpss/dstab/bpa/OmibTest.java
+++ ipss.plugin.test/src/org/interpss/dstab/bpa/OmibTest.java
@@ -4,8 +4,8 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.util.List;
+import java.util.logging.Level;
 
 import org.apache.commons.math.complex.Complex;
 import org.ieee.odm.ODMObjectFactory;
@@ -20,13 +20,13 @@ import org.interpss.numeric.NumericConstant;
 import org.interpss.numeric.util.Number2String;
 import org.junit.Test;
 
-import com.interpss.CoreObjectFactory;
-import com.interpss.DStabObjectFactory;
-import com.interpss.SimuObjectFactory;
+import com.interpss.common.util.IpssLogger;
+import com.interpss.core.CoreObjectFactory;
 import com.interpss.core.acsc.fault.AcscBusFault;
 import com.interpss.core.acsc.fault.SimpleFaultCode;
 import com.interpss.core.algo.LoadflowAlgorithm;
 import com.interpss.dstab.DStabBus;
+import com.interpss.dstab.DStabObjectFactory;
 import com.interpss.dstab.DStabilityNetwork;
 import com.interpss.dstab.algo.DynamicSimuAlgorithm;
 import com.interpss.dstab.algo.DynamicSimuMethod;
@@ -37,27 +37,28 @@ import com.interpss.dstab.devent.DynamicEvent;
 import com.interpss.dstab.devent.DynamicEventType;
 import com.interpss.simu.SimuContext;
 import com.interpss.simu.SimuCtxType;
+import com.interpss.simu.SimuObjectFactory;
 
 public class OmibTest extends DStabTestSetupBase{
 	@Test
 	public void OMIBTestCase() throws Exception {
 		IODMAdapter adapter = new BPAAdapter();
 		assertTrue(adapter.parseInputFile(IODMAdapter.NetType.DStabNet,
-				new String[] { "testdata/bpa/EQG017_omib.dat", 
-				               "testdata/bpa/EQG017_omib.swi"}));//"testdata/bpa/07c_onlyMach.swi"
+				new String[] { "testdata/bpa/EQG009_omib.dat", 
+				               "testdata/bpa/EQG009_omib.swi"}));//"testdata/bpa/07c_onlyMach.swi"
 		//assertTrue(adapter.parseInputFile("testdata/bpa/07c.dat" ));
 		
 		DStabModelParser parser=(DStabModelParser) adapter.getModel();
 
-		
 		parser.stdout();
+		/*
 		String xml=parser.toXmlDoc(false);
 		FileOutputStream out=new FileOutputStream(new File("testdata/ieee_odm/EQG007_OMIB.xml"));
 		out.write(xml.getBytes());
 		out.flush();
 		out.close();
-
-		SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.DSTABILITY_NET);
+        */
+		SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.DSTABILITY_NET, msg);
 		if (!new ODMDStabDataMapper(msg)
 					.map2Model(parser, simuCtx)) {
 			System.out.println("Error: ODM model to InterPSS SimuCtx mapping error, please contact support@interpss.com");
@@ -66,42 +67,68 @@ public class OmibTest extends DStabTestSetupBase{
 		
 		DynamicSimuAlgorithm dstabAlgo = simuCtx.getDynSimuAlgorithm();
 		DStabilityNetwork net = simuCtx.getDStabilityNet();
+		//System.out.println(net.getDStabBus("Bus2").getMachine().getExciter().getDataXmlString());
 		// run load flow test case
 		LoadflowAlgorithm aclfAlgo = dstabAlgo.getAclfAlgorithm();
 		assertTrue(aclfAlgo.loadflow());
 		System.out.println(AclfOutFunc.lfResultsBusStyle(net));
-		System.out.println("GENq="+net.getAclfBus("Bus2").getGenResults().getImaginary());
+		//System.out.println("GENq="+net.getAclfBus("Bus2").getGenResults().getImaginary());
 		dstabAlgo.setSimuMethod(DynamicSimuMethod.MODIFIED_EULER);
 		dstabAlgo.setSimuStepSec(0.001);
-		dstabAlgo.setTotalSimuTimeSec(0.03);
+		dstabAlgo.setTotalSimuTimeSec(0.30);
 		dstabAlgo.setRefMachine(net.getMachine("Bus3-mach1"));
 		
 		// create fault
-		create3PFaultEvent(net, "Bus1", "Bus1",0.2,0.1);
+		create3PFaultEvent(net, "Bus1", "Bus1",0.1,0.04);
 		
 		StateVariableRecorder ssRecorder = new StateVariableRecorder(0.0001);
 		ssRecorder.addCacheRecords("Bus2-mach1",      // mach id 
 				StateVariableRecorder.RecType.Machine,    // record type
 				DStabOutSymbol.OUT_SYMBOL_MACH_ANG,       // state variable name
-				0.1,                                      // time steps for recording 
+				0.05,                                      // time steps for recording 
 				300);                                      // total points to record 
+		ssRecorder.addCacheRecords("Bus2-mach1",      // mach id 
+				StateVariableRecorder.RecType.Machine,    // record type
+				DStabOutSymbol.OUT_SYMBOL_MACH_Efd,       // state variable name
+				0.01,                                      // time steps for recording 
+				300);                                      // total points to record 
+		ssRecorder.addCacheRecords("Bus2-mach1",      // mach id 
+				StateVariableRecorder.RecType.Machine,    // record type
+				DStabOutSymbol.OUT_SYMBOL_MACH_PM,       // state variable name
+				0.05,                                      // time steps for recording 
+				300);                                      // total points to record
+		
 		// set the output handler
 		dstabAlgo.setSimuOutputHandler(ssRecorder);
 		
-		dstabAlgo.setSimuOutputHandler(new TextSimuOutputHandler());
+		IpssLogger.getLogger().setLevel(Level.INFO);
+		//dstabAlgo.setSimuOutputHandler(new TextSimuOutputHandler());
 		if (dstabAlgo.initialization()) {
 			System.out.println("Running DStab simulation ...");
 			dstabAlgo.performSimulation();
 		}
 		
 		// output recorded simulation results
-//		List<StateVariableRecorder.Record> list = ssRecorder.getMachineRecords(
-//				"Bus2-mach1", StateVariableRecorder.RecType.Machine, DStabOutSymbol.OUT_SYMBOL_MACH_ANG);
-//		System.out.println("\n\n Bus1 Machine Anagle");
-//		for (Record rec : list) {
-//			System.out.println(Number2String.toStr(rec.t) + ", " + Number2String.toStr(rec.variableValue));
-//		}
-				
+		List<StateVariableRecorder.Record> list = ssRecorder.getMachineRecords(
+				"Bus2-mach1", StateVariableRecorder.RecType.Machine, DStabOutSymbol.OUT_SYMBOL_MACH_ANG);
+		System.out.println("\n\n Bus2 Machine Anagle");
+		for (Record rec : list) {
+			System.out.println(Number2String.toStr(rec.t) + ", " + Number2String.toStr(rec.variableValue));
+		}
+		list = ssRecorder.getMachineRecords(
+				"Bus2-mach1", StateVariableRecorder.RecType.Machine, DStabOutSymbol.OUT_SYMBOL_MACH_Efd);
+		System.out.println("\n\n Bus2 Machine EXC");
+		for (Record rec : list) {
+			System.out.println(Number2String.toStr(rec.t) + ", " + Number2String.toStr(rec.variableValue));
+		}
+		
+		list = ssRecorder.getMachineRecords(
+				"Bus2-mach1", StateVariableRecorder.RecType.Machine, DStabOutSymbol.OUT_SYMBOL_MACH_PM);
+		System.out.println("\n\n Bus2 Machine PM");
+		for (Record rec : list) {
+			System.out.println(Number2String.toStr(rec.t) + ", " + Number2String.toStr(rec.variableValue));
+		}
+			
 	}
 	//@Test
 	public void XmlDstabtestCase() throws Exception {
@@ -111,7 +138,7 @@ public class OmibTest extends DStabTestSetupBase{
 			if (parser.parse(new FileInputStream(file))) {
 				//System.out.println(parser.toXmlDoc(false));
 
-				SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.DSTABILITY_NET);
+				SimuContext simuCtx = SimuObjectFactory.createSimuNetwork(SimuCtxType.DSTABILITY_NET, msg);
 				if (!new ODMDStabDataMapper(msg)
 							.map2Model(parser, simuCtx)) {
 					System.out.println("Error: ODM model to InterPSS SimuCtx mapping error, please contact support@interpss.com");
@@ -125,17 +152,22 @@ public class OmibTest extends DStabTestSetupBase{
 				assertTrue(aclfAlgo.loadflow());
 				
 				dstabAlgo.setSimuMethod(DynamicSimuMethod.MODIFIED_EULER);
-				dstabAlgo.setSimuStepSec(0.01);
-				dstabAlgo.setTotalSimuTimeSec(0.40);
+				dstabAlgo.setSimuStepSec(0.001);
+				dstabAlgo.setTotalSimuTimeSec(0.5);
 
 				
 				// create fault
-				create3PFaultEvent(net, "Bus2", "infinitBus",0.2,0.1);
+				create3PFaultEvent(net, "Bus1", "Bus1",0.2,0.1);
 				
 				StateVariableRecorder ssRecorder = new StateVariableRecorder(0.0001);
-				ssRecorder.addCacheRecords("Bus1-mach1",      // mach id 
+				ssRecorder.addCacheRecords("Bus2-mach1",      // mach id 
 						StateVariableRecorder.RecType.Machine,    // record type
 						DStabOutSymbol.OUT_SYMBOL_MACH_ANG,       // state variable name
+						0.1,                                      // time steps for recording 
+						300);                                      // total points to record 
+				ssRecorder.addCacheRecords("Bus2-mach1",      // mach id 
+						StateVariableRecorder.RecType.Governor,    // record type
+						DStabOutSymbol.OUT_SYMBOL_GOV_PM,       // state variable name
 						0.1,                                      // time steps for recording 
 						300);                                      // total points to record 
 				// set the output handler
@@ -149,8 +181,14 @@ public class OmibTest extends DStabTestSetupBase{
 				
 				// output recorded simulation results
 				List<StateVariableRecorder.Record> list = ssRecorder.getMachineRecords(
-						"Bus1-mach1", StateVariableRecorder.RecType.Machine, DStabOutSymbol.OUT_SYMBOL_MACH_ANG);
-				System.out.println("\n\n Bus1 Machine Anagle");
+						"Bus2-mach1", StateVariableRecorder.RecType.Machine, DStabOutSymbol.OUT_SYMBOL_MACH_ANG);
+				System.out.println("\n\n Bus2 Machine Anagle");
+				for (Record rec : list) {
+					System.out.println(Number2String.toStr(rec.t) + ", " + Number2String.toStr(rec.variableValue));
+				}
+				list = ssRecorder.getMachineRecords(
+						"Bus2-mach1", StateVariableRecorder.RecType.Governor, DStabOutSymbol.OUT_SYMBOL_GOV_PM);
+				System.out.println("\n\n Bus2 Machine PM");
 				for (Record rec : list) {
 					System.out.println(Number2String.toStr(rec.t) + ", " + Number2String.toStr(rec.variableValue));
 				}
