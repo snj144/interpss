11793c0e9d2a98e27a5a34069773aabbe96e55ee
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2012-01-31 18:38:09 +0000

    working user defined AF

--- ieee.odm_pss/src/org/ieee/odm/model/base/ModelContansts.java
+++ ieee.odm_pss/src/org/ieee/odm/model/base/ModelContansts.java
@@ -4,7 +4,7 @@ import org.ieee.odm.schema.ODMSchemaVersionEnumType;
 
 public class ModelContansts {
 	public static final String ODM_Schema_NS = "org.ieee.odm.schema";
-	public static final ODMSchemaVersionEnumType ODM_Schema_Version = ODMSchemaVersionEnumType.V_0_8;
+	public static final ODMSchemaVersionEnumType ODM_Schema_Version = ODMSchemaVersionEnumType.V_0_8_0;
 
 	public static final double Deg2Rad = Math.PI / 180.0;
 	public static final double Rad2Deg = 180.0/ Math.PI;
