af8e0785bf99aa75d65573fedfb3f9e085ec36f3
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2012-03-24 04:27:09 +0000

    created DistFunction

--- ipss.pssl.test/src/com/interpss/pssl/test/dclf/DclfLODFPaper_Test.java
+++ ipss.pssl.test/src/com/interpss/pssl/test/dclf/DclfLODFPaper_Test.java
@@ -24,7 +24,7 @@
 
 package com.interpss.pssl.test.dclf;
 
-import static com.interpss.core.DclfFunction.DclfBranchAptr;
+import static com.interpss.core.dclf.funcImpl.DclfFunction.DclfBranchAptr;
 import static org.junit.Assert.assertTrue;
 
 import org.ieee.odm.model.aclf.AclfModelParser;
