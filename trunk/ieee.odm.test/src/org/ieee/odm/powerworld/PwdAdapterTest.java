aa26201bf1ae88104a12ddefcc5a5c8071f45df1
scutparis <scutparis@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2012-04-06 01:41:20 +0000

    pwd adapter code update

--- trunk/ieee.odm.test/src/org/ieee/odm/powerworld/PwdAdapterTest.java
+++ trunk/ieee.odm.test/src/org/ieee/odm/powerworld/PwdAdapterTest.java
@@ -70,7 +70,7 @@ public class PwdAdapterTest {
 		assertTrue(adapter.parseInputFile("testdata/powerworld/ieee14.aux"));
 		AclfModelParser parser=(AclfModelParser) adapter.getModel();
 		
-		parser.stdout();
+		//parser.stdout();
 		
 		//check network data
 		assertTrue(parser.getAclfNet().getBasePower().getValue()==100.0);
@@ -101,21 +101,22 @@ public class PwdAdapterTest {
 		assertTrue(line.getZ().getIm()==0.059170);
 		assertTrue(line.getTotalShuntY().getIm()==0.052800);
 		assertTrue(line.getTotalShuntY().getRe()==0.0);
-//		//check transformer data
-//		/*
-//		 * [BusNum,BusNum:1,LineCircuit,LineStatus,LineR,LineX,LineC,LineG,LineAMVA,LineBMVA,
-//             LineCMVA,LineShuntMW,LineShuntMW:1,LineShuntMVR,LineShuntMVR:1,LineXfmr,LineTap,
-//             LinePhase,SeriesCapStatus]
-//		 *   4     5 " 1" "Closed"  0.000000  0.100000  0.000000  0.000000  1000.000  1000.000  1000.000     0.000     0.000     0.000     0.000  "YES"    0.993750   0.000000 "Not Bypassed"
-//		 */
-//		XfrBranchXmlType xfr=(XfrBranchXmlType) parser.getBranch("Bus4","Bus5"," 1");
-//		//TODO Both b and g are zero, so MagnitizingY are not set.
-////		assertTrue(xfr.getMagnitizingY().getIm()==0.0);
-////		assertTrue(xfr.getMagnitizingY().getRe()==0.0);
-//		assertTrue(xfr.getZ().getIm()==0.100000);
-//		assertTrue(xfr.getZ().getRe()==0.000000);
-//		assertTrue(xfr.getFromTurnRatio().getValue()==0.993750);
-//		assertTrue(xfr.getToTurnRatio().getValue()==1.0);
+		//check transformer data
+		/*
+		 * [BusNum,BusNum:1,LineCircuit,LineStatus,LineR,LineX,LineC,LineG]
+		
+            4     7 " 1" "Closed"  0.000000  0.209120  0.000000  0.000000     0.000     0.000     0.000     0.000     0.000     0.000     0.000     0.000    0.0000    0.0000    0.0000    0.0000 0.978000   0.000000 "Not Bypassed" "Closed" "Not Bypassed" "NO " "From" "" "Default" "YES" "No" "NO " "NO "   0.0000      1 100.0000   0.0000   0.0000 "Transformer" "" "NO" "NO" "NO" "NO" "NO " "NO " "NO " "NO " "NO " "NO "
+            4     9 " 1" "Closed"  0.000000  0.556180  0.000000  0.000000     0.000     0.000     0.000     0.000     0.000     0.000     0.000     0.000    0.0000    0.0000    0.0000    0.0000 0.969000   0.000000 "Not Bypassed" "Closed" "Not Bypassed" "NO " "From" "" "Default" "YES" "No" "NO " "NO "   0.0000      1 100.0000   0.0000   0.0000 "Transformer" "" "NO" "NO" "NO" "NO" "NO " "NO " "NO " "NO " "NO " "NO "
+            5     6 " 1" "Closed"  0.000000  0.252020  0.000000  0.000000     0.000     0.000     0.000     0.000     0.000     0.000     0.000     0.000    0.0000    0.0000    0.0000    0.0000 0.932000   0.000000 "Not Bypassed" "Closed" "Not Bypassed" "NO " "From" "" "Default" "YES" "No" "NO " "NO "   0.0000      1 100.0000   0.0000   0.0000 "Transformer" "" "NO" "NO" "NO" "NO" "NO " "NO " "NO " "NO " "NO " "NO "
+		 */
+		XfrBranchXmlType xfr=(XfrBranchXmlType) parser.getBranch("Bus4","Bus7"," 1");
+		//TODO Both b and g are zero, so MagnitizingY are not set.
+//		assertTrue(xfr.getMagnitizingY().getIm()==0.0);
+//		assertTrue(xfr.getMagnitizingY().getRe()==0.0);
+		assertTrue(xfr.getZ().getIm()==0.209120);
+		assertTrue(xfr.getZ().getRe()==0.000000);
+		assertTrue(xfr.getFromTurnRatio().getValue()==0.978000);
+		assertTrue(xfr.getToTurnRatio().getValue()==1.0);
 		
 	}
 }
