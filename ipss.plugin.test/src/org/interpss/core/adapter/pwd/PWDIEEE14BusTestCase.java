4f12db3657303e58acc0e48a8638cc9553c598fe
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2012-04-08 13:22:45 +0000

    added PWD adapter testcase

---
+++ ipss.plugin.test/src/org/interpss/core/adapter/pwd/PWDIEEE14BusTestCase.java
@@ -0,0 +1,66 @@
+ /*
+  * @(#)CR_UserTestCases.java   
+  *
+  * Copyright (C) 2008 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 02/15/2008
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
+package org.interpss.core.adapter.pwd;
+
+import static org.junit.Assert.assertTrue;
+
+import org.interpss.PluginObjectFactory;
+import org.interpss.PluginTestSetup;
+import org.interpss.algo.ZeroZBranchProcesor;
+import org.interpss.custom.IpssFileAdapter;
+import org.interpss.display.AclfOutFunc;
+import org.interpss.numeric.datatype.Unit.UnitType;
+import org.junit.Test;
+
+import com.interpss.CoreObjectFactory;
+import com.interpss.core.aclf.AclfBus;
+import com.interpss.core.aclf.AclfNetwork;
+import com.interpss.core.aclf.adpter.AclfSwingBus;
+import com.interpss.core.algo.LoadflowAlgorithm;
+
+public class PWDIEEE14BusTestCase extends PluginTestSetup {
+	@Test
+	public void odmAdapterTestCase() throws Exception {
+		AclfNetwork net = PluginObjectFactory
+				.getFileAdapter(IpssFileAdapter.FileFormat.PWD)
+				.load("testData/pwd/ieee14.AUX")
+				.getAclfNet();	
+		
+		System.out.println(net.net2String());
+
+		LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
+	  	algo.loadflow();
+		
+	  	System.out.println(AclfOutFunc.loadFlowSummary(net));
+	  	
+  		assertTrue(net.isLfConverged());		
+  		//AclfBus swingBus = (AclfBus)net.getBus("Bus1");
+  		//AclfSwingBus swing = swingBus.toSwingBus();
+		//System.out.println(swing.getGenResults(UnitType.PU).getImaginary());
+  		//assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getReal()-1.0586)<0.01);
+  		//assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getImaginary()-0.4366)<0.01);
+	}	
+}
+
