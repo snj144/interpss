f932a7b7cdc82bf9a32fe5fe8a1606706f8b758c
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2012-07-02 02:30:20 +0000

    for cmd development

---
+++ branches/CmdLine_dev/ipss.plugin.core/src/org/interpss/mapper/odm/impl/MultiNetODMHepler.java
@@ -0,0 +1,61 @@
+/*
+ * @(#)ODMAclfDataMapper.java   
+ *
+ * Copyright (C) 2008 www.interpss.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @Author Mike Zhou
+ * @Version 1.0
+ * @Date 03/15/2012
+ * 
+ *   Revision History
+ *   ================
+ *
+ */
+
+package org.interpss.mapper.odm.impl;
+
+import static org.interpss.CorePluginFunction.AclfParser2AclfNet;
+
+import javax.xml.bind.JAXBElement;
+
+import org.ieee.odm.model.aclf.AclfModelParser;
+import org.ieee.odm.schema.NetworkXmlType;
+import org.interpss.mapper.odm.IMultiNetProcessor;
+
+import com.interpss.common.exp.InterpssException;
+import com.interpss.core.aclf.AclfNetwork;
+
+
+public class MultiNetODMHepler {
+	private AclfModelParser aclfParser;
+	
+	public MultiNetODMHepler(AclfModelParser aclfParser) {
+		this.aclfParser = aclfParser;
+	}
+	
+	/**
+	 * build multi-network case. The child networks are processed by the childNetProcessor
+	 * 
+	 * @param childNetProcesor
+	 * @return
+	 * @throws InterpssException
+	 */
+	public AclfNetwork buildAclfMultiNet(IMultiNetProcessor childNetProcesor) throws InterpssException {
+		AclfNetwork mainNet = AclfParser2AclfNet.fx(aclfParser);
+		
+		for (JAXBElement<? extends NetworkXmlType> childNet : aclfParser.getChildNetList()) {
+			childNetProcesor.setMainNet(mainNet).process(childNet.getValue());
+		}
+		return mainNet;
+	}
+}
