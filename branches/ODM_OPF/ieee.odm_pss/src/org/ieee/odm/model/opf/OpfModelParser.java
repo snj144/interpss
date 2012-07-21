433265a46edbb5a3fdebfa0c6b0f2af7199fcb91
ipssdev <ipssdev@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2012-07-21 22:27:55 +0000

    working on opf matpower odm adapter

--- branches/ODM_OPF/ieee.odm_pss/src/org/ieee/odm/model/opf/OpfModelParser.java
+++ branches/ODM_OPF/ieee.odm_pss/src/org/ieee/odm/model/opf/OpfModelParser.java
@@ -26,12 +26,25 @@ package org.ieee.odm.model.opf;
 
 import static org.ieee.odm.ODMObjectFactory.odmObjFactory;
 
+import org.ieee.odm.common.ODMException;
 import org.ieee.odm.model.aclf.AclfModelParser;
 import org.ieee.odm.model.base.BaseJaxbHelper;
+import org.ieee.odm.schema.ActivePowerLimitXmlType;
+import org.ieee.odm.schema.AnalysisCategoryEnumType;
 import org.ieee.odm.schema.BaseOpfNetworkXmlType;
+import org.ieee.odm.schema.ContentInfoXmlType;
+import org.ieee.odm.schema.LineBranchXmlType;
+import org.ieee.odm.schema.LoadflowBusXmlType;
+import org.ieee.odm.schema.NetworkCategoryEnumType;
 import org.ieee.odm.schema.NetworkXmlType;
+import org.ieee.odm.schema.OpfBranchXmlType;
 import org.ieee.odm.schema.OpfDclfNetworkXmlType;
+import org.ieee.odm.schema.OpfGenBusXmlType;
 import org.ieee.odm.schema.OpfNetworkXmlType;
+import org.ieee.odm.schema.OriginalDataFormatEnumType;
+import org.ieee.odm.schema.PieceWiseLinearModelXmlType;
+import org.ieee.odm.schema.QuadraticModelXmlType;
+import org.ieee.odm.schema.XfrBranchXmlType;
 
 /**
  * A Xml parser for the IEEE DOM schema. 
@@ -67,6 +80,100 @@ public class OpfModelParser extends AclfModelParser {
 	}	
 	
 	/**
+	 * Set BaseCase to Loadflow and Transmission 
+	 * 
+	 * @param parser
+	 * @param originalFormat
+	 */
+	public void setOPFTransInfo(OriginalDataFormatEnumType originalDataFormat) {
+		ContentInfoXmlType info = odmObjFactory.createContentInfoXmlType();
+		getStudyCase().setContentInfo(info);
+		info.setOriginalDataFormat(originalDataFormat);
+		info.setAdapterProviderName("www.interpss.org");
+		info.setAdapterProviderVersion("1.00");
+		
+		getStudyCase().setAnalysisCategory(
+				AnalysisCategoryEnumType.OPF);
+		getStudyCase().setNetworkCategory(
+				NetworkCategoryEnumType.TRANSMISSION);		
+	}	
+	
+	/**
+	 * add a new Bus record to the base case
+	 * 
+	 * @return
+	 */
+	public OpfGenBusXmlType createOpfGenBus() {
+		OpfGenBusXmlType busRec = odmObjFactory.createOpfGenBusXmlType();
+		busRec.setOffLine(false);
+		busRec.setAreaNumber(1);
+		busRec.setZoneNumber(1);
+		getBaseCase().getBusList().getBus().add(BaseJaxbHelper.bus(busRec));
+		return busRec;
+	}	
+	
+	/**
+	 * create a bus object with the id, make sure there is no duplication
+	 * 
+	 * @param id
+	 * @return
+	 * @throws Exception
+	 */
+	public OpfGenBusXmlType createOpfGenBus(String id) throws ODMException {
+		OpfGenBusXmlType busRec = createOpfGenBus();
+		busRec.setId(id);
+		if (this.objectCache.get(id) != null) {
+			throw new ODMException("Bus record duplication, bus id: " + id);
+		}
+		this.objectCache.put(id, busRec);
+		return busRec;
+	}		
+	
+	/**
+	 * add a new bus record to the base case and to the cache table
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public OpfGenBusXmlType createOpfGenBus(String id, long number) throws ODMException {
+		OpfGenBusXmlType busRec = createOpfGenBus(id);
+		busRec.setNumber(number);
+		return busRec;
+	}	
+	/**
+	 * create a LineBranchXmlType object
+	 * 
+	 * @return
+	 */
+	public OpfBranchXmlType createOpfBranch() {
+		OpfBranchXmlType branch = odmObjFactory.createOpfBranchXmlType();
+		branch.setRatingLimit(odmObjFactory.createBranchRatingLimitXmlType());
+		branch.setLineInfo(odmObjFactory.createLineBranchInfoXmlType());
+		intiBranchData(branch);
+		return branch;
+	}
+	
+	public PieceWiseLinearModelXmlType createPWCostModel(){
+		return odmObjFactory.createPieceWiseLinearModelXmlType();
+	}
+	public QuadraticModelXmlType createQuadraticCostModel(){
+		return odmObjFactory.createQuadraticModelXmlType();
+	}
+	public ActivePowerLimitXmlType createActivePowerLimit(){
+		return odmObjFactory.createActivePowerLimitXmlType();
+	}
+	public OpfBranchXmlType createOpfBranch(String fromId, String toId, String cirId) throws ODMException {
+		OpfBranchXmlType branch =  createOpfBranch();
+		addBranch2BaseCase(branch, fromId, toId, null, cirId);
+		return branch;
+	}
+	
+	
+	
+	public OpfModelParser(String encoding) {
+		super(encoding);
+	}
+	/**
 	 * get the base case object of type LoadflowXmlType
 	 * 
 	 * @return
@@ -89,7 +196,8 @@ public class OpfModelParser extends AclfModelParser {
 	@Override
 	public NetworkXmlType createBaseCase() {
 		if (getStudyCase().getBaseCase() == null) {
-			OpfDclfNetworkXmlType baseCase = odmObjFactory.createOpfDclfNetworkXmlType();
+			//OpfDclfNetworkXmlType baseCase = odmObjFactory.createOpfDclfNetworkXmlType();
+			OpfNetworkXmlType baseCase = odmObjFactory.createOpfNetworkXmlType();
 			baseCase.setBusList(odmObjFactory.createNetworkXmlTypeBusList());
 			baseCase.setBranchList(odmObjFactory.createNetworkXmlTypeBranchList());
 			getStudyCase().setBaseCase(BaseJaxbHelper.network(baseCase));
