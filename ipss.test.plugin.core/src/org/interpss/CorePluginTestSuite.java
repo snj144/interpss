ef61823325f1e531c364036f1376bc60c7be8178
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2012-09-02 03:08:54 +0000

    added dclf test cases

--- ipss.test.plugin.core/src/org/interpss/CorePluginTestSuite.java
+++ ipss.test.plugin.core/src/org/interpss/CorePluginTestSuite.java
@@ -8,6 +8,7 @@ import org.interpss.core.adapter.internal.IEEE14Test;
 import org.interpss.core.adapter.psse.SixBus_DclfPsXfr;
 import org.interpss.core.adapter.ucte.UCTEFormatAusPowerTest;
 import org.interpss.core.smallz.IEEE14BusBreakerTest;
+import org.interpss.core.smallz.IEEE14BusBreaker_dclf_Test;
 import org.interpss.core.smallz.IEEE14BusBreaker_lf_Test;
 import org.interpss.core.smallz.SampleSwitchBreakerModelTest;
 import org.interpss.dstab.DStab_2Bus;
@@ -32,6 +33,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({
 	// Dclf
 	SixBus_DclfPsXfr.class,
+	IEEE14BusBreaker_dclf_Test.class,
 	
 	// small Z branch
 	SampleSwitchBreakerModelTest.class,
