e3f1a5d14ca03b788918227768becae175ed1a0b
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2012-09-06 21:22:43 +0000

    added a new case

---
+++ trunk/ipss.test.plugin.core/src/org/interpss/core/adapter/psse/Mod_SixBus_DclfPsXfr.java
@@ -0,0 +1,103 @@
+ /*
+  * @(#) ISONE_CompreResult.java   
+  *
+  * Copyright (C) 2008 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 02/15/2008
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
+package org.interpss.core.adapter.psse;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.logging.Level;
+
+import org.apache.commons.math3.complex.Complex;
+import org.ieee.odm.common.ODMLogger;
+import org.interpss.CorePluginFunction;
+import org.interpss.CorePluginTestSetup;
+import org.interpss.IpssCorePlugin;
+import org.interpss.display.AclfOutFunc;
+import org.interpss.display.DclfOutFunc;
+import org.interpss.numeric.datatype.Unit.UnitType;
+import org.interpss.numeric.sparse.base.SparseEquation.SolverType;
+import org.junit.Test;
+
+import com.interpss.CoreObjectFactory;
+import com.interpss.core.DclfObjectFactory;
+import com.interpss.core.aclf.AclfNetwork;
+import com.interpss.core.aclf.adpter.AclfSwingBus;
+import com.interpss.core.dclf.DclfAlgorithm;
+import com.interpss.pssl.plugin.IpssAdapter;
+import com.interpss.pssl.plugin.IpssAdapter.PsseVersion;
+
+public class Mod_SixBus_DclfPsXfr extends CorePluginTestSetup {
+	@Test
+	public void aclf() throws Exception {
+		IpssCorePlugin.init();
+        IpssCorePlugin.setSparseEqnSolver(SolverType.Native);
+		ODMLogger.getLogger().setLevel(Level.WARNING);
+
+		AclfNetwork net = IpssAdapter.importAclfNet("testData/psse/v30/Mod_SixBus_2WPsXfr.raw")
+					.setFormat(IpssAdapter.FileFormat.PSSE)
+					.setPsseVersion(PsseVersion.PSSE_30)
+					.load(true, "output/odm.xml")
+					.getAclfNet();
+  		//System.out.println(net.net2String());
+
+	  	net.accept(CoreObjectFactory.createLfAlgoVisitor());
+	  	
+  		assertTrue(net.isLfConverged());
+  		
+		System.out.println(CorePluginFunction.AclfResultBusStyle.f(net));
+  		AclfSwingBus swing = net.getAclfBus("Bus1").toSwingBus();
+  		Complex p = swing.getGenResults(UnitType.PU);
+  		//assertTrue(Math.abs(p.getReal()-3.1032)<0.0001);
+  		//assertTrue(Math.abs(p.getImaginary()-0.5212)<0.0001);	   		
+ 	}
+	
+	@Test
+	public void dclf() throws Exception {
+		IpssCorePlugin.init();
+        //IpssCorePlugin.setSparseEqnSolver(SolverType.Native);
+		ODMLogger.getLogger().setLevel(Level.WARNING);
+
+		AclfNetwork net = IpssAdapter.importAclfNet("testData/psse/v30/Mod_SixBus_2WPsXfr.raw")
+					.setFormat(IpssAdapter.FileFormat.PSSE)
+					.setPsseVersion(PsseVersion.PSSE_30)
+					.load()
+					.getAclfNet();
+  		//System.out.println(net.net2String());
+		/*
+		net.accept(CoreObjectFactory.createBusNoArrangeVisitor());
+		for (Bus b : net.getBusList())
+			System.out.println(b.getId() + ": " + b.getSortNumber());
+ 		System.out.println(net.formB1Matrix());
+		*/
+		
+		DclfAlgorithm algo = DclfObjectFactory.createDclfAlgorithm(net);
+		algo.calculateDclf();
+
+		System.out.println(DclfOutFunc.dclfResults(algo, false));
+  		//assertTrue(Math.abs(algo.getBusPower(net.getAclfBus("Bus1"))-3.0723)<0.0001);
+
+		algo.destroy();			
+	}
+}
+
