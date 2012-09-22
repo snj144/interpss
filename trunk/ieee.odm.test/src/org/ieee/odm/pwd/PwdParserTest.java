446f276f350328ffcd1ebb231b642f7cf189a668
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2012-09-22 19:42:25 +0000

    added test case for PWDDataParser.java

---
+++ trunk/ieee.odm.test/src/org/ieee/odm/pwd/PwdParserTest.java
@@ -0,0 +1,45 @@
+package org.ieee.odm.pwd;
+
+import static org.junit.Assert.assertTrue;
+
+import org.ieee.odm.adapter.pwd.impl.PWDDataParser;
+import org.ieee.odm.common.ODMException;
+import org.junit.Test;
+
+public class PwdParserTest {
+	@Test
+	public void test() throws ODMException {
+		PWDDataParser parser = new PWDDataParser();
+		
+		parser.parseMetadata("A, B, C, A:1, D");
+		
+		parser.parseData("1, 2, C, 4, 5");
+
+		//System.out.println(parser.toString());
+		
+		assertTrue(parser.getDouble("A") == 1.0);
+		assertTrue(parser.getString("C").equals("C"));
+		assertTrue(parser.exist("A:1"));
+		assertTrue(!parser.exist("A:2"));
+	}
+
+	@Test
+	public void multi_line_test() throws ODMException {
+		PWDDataParser parser = new PWDDataParser();
+		
+		parser.parseMetadata("A, B, C");
+		parser.parseMetadata("A:1, D");
+		
+		// parseData() return false, since more data is expected
+		assertTrue(parser.parseData("1, 2") == false);
+
+		assertTrue(parser.parseData("C, 4, 5") == true);
+
+		//System.out.println(parser.toString());
+		
+		assertTrue(parser.getDouble("A") == 1.0);
+		assertTrue(parser.getString("C").equals("C"));
+		assertTrue(parser.exist("A:1"));
+		assertTrue(!parser.exist("A:2"));
+	}
+}
