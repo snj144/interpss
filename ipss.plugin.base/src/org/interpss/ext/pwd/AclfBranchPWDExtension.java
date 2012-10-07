c905262eeaa536bf2eece097493e98da9884bdbe
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2012-10-07 02:07:20 +0000

    implemented PWD custom string to extObject mapping

--- ipss.plugin.base/src/org/interpss/ext/pwd/AclfBranchPWDExtension.java
+++ ipss.plugin.base/src/org/interpss/ext/pwd/AclfBranchPWDExtension.java
@@ -1,4 +1,12 @@
 package org.interpss.ext.pwd;
 
-public class AclfBranchPWDExtension {
+import java.util.Hashtable;
+
+public class AclfBranchPWDExtension extends Hashtable<String,String> {
+	private static final long serialVersionUID = 1L;
+
+	public String toString() {
+		String str = "AclfBranchPWDExtension: " + super.toString();
+		return str;
+	}
 }
