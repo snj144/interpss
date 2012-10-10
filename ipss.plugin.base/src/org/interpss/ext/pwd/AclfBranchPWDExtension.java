eea6d579d68e8140bcf13476a77b5f0fad1ee7a5
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2012-10-10 19:39:04 +0000

    updated the PWD extension classes

--- ipss.plugin.base/src/org/interpss/ext/pwd/AclfBranchPWDExtension.java
+++ ipss.plugin.base/src/org/interpss/ext/pwd/AclfBranchPWDExtension.java
@@ -2,8 +2,25 @@ package org.interpss.ext.pwd;
 
 import java.util.Hashtable;
 
+/**
+ *  {CustomString:1=Sub3_230_L34B, CustomString=Line, SubStation=Sub3, EquimentName=L34B}
+ * 
+ * @author mzhou
+ *
+ */
 public class AclfBranchPWDExtension extends Hashtable<String,String> {
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Defined as "EquipmentType_SubstationName_EquipmentName”. Take Transformer_Sub1_T12 for example, 
+	 * it refers to the transformer connected to substation Sub1 (where the “fromBus” is located) 
+	 * with the equipment name of T12. 
+	 * 
+	 * @return
+	 */
+	public String getBranchOutageId() {
+		return this.get("CustomString") + "_" + this.get("SubStation") + "_" + this.get("EquimentName");
+	}
 
 	public String toString() {
 		String str = "AclfBranchPWDExtension: " + super.toString();
--- ipss.plugin.base/src/org/interpss/ext/pwd/AclfBusPWDExtension.java
+++ ipss.plugin.base/src/org/interpss/ext/pwd/AclfBusPWDExtension.java
@@ -2,8 +2,26 @@ package org.interpss.ext.pwd;
 
 import java.util.Hashtable;
 
+/**
+ *  {Gen_CustomString:2=DZONE_1, Gen_CustomString=Sub1_14.9_G1, Gen_CustomString:1=G1, SubStation=Sub1}
+ * 
+ * @author mzhou
+ *
+ */
 public class AclfBusPWDExtension extends Hashtable<String,String> {
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * “SubstationName_EquipmentName”. EquipmentName = Gen_CustomString:1
+	 * 
+	 * @return
+	 */
+	public String getGenOutageId() {
+		if (this.get("SubStation") != null && this.get("Gen_CustomString:1") != null)
+			return this.get("SubStation") + "_" + this.get("Gen_CustomString:1");
+		else 
+			return null;
+	}
 
 	public String toString() {
 		String str = "AclfBusPWDExtension: " + super.toString();
