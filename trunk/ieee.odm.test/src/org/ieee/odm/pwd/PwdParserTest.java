1e76ae1448fc7d5e97cbbc7d5c6eb47ceb4795d8
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2012-12-29 01:17:07 +0000

    code refactor

--- trunk/ieee.odm.test/src/org/ieee/odm/pwd/PwdParserTest.java
+++ trunk/ieee.odm.test/src/org/ieee/odm/pwd/PwdParserTest.java
@@ -2,17 +2,16 @@ package org.ieee.odm.pwd;
 
 import static org.junit.Assert.assertTrue;
 
-import org.ieee.odm.adapter.pwd.impl.PWDDataParser;
+import org.ieee.odm.adapter.InputLineStringParser;
 import org.ieee.odm.common.ODMException;
 import org.ieee.odm.model.aclf.AclfModelParser;
 import org.junit.Test;
 
-import org.ieee.odm.adapter.pwd.impl.PWDDataParser;
 
 public class PwdParserTest {
 	@Test
 	public void test() throws ODMException {
-		PWDDataParser parser = new PWDDataParser(new AclfModelParser());
+		InputLineStringParser parser = new InputLineStringParser();
 		
 		parser.parseMetadata("A, B, C, A:1, D");
 		
@@ -28,7 +27,7 @@ public class PwdParserTest {
 
 	@Test
 	public void multi_line_test() throws ODMException {
-		PWDDataParser parser = new PWDDataParser(new AclfModelParser());
+		InputLineStringParser parser = new InputLineStringParser();
 		
 		parser.parseMetadata("A, B, C");
 		parser.parseMetadata("A:1, D");
