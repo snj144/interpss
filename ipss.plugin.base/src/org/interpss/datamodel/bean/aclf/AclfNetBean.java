f76ff4d1b53e9d715d7fec7f8c3c8a4cbc8b53dd
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2013-01-10 15:30:47 +0000

    code refactor

--- ipss.plugin.base/src/org/interpss/datamodel/bean/aclf/AclfNetBean.java
+++ ipss.plugin.base/src/org/interpss/datamodel/bean/aclf/AclfNetBean.java
@@ -3,12 +3,12 @@ package org.interpss.datamodel.bean.aclf;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AclfCaseBean {
+public class AclfNetBean {
 	public String name, desc;
 	public double baseKva;
 	
 	public List<AclfBusBean> busList;
 	public List<AclfBranchBean> branchList;
 	
-	public AclfCaseBean() { busList = new ArrayList<>(); branchList = new ArrayList<>(); }
+	public AclfNetBean() { busList = new ArrayList<>(); branchList = new ArrayList<>(); }
 }
