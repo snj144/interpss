af60ff22af504ea87f896867ea81afcedbef6363
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2013-01-10 00:49:32 +0000

    added model.bean concept

---
+++ ipss.plugin.base/src/org/interpss/model/bean/aclf/AclfCaseBean.java
@@ -0,0 +1,14 @@
+package org.interpss.model.bean.aclf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AclfCaseBean {
+	public String name, desc;
+	public double baseKva;
+	
+	public List<AclfBusBean> busList;
+	public List<AclfBranchBean> branchList;
+	
+	public AclfCaseBean() { busList = new ArrayList<>(); branchList = new ArrayList<>(); }
+}
