81b2c074bda0a097c663e69f3f3b68a7e44cb591
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2013-01-12 04:21:34 +0000

    updated JSON test case

--- ipss.test.plugin.core/src/org/interpss/beanModel/JSONBeanTestCases.java
+++ ipss.test.plugin.core/src/org/interpss/beanModel/JSONBeanTestCases.java
@@ -27,10 +27,13 @@ package org.interpss.beanModel;
 import static org.junit.Assert.assertTrue;
 
 import org.interpss.CorePluginTestSetup;
+import org.interpss.datamodel.bean.BaseBranchBean;
 import org.interpss.datamodel.bean.aclf.AclfBranchBean;
 import org.interpss.datamodel.bean.aclf.AclfBusBean;
 import org.interpss.datamodel.bean.aclf.AclfNetBean;
+import org.interpss.datamodel.bean.aclf.AclfNetResultBean;
 import org.interpss.mapper.bean.aclf.AclfNetBeanMapper;
+import org.interpss.mapper.bean.aclf.AclfResultBeanMapper;
 import org.interpss.numeric.datatype.Unit.UnitType;
 import org.junit.Test;
 
@@ -59,7 +62,12 @@ public class JSONBeanTestCases extends CorePluginTestSetup {
   		//System.out.println(swing.getGenResults(UnitType.PU, net.getBaseKva()));
 		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getReal()-2.57943)<0.0001);
 		assertTrue(Math.abs(swing.getGenResults(UnitType.PU).getImaginary()-2.2994)<0.0001);
-
+		
+		AclfNetResultBean aclfResult = new AclfResultBeanMapper()
+				.map2Model(aclfNet);
+		
+		System.out.println(new Gson().toJson(aclfResult));
+		
 	}	
 	
 	@Test
@@ -84,6 +92,7 @@ public class JSONBeanTestCases extends CorePluginTestSetup {
 	}	
 	
 	private AclfNetBean createNetCaseData1() {
+		/*
 		String str = "{'base_kva':100000.0," +
 				"'bus_list':[" +
 				"	{'base_v':13800.0,'area':'1','zone':'1','load_code':'ConstP','v_msg':1.0,'v_ang':0.0,'p_gen':0.0,'q_gen':0.0,'p_load':1.6,'q_load':0.8,'id':'1'}," +
@@ -100,6 +109,23 @@ public class JSONBeanTestCases extends CorePluginTestSetup {
 				"	{'f_id':'5','t_id':'3','cir_id':'1','code':'Xfr','r':0.0,'x':0.03,'b':0.0,'f_ratio':1.0,'t_ratio':1.05,'f_ang':0.0,'t_ang':0.0}" +
 				"]," +
 				"'name':'Test case','desc':'A Aclf JSon test case'}";
+		*/
+		String str = "{'base_kva':'100000'," +
+				"'bus_list':[" +
+				"{'id':1,'base_v':138000,'load_code':'ConstP','p_load':1.6,'q_load':0.8}," +
+				"{'id':2,'base_v':138000,'load_code':'ConstP','p_load':2,'q_load':1}," +
+				"{'id':3,'base_v':138000,'load_code':'ConstP','p_load':3.7,'q_load':1.3}," +
+				"{'id':4,'base_v':1000,'gen_code':'PV','v_mag':1.05,'p_gen':5}," +
+				"{'id':5,'base_v':4000,'gen_code':'Swing','v_mag':1.05,'v_ang':0}" +
+				"]," +
+				"'branch_list':[" +
+				"{'f_id':1,'t_id':2,'cir_id':1,'code':'Line','r':0.04,'x':0.25,'b':0.5}," +
+				"{'f_id':1,'t_id':3,'cir_id':1,'code':'Line','r':0.1,'x':0.35}," +
+				"{'f_id':2,'t_id':3,'cir_id':1,'code':'Line','r':0.08,'x':0.3,'b':0.5}," +
+				"{'f_id':4,'t_id':2,'cir_id':1,'code':'Xfr','x':0.015,'t_ratio':1.05}," +
+				"{'f_id':5,'t_id':3,'cir_id':1,'code':'Xfr','x':0.03,'t_ratio':1.05}" +
+				"]" +
+				"}";
 		return new Gson().fromJson(str, AclfNetBean.class);
 	}
 	
@@ -150,7 +176,7 @@ public class JSONBeanTestCases extends CorePluginTestSetup {
 		busBean.area = "1";
 		busBean.zone = "1";
 		busBean.gen_code = AclfBusBean.GenCode.PV;
-		busBean.v_msg = 1.05;
+		busBean.v_mag = 1.05;
 		busBean.p_gen = 5.0;
 		
 		busBean = new AclfBusBean();
@@ -161,7 +187,7 @@ public class JSONBeanTestCases extends CorePluginTestSetup {
 		busBean.area = "1";
 		busBean.zone = "1";
 		busBean.gen_code = AclfBusBean.GenCode.Swing;
-		busBean.v_msg = 1.05;
+		busBean.v_mag = 1.05;
 		busBean.v_ang = 0.0;
 		
 		
@@ -171,7 +197,7 @@ public class JSONBeanTestCases extends CorePluginTestSetup {
 		//1   1   2    0.04    0.25    -0.25
 		braBean.f_id = "1";
 		braBean.t_id = "2";
-		braBean.code = AclfBranchBean.BranchCode.Line;
+		braBean.code = BaseBranchBean.BranchCode.Line;
 		braBean.r = 0.04;
 		braBean.x = 0.25;
 		braBean.b = 0.5;
@@ -181,7 +207,7 @@ public class JSONBeanTestCases extends CorePluginTestSetup {
 		//2   1   3    0.1     0.35     0.0
 		braBean.f_id = "1";
 		braBean.t_id = "3";
-		braBean.code = AclfBranchBean.BranchCode.Line;
+		braBean.code = BaseBranchBean.BranchCode.Line;
 		braBean.r = 0.1;
 		braBean.x = 0.35;
 
@@ -190,7 +216,7 @@ public class JSONBeanTestCases extends CorePluginTestSetup {
 		//3   2   3    0.08    0.3     -0.25
 		braBean.f_id = "2";
 		braBean.t_id = "3";
-		braBean.code = AclfBranchBean.BranchCode.Line;
+		braBean.code = BaseBranchBean.BranchCode.Line;
 		braBean.r = 0.08;
 		braBean.x = 0.3;
 		braBean.b = 0.5;
@@ -200,7 +226,7 @@ public class JSONBeanTestCases extends CorePluginTestSetup {
 		//4   4   2    0.0     0.015    1.05
 		braBean.f_id = "4";
 		braBean.t_id = "2";
-		braBean.code = AclfBranchBean.BranchCode.Xfr;
+		braBean.code = BaseBranchBean.BranchCode.Xfr;
 		braBean.x = 0.015;
 		braBean.t_ratio = 1.05;
 
@@ -209,7 +235,7 @@ public class JSONBeanTestCases extends CorePluginTestSetup {
 		//5   5   3    0.0     0.03     1.05
 		braBean.f_id = "5";
 		braBean.t_id = "3";
-		braBean.code = AclfBranchBean.BranchCode.Xfr;
+		braBean.code = BaseBranchBean.BranchCode.Xfr;
 		braBean.x = 0.03;
 		braBean.t_ratio = 1.05;
 	
