1ff9ed858925ee1f635d91ff787b368defa3e7d6
ipssdev <ipssdev@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2013-02-04 05:21:52 +0000

    Tag before working on ipss2.0_dclf implementation

---
+++ ipss.plugin.base/src/org/interpss/datamodel/bean/BaseNetBean.java
@@ -0,0 +1,33 @@
+package org.interpss.datamodel.bean;
+
+import java.util.List;
+
+import org.interpss.numeric.datatype.Unit.UnitType;
+
+public abstract class BaseNetBean extends BaseJSONBean {
+	public double 
+		base_kva;      						// network base kva 
+
+	/**
+	 * units 
+	 */
+	public UnitType
+		unit_ang  = UnitType.Deg,          	// angle unit for voltage, PsXfr shifting angle
+		unit_bus_v = UnitType.PU, 			// bus voltage unit
+		unit_bus_p = UnitType.PU,			// bus power (gen/load) unit
+		unit_branch_z = UnitType.PU,		// branch impedance unit
+		unit_branch_cur = UnitType.Amp,     // branch current unit
+		unit_branch_b = UnitType.PU;        // branch shunt Y unit
+	
+	public BaseNetBean() {}
+	
+	public boolean validate(List<String> msgList) {
+		boolean noErr = true;
+		if (this.base_kva == 0.0) {
+			msgList.add("NetBean data error: baseKva not defined");
+			noErr = false;
+		}
+		return noErr; 
+	}	
+	
+}
