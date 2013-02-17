e76347dcf7b161832b1017ed5988ffe6e49ee1ba
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2013-02-17 17:21:12 +0000

    repair the repo

---
+++ ipss.numeric/src/org/interpss/numeric/sparse/impl/SparseEqnMatrix2x2CSparseJImpl1.java
@@ -0,0 +1,15 @@
+package org.interpss.numeric.sparse.impl;
+
+import org.interpss.numeric.sparse.SparseEqnMatrix2x2;
+import org.interpss.numeric.sparse.base.BaseSparseEqnMatrix2x2CSparseJImpl;
+
+public class SparseEqnMatrix2x2CSparseJImpl1 extends BaseSparseEqnMatrix2x2CSparseJImpl implements SparseEqnMatrix2x2{
+
+        public SparseEqnMatrix2x2CSparseJImpl1() {
+                super();
+        }
+        
+        public SparseEqnMatrix2x2CSparseJImpl1(int n) {
+                super(n);
+        }
+}
