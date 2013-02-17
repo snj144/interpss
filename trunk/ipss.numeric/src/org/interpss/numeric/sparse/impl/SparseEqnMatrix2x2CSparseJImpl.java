643a4907ca12bb77bd6950d49828b140eace25e9
scutparis <scutparis@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2013-02-17 04:58:00 +0000

    CSparseJ Matrix2x2 implementation phase-1 completed

---
+++ trunk/ipss.numeric/src/org/interpss/numeric/sparse/impl/SparseEqnMatrix2x2CSparseJImpl.java
@@ -0,0 +1,18 @@
+package org.interpss.numeric.sparse.impl;
+
+import org.interpss.numeric.sparse.SparseEqnMatrix2x2;
+import org.interpss.numeric.sparse.base.BaseSparseEqnMatrix2x2CSparseJImpl;
+
+public class SparseEqnMatrix2x2CSparseJImpl extends BaseSparseEqnMatrix2x2CSparseJImpl implements SparseEqnMatrix2x2{
+
+	public SparseEqnMatrix2x2CSparseJImpl() {
+		super();
+	}
+	
+	public SparseEqnMatrix2x2CSparseJImpl(int n) {
+		super(n);
+	}
+
+
+
+}
