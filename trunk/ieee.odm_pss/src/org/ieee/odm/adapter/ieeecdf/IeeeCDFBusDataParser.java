9bf9adbbdb97658c5d46a2d50d221318bae33ac9
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2013-04-15 21:12:44 +0000

    refactor IEEE CDF adapter implementation using the DataParser

---
+++ trunk/ieee.odm_pss/src/org/ieee/odm/adapter/ieeecdf/IeeeCDFBusDataParser.java
@@ -0,0 +1,125 @@
+/*
+ * @(#)IeeeCDFNetDataParser.java   
+ *
+ * Copyright (C) 2006-2013 www.interpss.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @Author Mike Zhou
+ * @Version 1.0
+ * @Date 04/11/2013
+ * 
+ *   Revision History
+ *   ================
+ *
+ */
+
+package org.ieee.odm.adapter.ieeecdf;
+
+import java.util.StringTokenizer;
+
+import org.ieee.odm.adapter.BaseDataFieldParser;
+import org.ieee.odm.common.ODMException;
+import org.ieee.odm.model.base.ModelStringUtil;
+
+/**
+ * Class for processing IEEE CDF Network data line string
+ * 
+ * @author mzhou
+ *
+ */
+public class IeeeCDFBusDataParser extends BaseDataFieldParser {
+	/**
+	 * input line meta data
+	 */
+	public static String[] FieldDef = new String[] {
+		   //  0               1        2       3        4
+		     "BusNumber", "BusName", "Area",  "Zone", "Type", 
+		   //  5               6        7       8        9
+		     "VMag",      "VAng",    "LoadP", "LoadQ", "GenP", 
+		   //  10             11       12            13             14
+		     "GenQ",      "BaseKV",  "DesiredV",  "MaxVarVolt", "MinVarVolt", 
+		   //  15             16       17              18      19
+		     "ShuntG",    "ShuntB",  "RemoteBusNumber" 
+		   //  20             21       22      23      24
+		   //  25             26       27      28      29
+	};
+	
+	/**
+	 * constructor
+	 */
+	public IeeeCDFBusDataParser() {
+		super();
+		this.dataParser.setMetadata(FieldDef);
+	}
+
+	@Override public void parseFields(final String str) throws ODMException {
+		if (str.indexOf(',') >= 0) {
+			final StringTokenizer st = new StringTokenizer(str, ",");
+			int cnt = 0;
+			while (st.hasMoreTokens()) {
+				this.dataParser.setValue(cnt++, st.nextToken().trim());
+			}
+		} else {
+			//Columns  1- 4   Bus number [I] *
+			this.dataParser.setValue(0, str.substring(0, 4).trim());
+
+			//Columns  6-17   Name [A] (left justify) *
+			this.dataParser.setValue(1, str.substring(5, 17).trim());
+
+			//Columns 19-20   Load flow area number [I].  Don't use zero! *
+			//Columns 21-23   Loss zone number [I]
+			this.dataParser.setValue(2, str.substring(18, 20).trim());
+			this.dataParser.setValue(3, str.substring(20, 23).trim());
+
+			//Columns 77-83   Base kV [F]
+			this.dataParser.setValue(11, str.substring(76, 83));
+
+			//Columns 25-26   Type [I] *
+			//		0 - Unregulated (load, PQ)
+			//		1 - Hold MVAR generation within voltage limits, (gen, PQ)
+			//		2 - Hold voltage within VAR limits (gen, PV)
+			//		3 - Hold voltage and angle (swing, V-Theta; must always have one)
+			this.dataParser.setValue(4, str.substring(24, 26).trim());
+
+			//Columns 28-33   Final voltage, p.u. [F] *
+			//Columns 34-40   Final angle, degrees [F] *
+			this.dataParser.setValue(5, str.substring(27, 33));
+			this.dataParser.setValue(6, str.substring(33, 40));
+
+			//Columns 41-49   Load MW [F] *
+			//Columns 50-59   Load MVAR [F] *
+			this.dataParser.setValue(7, str.substring(40, 49));
+			this.dataParser.setValue(8, str.substring(49, 59));
+
+			//Columns 60-67   Generation MW [F] *
+			//Columns 68-75   Generation MVAR [F] *
+			this.dataParser.setValue(9, str.substring(59, 67));
+			this.dataParser.setValue(10, str.substring(67, 75));
+
+			//Columns 107-114 Shunt conductance G (per unit) [F] *
+			//Columns 115-122 Shunt susceptance B (per unit) [F] *
+			this.dataParser.setValue(15, ModelStringUtil.getString(str,107, 114));
+			this.dataParser.setValue(16, ModelStringUtil.getString(str,115, 122));
+
+			//Columns 85-90   Desired volts (pu) [F] (This is desired remote voltage if this bus is controlling another bus.)
+			this.dataParser.setValue(12, ModelStringUtil.getString(str,85, 90));
+
+			//Columns 91-98   Minimum MVAR or voltage limit [F]
+			//Columns 99-106  Maximum MVAR or voltage limit [F]
+			this.dataParser.setValue(13, ModelStringUtil.getString(str,91, 98));
+			this.dataParser.setValue(14, ModelStringUtil.getString(str,99, 106));
+
+			//Columns 124-127 Remote controlled bus number
+			this.dataParser.setValue(17, ModelStringUtil.getString(str,123, 127).trim());
+		}
+	}
+}
