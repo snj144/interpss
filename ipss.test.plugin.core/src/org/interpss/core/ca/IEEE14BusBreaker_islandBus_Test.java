5158f6717cd8b871430e8bfd84f834c1180d36a4
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2013-05-07 19:13:19 +0000

    updated IOutageIslanding interface

--- ipss.test.plugin.core/src/org/interpss/core/ca/IEEE14BusBreaker_islandBus_Test.java
+++ ipss.test.plugin.core/src/org/interpss/core/ca/IEEE14BusBreaker_islandBus_Test.java
@@ -57,7 +57,7 @@ public class IEEE14BusBreaker_islandBus_Test extends CorePluginTestSetup {
 		list.add(CoreObjectFactory.createOutageBranch(net.getBranch("Bus4->Bus9(1)")));
 		list.add(CoreObjectFactory.createOutageBranch(net.getBranch("Bus5->Bus6(1)")));
 		
-		IslandBusProcesor proc = new IslandBusProcesor();
+		IslandBusProcesor proc = new IslandBusProcesor(net);
 		proc.findIslandBus(list, net);
 	  	System.out.println("Original network with islanding");
 	  	System.out.println(proc.getIslandBusIdSet());
@@ -94,7 +94,7 @@ public class IEEE14BusBreaker_islandBus_Test extends CorePluginTestSetup {
 		list.add(CoreObjectFactory.createOutageBranch(net.getBranch("Bus4->Bus9(1)")));
 		list.add(CoreObjectFactory.createOutageBranch(net.getBranch("Bus5->Bus6(1)")));
 		
-		IslandBusProcesor proc = new IslandBusProcesor();
+		IslandBusProcesor proc = new IslandBusProcesor(net);
 		proc.findIslandBus(list, net);
 	  	System.out.println("Consolidated network with islanding");
 	  	System.out.println(proc.getIslandBusIdSet());
