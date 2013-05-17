f6e4dfea6dddd699eb2d7582c1d8c37e3cd5f115
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2013-05-17 02:14:20 +0000

    refactor PSSE adatper, working in progress

---
+++ ieee.odm_pss/src/org/ieee/odm/adapter/psse/mapper/PSSEOwnerDataMapper.java
@@ -0,0 +1,43 @@
+ /*
+  * @(#)PSSEV30GenDataRec.java   
+  *
+  * Copyright (C) 2006 www.interpss.org
+  *
+  * This program is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+  * as published by the Free Software Foundation; either version 2.1
+  * of the License, or (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * @Author Mike Zhou
+  * @Version 1.0
+  * @Date 09/15/2006
+  * 
+  *   Revision History
+  *   ================
+  *
+  */
+
+package org.ieee.odm.adapter.psse.mapper;
+
+import org.ieee.odm.adapter.psse.PsseVersion;
+import org.ieee.odm.adapter.psse.parser.PSSEOwnerDataParser;
+import org.ieee.odm.common.ODMException;
+import org.ieee.odm.model.aclf.AclfModelParser;
+
+public class PSSEOwnerDataMapper extends BasePSSEDataMapper {
+	
+	public PSSEOwnerDataMapper(PsseVersion ver) {
+		super(ver);
+		this.dataParser = new PSSEOwnerDataParser(ver);
+	}
+	
+
+	public void procLineString(String lineStr, final AclfModelParser parser) throws ODMException {
+		dataParser.parseFields(lineStr);
+	}
+}
