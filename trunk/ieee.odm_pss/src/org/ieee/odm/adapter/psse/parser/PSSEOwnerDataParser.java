a34f86ebd1c28ddb757ca599b0f13acb86312bdf
interpss <interpss@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2013-05-17 02:14:20 +0000

    refactor PSSE adatper, working in progress

---
+++ trunk/ieee.odm_pss/src/org/ieee/odm/adapter/psse/parser/PSSEOwnerDataParser.java
@@ -0,0 +1,53 @@
+/*
+ * @(#)PSSEBusDataParser.java   
+ *
+ * Copyright (C) 2006-2013 www.interpss.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
+ * as published by the Free Software Foundation; either version 2.1
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * @Author Mike Zhou
+ * @Version 1.0
+ * @Date 04/11/2013
+ * 
+ *   Revision History
+ *   ================
+ *
+ */
+
+package org.ieee.odm.adapter.psse.parser;
+
+import org.ieee.odm.adapter.psse.PsseVersion;
+import org.ieee.odm.common.ODMException;
+
+/**
+ * Class for processing IEEE CDF bus data line string
+ * 
+ * @author mzhou
+ *
+ */
+public class PSSEOwnerDataParser extends BasePSSEDataParser {
+	public PSSEOwnerDataParser(PsseVersion ver) {
+		super(ver);
+	}	
+	
+	@Override public String[] getMetadata() {
+		/* Format V26
+		 * 
+		*/
+		return new String[] {
+		   //  0----------1----------2----------3----------4
+			 "ARFROM",   "ARTO",    "TRID",   "PTRAN"             
+		};
+	}
+	
+	@Override public void parseFields(final String str) throws ODMException {
+  	}
+}
