f47d78b06032585f24867c057457b19bbed2ac01
scutparis <scutparis@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2013-07-07 01:42:54 +0000

    PSSE Dynamic data parser

---
+++ ieee.odm_pss/src/org/ieee/odm/adapter/psse/parser/dynamic/tur_gov/PSSETurGovIEEE1973Parser.java
@@ -0,0 +1,7 @@
+package org.ieee.odm.adapter.psse.parser.dynamic.tur_gov;
+
+public class PSSETurGovIEEE1973Parser {
+	
+	//PSSE IEESGO
+
+}
---
+++ ieee.odm_pss/src/org/ieee/odm/adapter/psse/parser/dynamic/tur_gov/PSSETurGovTGOV1Parser.java
@@ -0,0 +1,5 @@
+package org.ieee.odm.adapter.psse.parser.dynamic.tur_gov;
+
+public class PSSETurGovTGOV1Parser {
+
+}
