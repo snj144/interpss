264af63d08a13efa4e4adffa765e3bd04ebc1b32
scutparis <scutparis@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2013-07-12 19:44:35 +0000

    check in IEEE 9Bus acsc test case

---
+++ ipss.test.plugin.core/src/org/interpss/odm/acsc/IEEE9Bus_Acsc_test.java
@@ -0,0 +1,82 @@
+package org.interpss.odm.acsc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.ieee.odm.adapter.IODMAdapter.NetType;
+import org.ieee.odm.adapter.psse.PSSEAdapter;
+import org.ieee.odm.adapter.psse.PSSEAdapter.PsseVersion;
+import org.ieee.odm.model.acsc.AcscModelParser;
+import org.interpss.IpssCorePlugin;
+import org.interpss.mapper.odm.ODMAcscDataMapper;
+import org.interpss.numeric.exp.IpssNumericException;
+import org.interpss.numeric.sparse.ISparseEqnComplex;
+import org.junit.Test;
+
+import com.interpss.CoreObjectFactory;
+import com.interpss.common.exp.InterpssException;
+import com.interpss.core.acsc.AcscNetwork;
+import com.interpss.core.acsc.SequenceCode;
+import com.interpss.core.algo.AclfMethod;
+import com.interpss.core.algo.LoadflowAlgorithm;
+
+
+public class IEEE9Bus_Acsc_test {
+	
+	@Test
+	public void testIeee9Bus() throws InterpssException, IpssNumericException{
+			IpssCorePlugin.init();
+			PSSEAdapter adapter = new PSSEAdapter(PsseVersion.PSSE_30);
+			assertTrue(adapter.parseInputFile(NetType.AcscNet, new String[]{
+					"testData/psse/v30/IEEE9Bus/ieee9.raw",
+					"testData/psse/v30/IEEE9Bus/ieee9.seq"
+			}));
+			AcscModelParser acscParser =(AcscModelParser) adapter.getModel();
+			acscParser.stdout();
+			
+			AcscNetwork net = new ODMAcscDataMapper().map2Model(acscParser).getAcscNet();
+			
+			//set the order in original sequence for better testing
+			for(int i=1;i<=net.getNoBus();i++){
+				net.getBus("Bus"+i).setSortNumber(i-1);
+			}
+			net.setBusNumberArranged(true);
+			
+			LoadflowAlgorithm algo = CoreObjectFactory.createLoadflowAlgorithm(net);
+		  	algo.setLfMethod(AclfMethod.PQ);
+		  	algo.getLfAdjAlgo().setApplyAdjustAlgo(false);
+		  	algo.loadflow();
+	  	
+	  		assertTrue( net.isLfConverged());
+	  		//System.out.println(AclfOutFunc.loadFlowSummary(net));
+	  		//System.out.println(net.net2String());
+	  		
+	  		
+	        ISparseEqnComplex posYMatrix = net.formYMatrix(SequenceCode.POSITIVE, false);
+	        
+	        //Gen Bus: Bus 1
+	        //Yii: 0.0 + (-42.63668430335097i)
+	        assertTrue(posYMatrix.getA(0, 0).getReal()==0);
+	        assertTrue(Math.abs(posYMatrix.getA(0, 0).getImaginary()+42.6366)<1.0E-4);
+	        //Yij (bus1->bus4): -0.0 + (17.636684303350968i)
+	        assertTrue(posYMatrix.getA(0, 3).getReal()==0);
+	        assertTrue(Math.abs(posYMatrix.getA(0, 3).getImaginary()-17.6366)<1.0E-4);
+	        
+	        //Load Bus: Bus 5
+	        //Yii: 3.81 - j17.84
+	        assertTrue(Math.abs(posYMatrix.getA(4, 4).getReal()-3.81)<1.0E-2);
+	        assertTrue(Math.abs(posYMatrix.getA(4, 4).getImaginary()+17.84)<1.0E-2);
+	        
+	        //Y54:-1.37 + j11.60
+	        assertTrue(Math.abs(posYMatrix.getA(4, 3).getReal()+1.37)<1.0E-2);
+	        assertTrue(Math.abs(posYMatrix.getA(4, 3).getImaginary()-11.60)<1.0E-2);
+	        
+	        //Non-Gen, Non-Load: Bus7
+	        //Yii 2.80 - j35.45
+	        assertTrue(Math.abs(posYMatrix.getA(6, 6).getReal()-2.80)<1.0E-2);
+	        assertTrue(Math.abs(posYMatrix.getA(6, 6).getImaginary()+35.45)<1.0E-2);
+	       
+			
+		}
+	
+
+}
