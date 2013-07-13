f85141fb0c838a0fa6b231d02c60449b79d28839
scutparis <scutparis@d3a5fb67-f619-0410-90ae-095cbca2acfc>
2013-07-13 23:38:25 +0000

    update IEEE 9Bus Dstab test case

--- trunk/ipss.test.plugin.core/src/org/interpss/dstab/DStab_IEEE9Bus_Test.java
+++ trunk/ipss.test.plugin.core/src/org/interpss/dstab/DStab_IEEE9Bus_Test.java
@@ -1,6 +1,10 @@
 package org.interpss.dstab;
 
+import static com.interpss.dstab.cache.StateVariableRecorder.StateVarRecType.MachineState;
 import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+import java.util.logging.Level;
 
 import org.ieee.odm.adapter.IODMAdapter.NetType;
 import org.ieee.odm.adapter.psse.PSSEAdapter;
@@ -8,16 +12,23 @@ import org.ieee.odm.adapter.psse.PSSEAdapter.PsseVersion;
 import org.ieee.odm.model.acsc.AcscModelParser;
 import org.ieee.odm.model.dstab.DStabModelParser;
 import org.interpss.IpssCorePlugin;
+import org.interpss.display.AclfOutFunc;
 import org.interpss.mapper.odm.ODMAcscDataMapper;
 import org.interpss.mapper.odm.ODMDStabDataMapper;
+import org.interpss.numeric.util.Number2String;
 import org.junit.Test;
 
 import com.interpss.CoreObjectFactory;
 import com.interpss.SimuObjectFactory;
+import com.interpss.common.util.IpssLogger;
 import com.interpss.core.acsc.AcscNetwork;
 import com.interpss.core.algo.LoadflowAlgorithm;
 import com.interpss.dstab.DStabilityNetwork;
 import com.interpss.dstab.algo.DynamicSimuAlgorithm;
+import com.interpss.dstab.algo.DynamicSimuMethod;
+import com.interpss.dstab.cache.StateVariableRecorder;
+import com.interpss.dstab.cache.StateVariableRecorder.StateRecord;
+import com.interpss.dstab.common.DStabOutSymbol;
 import com.interpss.simu.SimuContext;
 import com.interpss.simu.SimuCtxType;
 
@@ -44,14 +55,55 @@ public class DStab_IEEE9Bus_Test extends DStabTestSetupBase{
 		
 		
 	    DStabilityNetwork dsNet =simuCtx.getDStabilityNet();
-	    System.out.println(dsNet.net2String());
+	    //System.out.println(dsNet.net2String());
 	    
 		DynamicSimuAlgorithm dstabAlgo = simuCtx.getDynSimuAlgorithm();
 		LoadflowAlgorithm aclfAlgo = dstabAlgo.getAclfAlgorithm();
 		assertTrue(aclfAlgo.loadflow());
+		System.out.println(AclfOutFunc.loadFlowSummary(dsNet));
 		
+		dstabAlgo.setSimuMethod(DynamicSimuMethod.MODIFIED_EULER);
+		dstabAlgo.setSimuStepSec(0.001);
+		dstabAlgo.setTotalSimuTimeSec(0.02);
+		dstabAlgo.setRefMachine(dsNet.getMachine("Bus1-mach1"));
 		
+		StateVariableRecorder ssRecorder = new StateVariableRecorder(0.0001);
+		ssRecorder.addCacheRecords("Bus2-mach1",      // mach id 
+				MachineState,    // record type
+				DStabOutSymbol.OUT_SYMBOL_MACH_ANG,       // state variable name
+				0.005,                                      // time steps for recording 
+				4);                                      // total points to record 
 		
+		ssRecorder.addCacheRecords("Bus2-mach1",      // mach id 
+				MachineState,    // record type
+				DStabOutSymbol.OUT_SYMBOL_MACH_PM,       // state variable name
+				0.005,                                      // time steps for recording 
+				4);                                      // total points to record
+		
+		// set the output handler
+		dstabAlgo.setSimuOutputHandler(ssRecorder);
+		
+		IpssLogger.getLogger().setLevel(Level.INFO);
+		if (dstabAlgo.initialization()) {
+			System.out.println("Running DStab simulation ...");
+			dstabAlgo.performSimulation();
+		}
+		
+		// output recorded simulation results
+		List<StateRecord> list = ssRecorder.getMachineRecords(
+				"Bus2-mach1", MachineState, DStabOutSymbol.OUT_SYMBOL_MACH_ANG);
+		System.out.println("\n\n Bus2 Machine Anagle");
+		for (StateRecord rec : list) {
+			System.out.println(Number2String.toStr(rec.t) + ", " + Number2String.toStr(rec.variableValue));
+		}
+
+		
+		list = ssRecorder.getMachineRecords(
+				"Bus2-mach1", MachineState, DStabOutSymbol.OUT_SYMBOL_MACH_PM);
+		System.out.println("\n\n Bus2 Machine PM");
+		for (StateRecord rec : list) {
+			System.out.println(Number2String.toStr(rec.t) + ", " + Number2String.toStr(rec.variableValue));
+		}
 	}
 
 }
